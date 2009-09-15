8adeaea06a4c1a66621e96c04bd66e5b0cf20305
Frederik Vernelen <frederik.vernelen@fedict.be>
2009-09-15 11:39:50 +0000

    in the sdk doxygen script : moved the creation of the doc external dirs upwards , as doxygen.sh is using them as well

--- eid-mw/_src/eidmw/_DocsInternal/API/doxygen_sdk.sh
+++ eid-mw/_src/eidmw/_DocsInternal/API/doxygen_sdk.sh
@@ -3,14 +3,14 @@
 
 DOXYGEN_CONFIG_FILE="DoxySDK.cfg"
 
+if [ ! -e ../../_DocsExternal/C++_Api/html ]; then
+mkdir -p ../../_DocsExternal/C++_Api/html
+fi
+
 if [ `uname -s` = "Darwin" ]; then
 ../../../ThirdParty/doxygen-1.5.3/macosx/doxygen $DOXYGEN_CONFIG_FILE
 else
 ./doxygen.sh $DOXYGEN_CONFIG_FILE
-fi
-
-if [ ! -e ../../_DocsExternal/C++_Api/html ]; then
-mkdir -p ../../_DocsExternal/C++_Api/html
 fi
 
 cp *.css ../../_DocsExternal/C++_Api/html
@@ -19,14 +19,14 @@ cp *.jpg ../../_DocsExternal/C++_Api/html
 
 DOXYGEN_CONFIG_FILE="DoxySDK_java.cfg"
 
+if [ ! -e ../../_DocsExternal/Java_Api/html ]; then
+mkdir -p ../../_DocsExternal/Java_Api/html
+fi
+
 if [ `uname -s` = "Darwin" ]; then
 ../../../ThirdParty/doxygen-1.5.3/macosx/doxygen $DOXYGEN_CONFIG_FILE
 else
 ./doxygen.sh $DOXYGEN_CONFIG_FILE
-fi
-
-if [ ! -e ../../_DocsExternal/Java_Api/html ]; then
-mkdir -p ../../_DocsExternal/Java_Api/html
 fi
 
 cp *.css ../../_DocsExternal/Java_Api/html
