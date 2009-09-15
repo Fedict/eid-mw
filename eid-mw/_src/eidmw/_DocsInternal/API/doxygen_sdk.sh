240e113bc805f0c15bebd30b553d23cc5eca2aaf
Frederik Vernelen <frederik.vernelen@fedict.be>
2009-09-15 09:11:59 +0000

    changed doxygen script : before copying doc files, check if destination dir exist, and if not, create it

--- eid-mw/_src/eidmw/_DocsInternal/API/doxygen_sdk.sh
+++ eid-mw/_src/eidmw/_DocsInternal/API/doxygen_sdk.sh
@@ -9,6 +9,10 @@ else
 ./doxygen.sh $DOXYGEN_CONFIG_FILE
 fi
 
+if [ ! -e ../../_DocsExternal/C++_Api/html ]; then
+mkdir -p ../../_DocsExternal/C++_Api/html
+fi
+
 cp *.css ../../_DocsExternal/C++_Api/html
 cp *.jpg ../../_DocsExternal/C++_Api/html
 
@@ -19,6 +23,10 @@ if [ `uname -s` = "Darwin" ]; then
 ../../../ThirdParty/doxygen-1.5.3/macosx/doxygen $DOXYGEN_CONFIG_FILE
 else
 ./doxygen.sh $DOXYGEN_CONFIG_FILE
+fi
+
+if [ ! -e ../../_DocsExternal/Java_Api/html ]; then
+mkdir -p ../../_DocsExternal/Java_Api/html
 fi
 
 cp *.css ../../_DocsExternal/Java_Api/html
