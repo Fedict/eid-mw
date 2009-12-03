8d2d75212ca0ad7650ec1a75bcca31eaa68e337d
Frederik Vernelen <frederik.vernelen@fedict.be>
2009-12-03 14:54:09 +0000

    bugfix in the mac quickinstaller (acr install package name was wrong for 10.6)

--- eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
+++ eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
@@ -9,8 +9,9 @@ if [[ $ver == *10.4* ]]; then
 	/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage_10.4.mpkg -target /
 else if [[ $ver == *10.5* ]]; then
 		/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage_10.5.mpkg -target /
-	else
-		/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage.mpkg_10.6.mpkg -target /
+	else if [[ $ver == *10.6* ]]; then
+		/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage_10.6.mpkg -target /
+		fi
 	fi
 fi
 
