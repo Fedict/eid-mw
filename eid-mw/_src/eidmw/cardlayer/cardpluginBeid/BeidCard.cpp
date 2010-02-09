58e5b2fd58e8c7915567d9690a3f8cc4c01f2589
Frederik Vernelen <frederik.vernelen@fedict.be>
2010-02-09 13:57:25 +0000

    disabled filecaching

--- eid-mw/_src/eidmw/cardlayer/cardpluginBeid/BeidCard.cpp
+++ eid-mw/_src/eidmw/cardlayer/cardpluginBeid/BeidCard.cpp
@@ -631,7 +631,7 @@ tCacheInfo CBeidCard::GetCacheInfo(const std::string &csPath)
 	tCacheInfo certCache = {CERT_CACHE, 0};
 	tCacheInfo check16Cache = {CHECK_16_CACHE, 0}; // Check 16 bytes at offset 0
 	tCacheInfo checkSerial = {CHECK_SERIAL, 0}; // Check if the card serial nr is present
-
+/*
 	// csPath -> file ID
 	unsigned int uiFileID = 0;
 	unsigned long ulLen = (unsigned long) (csPath.size() / 2);
@@ -667,6 +667,6 @@ tCacheInfo CBeidCard::GetCacheInfo(const std::string &csPath)
 	case 0x503B: // EF(Cert#6) (root)
 		return certCache;
 	}
-
+*/
 	return dontCache;
 }
