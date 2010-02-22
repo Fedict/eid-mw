8ed295e7ba2467a33fed76e000e11014db14b2a4
Frank Mariën <frank.marien@fedict.be>
2010-02-22 10:49:11 +0000

    new files for #515 #516 #517

---
+++ eid-mw/_src/eDT/DiagEngine/AnalyseMeta.h
@@ -0,0 +1,152 @@
+/* ****************************************************************************
+
+ * eID Middleware Project.
+ * Copyright (C) 2008-2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see
+ * http://www.gnu.org/licenses/.
+
+**************************************************************************** */
+#ifndef _ANALYSEMETA_H_
+#define _ANALYSEMETA_H_
+
+#include "analysis.h"
+#include "Repository.h"
+
+class AnalyseMeta : public Analysis
+{
+public:
+	AnalyseMeta()
+	{
+		m_testName = "diagnostics";
+		m_friendlyName = "Diagnostics";
+	}
+
+	virtual ~AnalyseMeta()
+	{
+	}
+
+	virtual int run()
+	{
+		int retVal = DIAGLIB_OK;
+		m_bPassed = true;
+		setProgress(0);
+		setStartTime();
+		Report_TYPE reportType = REPORT_TYPE_RESULT;
+
+		try
+		{
+			progressInit(1);
+			reportPrintHeader2(reportType, L"Meta Diagnostics",L'#');
+			ContributionMap contributions=REP_RESULTS();
+			for(ContributionMap::const_iterator i=contributions.begin();i!=contributions.end();i++)
+			{
+				std::wstring	contribStr(i->first);
+								contribStr.append(L"=");
+								contribStr.append(i->second);
+				commentToReport(reportType,contribStr);
+			}
+				
+			progressIncrement();
+		}
+		//------------------------------------------
+		// exception from writing to the report
+		//------------------------------------------
+		catch (ExcReport& exc) 
+		{
+			processParamsToStop();
+			retVal = exc.getErr();
+		}
+		resultToReport(reportType,m_bPassed);
+		progressRelease();
+		return retVal;
+	}
+};
+
+#endif
+/* ****************************************************************************
+
+ * eID Middleware Project.
+ * Copyright (C) 2008-2010 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see
+ * http://www.gnu.org/licenses/.
+
+**************************************************************************** */
+#ifndef _ANALYSEMETA_H_
+#define _ANALYSEMETA_H_
+
+#include "analysis.h"
+#include "Repository.h"
+
+class AnalyseMeta : public Analysis
+{
+public:
+	AnalyseMeta()
+	{
+		m_testName = "diagnostics";
+		m_friendlyName = "Diagnostics";
+	}
+
+	virtual ~AnalyseMeta()
+	{
+	}
+
+	virtual int run()
+	{
+		int retVal = DIAGLIB_OK;
+		m_bPassed = true;
+		setProgress(0);
+		setStartTime();
+		Report_TYPE reportType = REPORT_TYPE_RESULT;
+
+		try
+		{
+			progressInit(1);
+			reportPrintHeader2(reportType, L"Meta Diagnostics",L'#');
+			ContributionMap contributions=REP_RESULTS();
+			for(ContributionMap::const_iterator i=contributions.begin();i!=contributions.end();i++)
+			{
+				std::wstring	contribStr(i->first);
+								contribStr.append(L"=");
+								contribStr.append(i->second);
+				commentToReport(reportType,contribStr);
+			}
+				
+			progressIncrement();
+		}
+		//------------------------------------------
+		// exception from writing to the report
+		//------------------------------------------
+		catch (ExcReport& exc) 
+		{
+			processParamsToStop();
+			retVal = exc.getErr();
+		}
+		resultToReport(reportType,m_bPassed);
+		progressRelease();
+		return retVal;
+	}
+};
+
+#endif
