f8ad10f15ce493e908d79ce53209cb977f3a655f
Frederik Vernelen <frederik.vernelen@fedict.be>
2010-02-23 10:21:21 +0000

    updated the download-install-xerces.sh for mac from xerces v 2.8 to xerces v 3.1

--- eid-mw/macos/download-install-xerces.sh
+++ eid-mw/macos/download-install-xerces.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
-if [[ ! -e "/usr/local/lib/libxerces-c.28.0.dylib" ]]
+if [[ ! -e "/usr/local/lib/libxerces-c-3.1.dylib" ]]
 then
-	mkdir -p ../_src/ThirdParty/Xerces/Xerces-2.8.0-mac/include
-	mkdir -p ../_src/ThirdParty/Xerces/Xerces-2.8.0-mac/lib
-	cd ../_src/ThirdParty/Xerces/Xerces-2.8.0-mac
+	mkdir -p ../_src/ThirdParty/Xerces/Xerces-3.1.0-mac/include
+	mkdir -p ../_src/ThirdParty/Xerces/Xerces-3.1.0-mac/lib
+	cd ../_src/ThirdParty/Xerces/Xerces-3.1.0-mac
 
 	# Download xerces
-	ftp http://apache.mogo.be/xerces/c/2/binaries/xerces-c_2_8_0-x86-macosx-gcc_4_0.tar.gz
-	ftp http://apache.mogo.be/xerces/c/2/binaries/xerces-c_2_8_0-powerpc-macosx-gcc_4_0.tar.gz
+	ftp http://apache.mogo.be/xerces/c/3/binaries/xerces-c-3.1.0-x86-macosx-gcc-4.0.tar.gz
 
 	# Unzip tarballs
-	tar -xzf xerces-c_2_8_0-x86-macosx-gcc_4_0.tar.gz
-	tar -xzf xerces-c_2_8_0-powerpc-macosx-gcc_4_0.tar.gz
+	tar -xzf xerces-c-3.1.0-x86-macosx-gcc-4.0.tar.gz
 
-	# Make a universal binary
-	lipo xerces-c_2_8_0-powerpc-macosx-gcc_4_0/lib/libxerces-c.28.0.dylib xerces-c_2_8_0-x86-macosx-gcc_4_0/lib/libxerces-c.28.0.dylib -output lib/libxerces-c.28.0.dylib -create
+	# Copy include and lib
+	cp -R xerces-c-3.1.0-x86-macosx-gcc-4.0/include/* include
+	cp -R xerces-c-3.1.0-x86-macosx-gcc-4.0/lib/* lib
+	sudo rm -R xerces-c-3.1.0-x86-macosx-gcc-4.0/
 
 	# Install in /usr/local/lib
 	sudo mkdir -p /usr/local/lib
-	sudo cp lib/libxerces-c.28.0.dylib /usr/local/lib/
-	sudo ln -s /usr/local/lib/libxerces-c.28.0.dylib /usr/local/lib/libxerces-c.28.dylib
+	sudo cp lib/libxerces-c-3.1.dylib /usr/local/lib/
+	sudo ln -s /usr/local/lib/libxerces-c.3.1.dylib /usr/local/lib/libxerces-c.3.dylib
 
-	cp -R xerces-c_2_8_0-x86-macosx-gcc_4_0/include/* include
 	cd -
 
 fi
