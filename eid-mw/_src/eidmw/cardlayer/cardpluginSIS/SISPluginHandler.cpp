e0aff6df789688e0f9af88fef4e88d428ed68976
Frederik Vernelen <frederik.vernelen@fedict.be>
2010-04-21 15:01:45 +0000

    name of the SIScardplugin is linked to the cardreader that needs to be used, for ACS ACR38U however, we replaced the space by an underscore, so need to take that into account

--- eid-mw/_src/eidmw/cardlayer/cardpluginSIS/SISPluginHandler.cpp
+++ eid-mw/_src/eidmw/cardlayer/cardpluginSIS/SISPluginHandler.cpp
@@ -187,7 +187,10 @@ static void AddPluginName(const char *csPluginName, const std::string & csPath)
 			char csReaderName[200];
 			memcpy(csReaderName, ptr1, ptr2 - ptr1);
 			csReaderName[ptr2 - ptr1] = '\0';
-
+			if (memcmp(csReaderName, "ACS_ACR38U", sizeof("ACS_ACR38U"))==0)
+			{
+				memcpy(csReaderName, "ACS ACR38U", sizeof("ACS ACR38U"));
+			}
 			AddPluginInfo(csPath, csReaderName);
 		}
 	}
