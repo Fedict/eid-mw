ceeb071f55e938cd2179ae25aa53c21d66826a89
Frank Mariën <frank.marien@fedict.be>
2010-06-04 16:05:57 +0000

    fixed old copyright message in .c sources

--- test/pkcs11test/src/sign.c
+++ test/pkcs11test/src/sign.c
@@ -1,6 +1,6 @@
 /*
  * eID Middleware Project.
- * Copyright (C) 2008-2009 FedICT.
+ * Copyright (C) 2008-2010 FedICT.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version
