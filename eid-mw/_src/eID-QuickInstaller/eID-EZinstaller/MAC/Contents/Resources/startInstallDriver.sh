843d2c375fd9752d6af4f86f817722d42fd8981b
Frederik Vernelen <frederik.vernelen@fedict.be>
2010-07-06 13:29:45 +0000

    bugfix in mac installdriverscript, updated mac preparepcscd script

--- eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
+++ eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
@@ -5,13 +5,11 @@ IFS=$'\n' ver=($(sw_vers))
 ver=(${ver[1]})
 
 if [[ $ver == *10.5* ]]; then
-		/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage_10.5.mpkg -target
-        /usr/sbin/installer -pkg $DIRNAME/libusb.pkg -target
-        /usr/sbin/installer -pkg $DIRNAME/ifd_ccid.pkg -target
+		/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage_10.5.mpkg -target /
+        /usr/sbin/installer -pkg $DIRNAME/libusb.pkg -target /
+        /usr/sbin/installer -pkg $DIRNAME/ifd_ccid.pkg -target /
 	else if [[ $ver == *10.6* ]]; then
-		/usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_10.6.mpkg -target
-        /usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.6.mpkg -target
+		/usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_10.6.mpkg -target /
+        /usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.6.mpkg -target /
 	fi
 fi
-
-
