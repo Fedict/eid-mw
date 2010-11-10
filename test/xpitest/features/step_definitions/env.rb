8f673d8a74aaee4da076f0d8587864df8a414b92
Koen De Causmaecker <koen.de.causmaecker@fedict.be>
2010-11-10 15:37:21 +0000

    move to rspec 2.1

--- test/xpitest/features/step_definitions/env.rb
+++ test/xpitest/features/step_definitions/env.rb
@@ -1,13 +1,10 @@
-require 'spec'
+require 'rspec-expectations'
 require 'firewatir'
 require 'cucumber/formatter/unicode'
 require 'smartcard'
 require 'win32api'
 
 Before do
-  include Spec::Matchers 
-  
- # Watir::Browser.default = "firefox"
    @browser ||= FireWatir::Firefox.new(:profile => "xpitest", :waitTime => 3)  
 end
 
