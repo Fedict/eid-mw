5474e348c8063de8dfccefaf99c338b32479c3fc
Koen De Causmaecker <koen.de.causmaecker@fedict.be>
2010-12-02 13:01:44 +0000

    Removed specific firefox profile

--- test/xpitest/features/step_definitions/env.rb
+++ test/xpitest/features/step_definitions/env.rb
@@ -5,7 +5,8 @@ require 'smartcard'
 require 'win32api'
 
 Before do
-   @browser ||= FireWatir::Firefox.new(:profile => "xpitest", :waitTime => 3)  
+  # @browser ||= FireWatir::Firefox.new(:profile => "xpitest", :waitTime => 3)  
+   @browser ||= FireWatir::Firefox.new(:waitTime => 3)  
 end
 
 After do
