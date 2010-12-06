721deafd88a7b99999d6d7f6b2702ad7ca62c9c1
Frederik Vernelen <frederik.vernelen@fedict.be>
2010-12-06 13:23:57 +0000

    added 32-bit installer of the SDK

---
+++ installers/sdk/Windows/candle_light.cmd
@@ -0,0 +1,45 @@
+:: Check WiX
+:: =========
+@call "%~dp0..\..\SetPathWix.bat"
+@if %ERRORLEVEL%==0 goto find_wix
+
+@echo [TIP] Install ProjectAggregator2.msi and Wix3-3.0.4415.msi
+@goto end
+
+:find_wix
+@echo [INFO] Using BEID_DIR_WIX=%BEID_DIR_WIX%
+
+
+@echo [INFO] Making the nl-nl version
+
+"%BEID_DIR_WIX%\bin\candle.exe" -v -dLang=nl -out obj\Product.wixobj -arch x86 -ext "%BEID_DIR_WIX%\bin\WixDifxAppExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUtilExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUIExtension.dll" Product.wxs
+
+"%BEID_DIR_WIX%\bin\Light.exe" -v -cultures:nl-nl -ext "%BEID_DIR_WIX%\bin\WixDifxAppExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUtilExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUIExtension.dll" -loc Strings-nl.wxl -loc WixUI_nl-nl.wxl -out bin\nl-nl\BeidMW35SDK-Basic-nl.msi -pdbout bin\nl-nl\BeidMW35SDK-Basic-nl.wixpdb -sice:ICE09 obj\Product.wixobj "%BEID_DIR_WIX%\bin\difxapp_x86.wixlib"
+
+
+
+@echo [INFO] Making the fr-fr version
+
+"%BEID_DIR_WIX%\bin\candle.exe" -v -dLang=fr -out obj\Product.wixobj -arch x86 -ext "%BEID_DIR_WIX%\bin\WixDifxAppExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUtilExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUIExtension.dll" Product.wxs
+
+"%BEID_DIR_WIX%\bin\Light.exe" -v -cultures:fr-fr -ext "%BEID_DIR_WIX%\bin\WixDifxAppExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUtilExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUIExtension.dll" -loc Strings-fr.wxl -out bin\fr-fr\BeidMW35SDK-Basic-fr.msi -pdbout bin\fr-fr\BeidMW35SDK-Basic-fr.wixpdb -sice:ICE09 obj\Product.wixobj "%BEID_DIR_WIX%\bin\difxapp_x86.wixlib"
+
+
+
+@echo [INFO] Making the de-de version
+
+"%BEID_DIR_WIX%\bin\candle.exe" -v -dLang=de -out obj\Product.wixobj -arch x86 -ext "%BEID_DIR_WIX%\bin\WixDifxAppExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUtilExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUIExtension.dll" Product.wxs
+
+"%BEID_DIR_WIX%\bin\Light.exe" -v -cultures:de-de -ext "%BEID_DIR_WIX%\bin\WixDifxAppExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUtilExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUIExtension.dll" -loc Strings-de.wxl -out bin\de-de\BeidMW35SDK-Basic-de.msi -pdbout bin\de-de\BeidMW35SDK-Basic-de.wixpdb -sice:ICE09 obj\Product.wixobj "%BEID_DIR_WIX%\bin\difxapp_x86.wixlib"
+
+
+
+@echo [INFO] Making the en-us version
+
+"%BEID_DIR_WIX%\bin\candle.exe" -v -dLang=en -out obj\Product.wixobj -arch x86 -ext "%BEID_DIR_WIX%\bin\WixDifxAppExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUtilExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUIExtension.dll" Product.wxs
+
+"%BEID_DIR_WIX%\bin\Light.exe" -v -cultures:en-us -ext "%BEID_DIR_WIX%\bin\WixDifxAppExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUtilExtension.dll" -ext "%BEID_DIR_WIX%\bin\WixUIExtension.dll" -loc Strings-en.wxl -out bin\en-us\BeidMW35SDK-Basic-en.msi -pdbout bin\en-us\BeidMW35SDK-Basic-en.wixpdb -sice:ICE09 obj\Product.wixobj "%BEID_DIR_WIX%\bin\difxapp_x86.wixlib"
+
+@pause
+
+@call "%~dp0multi-lang.cmd"
