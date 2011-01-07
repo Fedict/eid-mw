26d461dabd5c621923a72fb751bd9685c4bceb39
Frederik Vernelen <frederik.vernelen@fedict.be>
2011-01-07 15:36:32 +0000

    Initial version of the 4.0viewer msi installer build system

---
+++ installers/eid-viewer/Windows/heat.cmd
@@ -0,0 +1,35 @@
+:: Check WiX
+:: =========
+@call "%~dp0..\..\..\windows\SetPaths.bat"
+@if %ERRORLEVEL%==1 goto end
+
+
+:: Create the revision number
+:: ==========================
+set SVNREvision=
+svn info || echo | FindStr /L Revision > svn_info.txt
+
+for /f "tokens=2" %%T in (svn_info.txt) do set SVNRevision=%%T
+
+if not "%SVNRevision%"=="" goto skiptarfile
+Set /P SVNRevision=< ..\..\..\svn_revision                                                      
+
+:skiptarfile
+Set /A SVNRevision+=6000
+
+echo ^<Include^>>svn_revision.wxs
+echo ^<?define RevisionNumber=%SVNRevision%?^>>>svn_revision.wxs
+echo ^</Include^>>>svn_revision.wxs
+
+
+:: create the lib files' identifiers
+:: ==================================
+::heat dir path -srd -dr MyDirName -cg SampleGroup -out SampleGroup.wxs
+
+::"%BEID_DIR_WIX%\bin\heat"
+"%BEID_DIR_WIX%\bin\heat" dir ..\..\..\eid-viewer\eid-viewer\lib -srd -dr BEIDVIEWERLIBDIR -cg BEIDVIEWERLIB -gg -out libfiles.wxs
+
+
+echo [INFO] Done creating lib files' identifiers
+:END
+echo end
