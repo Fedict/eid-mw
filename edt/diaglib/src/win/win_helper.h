a03445ada41ede0dd60c8759ff2d27493c262965
Frank Mariën <frank.marien@fedict.be>
2011-02-10 15:08:18 +0000

    removed eid-viewer from configure while figuring out the distcheck problem removed edt module

--- edt/diaglib/src/win/win_helper.h
+++
@@ -1,3 +0,0 @@
-int strcpy_s(char *dest, size_t len, const char *src);
-int wcscpy_s(wchar_t *dest, size_t len, const wchar_t *src);
-
--- edt/tests/test_vasprintf.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "secure_helper.h"
-
-int main(int argc, char* argv[])
-{
-	fprintf(STDERR,"%d written : [%s]\n",fwprintf(STDOUT,"hello %s world\n","beautiful"));
-}
-
