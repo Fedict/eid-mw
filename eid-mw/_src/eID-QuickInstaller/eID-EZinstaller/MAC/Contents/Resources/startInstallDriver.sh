d3962fd7378ed985fbb6c08294e9f8cd9bd99f00
Frederik Vernelen <frederik.vernelen@fedict.be>
2011-05-17 08:41:06 +0000

    mac QI: added acr38 driver version 1.7.11

--- eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
+++ eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
@@ -1,21 +1,14 @@
 #!/bin/sh
 DIRNAME=`dirname $0`
 
-IFS=$'\n' ver=($(sw_vers))
-ver=(${ver[1]})
+IFS=$'\n' ver=($(sw_vers -productVersion))
 
-if [[ $ver == *10.5.0* || $ver == *10.5.1* || $ver == *10.5.2* || $ver == *10.5.3* || $ver == *10.5.4* || $ver == *10.5.5* ]]; then
+if [[ ($ver > "10.4.8") && ($ver < "10.5.6") ]]; then
 		/usr/sbin/installer -pkg $DIRNAME/ACR38DriverPackage_10.5.mpkg -target /
         /usr/sbin/installer -pkg $DIRNAME/libusb.pkg -target /
         /usr/sbin/installer -pkg $DIRNAME/ifd_ccid.pkg -target /
-    else if [[ $ver == *10.5* ]]; then
-		/usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_10.5.mpkg -target /
-		launchctl unload -w /Library/LaunchDaemons/org.opensc.pcscd.autostart /
-		rm -f /Library/LaunchDaemons/org.opensc.pcscd.autostart /
-		/Library/OpenSC/bin/sc-securityd.py active /
-        else if [[ $ver == *10.6* ]]; then
-            /usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_10.6.mpkg -target /
+    else if [[ $ver == *10.5* || $ver == *10.6* ]]; then
+            /usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11.mpkg -target /
             /usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.6.mpkg -target /
-        fi
 	fi
 fi
