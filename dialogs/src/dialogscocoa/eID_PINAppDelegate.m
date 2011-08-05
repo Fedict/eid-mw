f3c85bdfdf6114d16dbc27cdd19360c1b92a9674
Frank Mariën <frank.marien@fedict.be>
2011-08-05 10:54:01 +0000

    added cocoa dialogs (only PIN request implemented)

---
+++ dialogs/src/dialogscocoa/eID_PINAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  eID_PINAppDelegate.m
+//  eID-PIN
+//
+//  Created by Frank Mariën on 02/08/11.
+//  Copyright 2011 FedICT. All rights reserved.
+//
+
+#import "eID_PINAppDelegate.h"
+
+@implementation eID_PINAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
