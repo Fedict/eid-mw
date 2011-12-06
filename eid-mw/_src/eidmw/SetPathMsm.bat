65891d5ab9da357d7bf1e1a7a3128ef53435346c
Frederik Vernelen <frederik.vernelen@fedict.be>
2011-12-06 08:56:26 +0000

    changed default msm path for 32 bit wxs

--- eid-mw/_src/eidmw/SetPathMsm.bat
+++ eid-mw/_src/eidmw/SetPathMsm.bat
@@ -8,14 +8,14 @@
 @if "%FILE_NOT_FOUND%"=="" goto find_msm
 @echo        Not found in "%BEID_DIR_MSM%"
 
-@set BEID_DIR_MSM=%~dp0..\ThirdParty\Wix\Merge Modules
+@set BEID_DIR_MSM="C:\Program Files\Common Files\Merge Modules"
 @set FILE_NOT_FOUND=
 @for %%i in (%FILE_TO_FIND%) do @if not exist "%BEID_DIR_MSM%\%%~i" set FILE_NOT_FOUND=%%~i
 @if "%FILE_NOT_FOUND%"=="" goto find_msm
 @echo        Not found in "%BEID_DIR_MSM%"
 
 @echo [ERROR] Merge module (msm) could not be found
-@echo         If the path is different from "%~dp0..\ThirdParty\Wix\Merge Modules"
+@echo         If the path is different from "C:\Program Files\Common Files\Merge Modules"
 @echo         please define BEID_DIR_MSM environment variable.
 @exit /B 1
 
