617f1b0ec6c3449691a56288ca1485ff5e04d76c
Frederik Vernelen <frederik.vernelen@fedict.be>
2012-02-10 15:29:33 +0000

    mac: updated releasenumber to 4.0.1

--- eid-mw/macos/make-mac.sh
+++ eid-mw/macos/make-mac.sh
@@ -3,7 +3,7 @@
 # java 1.4.2 is not supported any more since oct 2008
 MINJAVAVER=1.5
 MACHINETYPE=i386
-MWVER=4.0.0
+MWVER=4.0.1
 
 #########################################
 # we must be running on intel
