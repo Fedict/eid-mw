49c1c874675af383aa92d166ce727969b84b1561
Frederik Vernelen <frederik.vernelen@fedict.be>
2012-02-21 12:49:27 +0000

    added commandline option "-c" for the beidsccertprop.exe, when used, the application exits when a beidsccertprop.exe is already running

--- eid-mw/_src/eidmw/sccertprop/certprop.h
+++ eid-mw/_src/eidmw/sccertprop/certprop.h
@@ -1,7 +1,7 @@
 /* ****************************************************************************
 
  * eID Middleware Project.
- * Copyright (C) 2011 FedICT.
+ * Copyright (C) 2011-2012 FedICT.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version
--- eid-mw/_src/eidmw/sccertprop/main.cpp
+++ eid-mw/_src/eidmw/sccertprop/main.cpp
@@ -1,7 +1,7 @@
 /* ****************************************************************************
 
  * eID Middleware Project.
- * Copyright (C) 2011 FedICT.
+ * Copyright (C) 2011-2012 FedICT.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License version
@@ -19,9 +19,43 @@
 **************************************************************************** */
 
 #include "certprop.h"
+#include "runcheck.h"
 
 HANDLE                  ghSvcStopEvent = NULL;
 
 int WINAPI WinMain(HINSTANCE inst,HINSTANCE prev,LPSTR cmd,int show) {
+
+	WCHAR* AppName=TEXT("beidsccertprop.exe");
+	DWORD dwAppsRunning = 0; 
+	char* cmdptr = cmd;
+
+	while( *cmdptr != 0)
+	{
+		if(*cmdptr == '-')
+		{
+			cmdptr++;
+			switch(*cmdptr)
+			{
+			case 'c':
+				if( CheckAppRunning(AppName, &dwAppsRunning) == CHECK_OK)
+				{
+					if(dwAppsRunning > 1)
+					{
+						//we exist already more then once, so we exit
+						return 0;
+					}
+				}
+				break;
+			default:
+				break;
+			}
+			if(*cmdptr == 0)
+			{
+				break;
+			}
+		}
+		cmdptr++;
+	}
+
 	return CertProp();
 }
