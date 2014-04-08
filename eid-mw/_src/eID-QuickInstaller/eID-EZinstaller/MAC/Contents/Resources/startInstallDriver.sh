6db3ad1ded05e35e6e890d1af4c14f67e649aa30
Frederik Vernelen <frederik.vernelen@fedict.be>
2014-04-08 14:49:56 +0000

    install acr38u driver with pcsc autostart package on OS X 10.9

--- eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
+++ eid-mw/_src/eID-QuickInstaller/eID-EZinstaller/MAC/Contents/Resources/startInstallDriver.sh
@@ -14,8 +14,11 @@ if [[ ($ver == *10.5*) && ($ver < "10.5.6") ]]; then
             /usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11.mpkg -target /
             /usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.6.mpkg -target /
 		else if [[ $ver == *10.7* || $ver == *10.8* ]]; then
-				/usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11_10.7.mpkg -target /
-				/usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.7.mpkg -target /
+			/usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11_10.7.mpkg -target /
+			/usr/sbin/installer -pkg $DIRNAME/acsccid_installer_10.7.mpkg -target /
+				else if [[ $ver == *10.9* ]]; then
+					/usr/sbin/installer -pkg $DIRNAME/acr38driver_installer_1.7.11_10.7.mpkg -target /
+				fi
 		fi
 	fi
 fi
