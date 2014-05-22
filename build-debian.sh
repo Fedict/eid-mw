efe81167ae69d18db8abc87a7ae5d82631e840f6
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-05-22 11:25:08 +0000

    Add the distribution name to the version number. We'll build packages on individual distributions from now on.

--- build-debian.sh
+++ build-debian.sh
@@ -9,9 +9,13 @@
 SVNREV=$(cat svn_revision)
 # Upstream
 MAINVER=$(./configure --version|grep ^eid-mw|cut -d' ' -f3)
+# Distribution name
+DIST=$(lsb_release -c -s)
 
-# Generate changelog entry with correct version number
-dch -v ${MAINVER}r${SVNREV} "Snapshot build"
+# Generate changelog entry with correct version number, suppressing a possible
+# warning from dch that the distribution name may or may not be valid.
+# This would be true for an official upload, but this isn't that.
+yes | dch -v ${MAINVER}r${SVNREV}-0${DIST}1 -D${DIST} "Snapshot build"
 
 # Build
 make distclean
