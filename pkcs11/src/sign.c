52b2d1ea5466e7b0ebec094ac34dae58dfd841f0
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-07-23 11:38:37 +0000

    Add another missing "goto cleanup"
    Coverity CID#18490

--- pkcs11/src/sign.c
+++ pkcs11/src/sign.c
@@ -70,6 +70,7 @@ CK_RV C_DigestInit(CK_SESSION_HANDLE hSession,   /* the session's handle */
          {
          log_trace( WHERE, "E: error allocating memory");
          ret = CKR_HOST_MEMORY;
+	 goto cleanup;
          }
       }
 
