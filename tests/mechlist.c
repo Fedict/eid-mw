eb8414e06de72e4940ee9a8f019e229096fa510d
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-08-07 09:20:28 +0000

    Make it test a few more things

--- tests/mechlist.c
+++ tests/mechlist.c
@@ -5,12 +5,15 @@
 
 #include "testlib.h"
 
+#define HAS_CKM(ckm, crit) case ckm: { printf("Found " #ckm "\n"); known_mechs++; if(crit) crit_mechs++; } break;
+
 int mechlist(void) {
 	CK_RV rv;
 	CK_SLOT_ID_PTR slotlist;
 	CK_ULONG count=0;
-	CK_TOKEN_INFO info;
 	CK_MECHANISM_TYPE_PTR mechlist;
+	int known_mechs = 0;
+	int crit_mechs = 0;
 	int i;
 
 	rv = C_Initialize(NULL_PTR);
@@ -35,9 +38,44 @@ int mechlist(void) {
 
 	rv = C_GetMechanismList(slotlist[0], mechlist, &count);
 	check_rv;
-	for(i=0; i<count; i++) {
-		printf("mechanism %2d: 0x%08lx\n", i, mechlist[i]);
+	if(count == 0) {
+		printf("Token supports no mechanisms!\n");
+		return TEST_RV_FAIL;
 	}
+
+	for(i=0; i<count; i++) {
+		switch(mechlist[i]) {
+		HAS_CKM(CKM_RSA_PKCS, 1);
+		HAS_CKM(CKM_RIPEMD160, 0);
+		HAS_CKM(CKM_MD5, 0);
+		HAS_CKM(CKM_SHA_1, 1);
+		HAS_CKM(CKM_SHA256, 1);
+		HAS_CKM(CKM_SHA384, 0);
+		HAS_CKM(CKM_SHA512, 0);
+		HAS_CKM(CKM_RIPEMD160_RSA_PKCS, 0);
+		HAS_CKM(CKM_MD5_RSA_PKCS, 0);
+		HAS_CKM(CKM_SHA1_RSA_PKCS, 1);
+		HAS_CKM(CKM_SHA256_RSA_PKCS, 1);
+		HAS_CKM(CKM_SHA384_RSA_PKCS, 0);
+		HAS_CKM(CKM_SHA512_RSA_PKCS, 0);
+		default:
+			printf("Found unknown mechanism 0x%08x\n", mechlist[i]);
+			break;
+		}
+	}
+
+	if(count == known_mechs) {
+		printf("INFO: no unknown mechanisms found\n");
+	} else {
+		if(count > known_mechs) {
+			printf("INFO: %lu unknown mechanisms found\n", count - known_mechs);
+		} else {
+			fprintf(stderr, "E: something weird happened");
+			return TEST_RV_FAIL;
+		}
+	}
+
+	verbose_assert(crit_mechs == 5);
 
 	rv = C_Finalize(NULL_PTR);
 	check_rv;
