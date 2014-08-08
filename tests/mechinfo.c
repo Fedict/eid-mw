ba5b1911caaa2d979973c5e8435c7bc368f0a71d
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-08-08 14:00:50 +0000

    Check properties of all mechanisms, not just RSA. Also, check that RSA key size is 2048 on 2K cards (which is currently not the case)

--- tests/mechinfo.c
+++ tests/mechinfo.c
@@ -5,6 +5,8 @@
 
 #include "testlib.h"
 
+#define ADD_CKM(ckm) ckms[ckm] = #ckm
+
 int mechinfo(void) {
 	CK_RV rv;
 	CK_SLOT_ID slot;
@@ -13,6 +15,24 @@ int mechinfo(void) {
 	CK_MECHANISM_TYPE_PTR mechlist;
 	int i, ret;
 	int found = 0;
+	int card_2k = 0;
+	char* ckms[0x2002];
+
+	ADD_CKM(CKM_RSA_PKCS);
+	ADD_CKM(CKM_RIPEMD160);
+	ADD_CKM(CKM_MD5);
+	ADD_CKM(CKM_SHA_1);
+	ADD_CKM(CKM_SHA256);
+	ADD_CKM(CKM_SHA384);
+	ADD_CKM(CKM_SHA512);
+        ADD_CKM(CKM_RIPEMD160_RSA_PKCS);
+        ADD_CKM(CKM_MD5_RSA_PKCS);
+        ADD_CKM(CKM_SHA1_RSA_PKCS);
+        ADD_CKM(CKM_SHA1_RSA_PKCS_PSS);
+        ADD_CKM(CKM_SHA256_RSA_PKCS);
+        ADD_CKM(CKM_SHA256_RSA_PKCS_PSS);
+        ADD_CKM(CKM_SHA384_RSA_PKCS);
+        ADD_CKM(CKM_SHA512_RSA_PKCS);
 
 	rv = C_Initialize(NULL_PTR);
 	check_rv;
@@ -28,27 +48,43 @@ int mechinfo(void) {
 	rv = C_GetMechanismList(slot, mechlist, &count);
 	check_rv;
 
+	switch(count) {
+		case 13:
+			printf("Found 1K card\n");
+			break;
+		case 15:
+			printf("Found 2K card\n");
+			card_2k = 1;
+			break;
+	}
+
 	for(i=0; i<count; i++) {
+		printf("Mechanism %#08lx (%s):\n", mechlist[i], ckms[mechlist[i]]);
+
+		rv = C_GetMechanismInfo(slot, mechlist[i], &info);
+		check_rv;
+
+		printf("minimum key length: %lu\n", info.ulMinKeySize);
+		printf("maximum key length: %lu\n", info.ulMaxKeySize);
+		printf("flags: %#08lx\n", info.flags);
+		if(mechlist[i] != CKM_SHA1_RSA_PKCS_PSS && mechlist[i] != CKM_SHA256_RSA_PKCS_PSS) {
+			verbose_assert(info.ulMinKeySize == info.ulMaxKeySize);
+		}
 		if(mechlist[i] == CKM_RSA_PKCS) {
-			found=1;
+			if(card_2k) {
+				verbose_assert(info.ulMaxKeySize == 2048);
+			} else {
+				verbose_assert(info.ulMaxKeySize == 1024);
+			}
+			verbose_assert(info.flags & CKF_HW);
+			verbose_assert(info.flags & CKF_SIGN);
+			verbose_assert((info.flags & ~CKF_HW & ~CKF_SIGN) == 0);
 		}
 	}
 	if(!found) {
 		printf("Couldn't find PKCS RSA mechanism on this token\n");
 		return TEST_RV_SKIP;
 	}
-
-	rv = C_GetMechanismInfo(slot, CKM_RSA_PKCS, &info);
-	check_rv;
-
-	printf("RSA minimum key length: %lu\n", info.ulMinKeySize);
-	printf("RSA maximum key length: %lu\n", info.ulMaxKeySize);
-	verbose_assert(info.ulMinKeySize == info.ulMaxKeySize);
-	verbose_assert(info.ulMinKeySize == 1024 || info.ulMaxKeySize == 2048);
-	printf("RSA flags: %#08lx\n", info.flags);
-	verbose_assert(info.flags & CKF_HW);
-	verbose_assert(info.flags & CKF_SIGN);
-	verbose_assert((info.flags & ~CKF_HW & ~CKF_SIGN) == 0);
 
 	rv = C_Finalize(NULL_PTR);
 	check_rv;
