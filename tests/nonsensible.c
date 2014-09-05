fe18fecb1fe9a49f91211429a26d548149d32b55
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-09-05 13:00:24 +0000

    Grep source tree for CKR_FUNCTION_NOT_SUPPORTED, and add them all in one go.

--- tests/nonsensible.c
+++ tests/nonsensible.c
@@ -11,7 +11,7 @@ TEST_FUNC(nonsensible) {
 	CK_SESSION_HANDLE session;
 	CK_ULONG data;
 	CK_MECHANISM mech;
-	CK_ATTRIBUTE temp;
+	CK_ATTRIBUTE attr;
 	int ret;
 	ckrv_mod m[] = {
 		{ CKR_OK, TEST_RV_FAIL },
@@ -21,38 +21,44 @@ TEST_FUNC(nonsensible) {
 	check_rv(C_Initialize(NULL_PTR));
 
 	check_rv_long(C_InitToken(slot, "1111", 4, ""),m);
-
 	check_rv_long(C_InitPIN(slot, "1111", 4),m);
-
 	check_rv_long(C_CreateObject(session, NULL_PTR, 0, &object),m);
-
 	check_rv_long(C_CopyObject(session, object, NULL_PTR, 0, &object),m);
-
 	check_rv_long(C_DestroyObject(session, object),m);
-
 	check_rv_long(C_GetObjectSize(session, object, &data),m);
-
 	check_rv_long(C_EncryptInit(session, &mech, object),m);
-
 	check_rv_long(C_Encrypt(session, NULL_PTR, 0, NULL_PTR, &data),m);
-
 	check_rv_long(C_EncryptUpdate(session, NULL_PTR, 0, NULL_PTR, &data),m);
-
 	check_rv_long(C_EncryptFinal(session, NULL_PTR, 0),m);
-
 	check_rv_long(C_DecryptInit(session, &mech, object),m);
-
 	check_rv_long(C_Decrypt(session, NULL_PTR, 0, NULL_PTR, &data),m);
-
 	check_rv_long(C_DecryptUpdate(session, NULL_PTR, 0, NULL_PTR, &data),m);
-
 	check_rv_long(C_DecryptFinal(session, NULL_PTR, 0),m);
-
+	check_rv_long(C_DigestEncryptUpdate(session, NULL_PTR, 0, NULL_PTR, &data),m);
+	check_rv_long(C_DecryptDigestUpdate(session, NULL_PTR, 0, NULL_PTR, &data),m);
+	check_rv_long(C_SignEncryptUpdate(session, NULL_PTR, 0, NULL_PTR, &data),m);
+	check_rv_long(C_DecryptVerifyUpdate(session, NULL_PTR, 0, NULL_PTR, &data),m);
 	check_rv_long(C_GetOperationState(session, NULL_PTR, &data), m);
-
-	check_rv_long(C_SetOperationState(session, NULL_PTR, &data, 0, 0), m);
-
-	check_rv_long(C_SetAttributeValue(session, object, &temp, 1), m);
+	check_rv_long(C_SetOperationState(session, NULL_PTR, data, 0, 0), m);
+	check_rv_long(C_SetAttributeValue(session, object, &attr, 1), m);
+	check_rv_long(C_GenerateKey(session, &mech, &attr, 1, &object), m);
+	check_rv_long(C_GenerateKeyPair(session, &mech, &attr, 1, &attr, 1, &object, &object), m);
+	check_rv_long(C_WrapKey(session, &mech, object, object, NULL_PTR, &data), m);
+	check_rv_long(C_UnwrapKey(session, &mech, object, NULL_PTR, data, &attr, data, &object), m);
+	check_rv_long(C_DeriveKey(session, &mech, object, &attr, data, &object), m);
+	check_rv_long(C_SeedRandom(session, NULL_PTR, data), m);
+	check_rv_long(C_GenerateRandom(session, NULL_PTR, data), m);
+	check_rv_long(C_GetFunctionStatus(session), m);
+	check_rv_long(C_CancelFunction(session), m);
+	check_rv_long(C_DigestKey(session, object), m);
+	check_rv_long(C_SignRecoverInit(session, &mech, object), m);
+	check_rv_long(C_SignRecover(session, NULL_PTR, data, NULL_PTR, &data), m);
+	check_rv_long(C_VerifyInit(session, &mech, object), m);
+	check_rv_long(C_Verify(session, NULL_PTR, data, NULL_PTR, data), m);
+	check_rv_long(C_VerifyUpdate(session, NULL_PTR, data), m);
+	check_rv_long(C_VerifyFinal(session, NULL_PTR, data), m);
+	check_rv_long(C_VerifyRecoverInit(session, &mech, object), m);
+	check_rv_long(C_VerifyRecover(session, NULL_PTR, data, NULL_PTR, &data), m);
 
 	check_rv(C_Finalize(NULL_PTR));
 
