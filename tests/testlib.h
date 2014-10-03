b07460820b906dba947ce5a74d38ce4111c7ac3e
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-10-03 08:55:01 +0000

    Remove bad idea

--- tests/testlib.h
+++ tests/testlib.h
@@ -58,34 +58,6 @@ void hex_dump(char* data, CK_ULONG length);
 /* Helper functions to not have to repeat common operations all the time */
 int find_slot(CK_BBOOL with_token, CK_SLOT_ID_PTR slot);
 
-typedef unsigned int testmask;
-
-/* masks for tests that exist */
-#define TEST_INIT_FINALIZE	1 << 0
-#define TEST_GETINFO 		1 << 1
-#define TEST_FUNCLIST		1 << 2
-#define TEST_SLOTLIST		1 << 3
-#define TEST_SLOTINFO		1 << 4
-#define TEST_TKINFO		1 << 5
-#define TEST_DOUBLE_INIT	1 << 6
-#define TEST_FORK_INIT		1 << 7
-#define TEST_SLOTEVENT		1 << 8
-#define TEST_MECHLIST		1 << 9
-#define TEST_MECHINFO		1 << 10
-#define TEST_SESSIONS		1 << 11
-#define TEST_SESSIONS_NOCARD	1 << 12
-#define TEST_SESSIONINFO	1 << 13
-#define TEST_LOGIN		1 << 14
-#define TEST_NONSENSIBLE	1 << 15
-#define TEST_OBJECTS		1 << 16
-#define TEST_READDATA		1 << 17
-#define TEST_DIGEST		1 << 18
-#define TEST_DECODEPHOTO	1 << 19
-
-#define TESTS_COUNT		20
-
-#define TESTS_ALL		((testmask)1 << TEST_COUNT) - 1
-#define FIRST_TEST		TEST_INIT_FINALIZE
 /* function definitions for tests that exist */
 int init_finalize();
 int getinfo();
@@ -107,20 +79,5 @@ int objects();
 int readdata();
 int digest();
 int decode_photo();
-
-/* Define the tests to run for architectures where tests aren't run in individual programs */
-#ifndef TESTS_TO_RUN
-#define TESTS_TO_RUN TESTS_ALL
-#endif
-
-/* Array of function pointers to the above */
-typedef int(*testptr_t)();
-testptr_t test_ptrs[TESTS_COUNT];
-
-/* initialize the above array */
-int init_tests();
-
-/* Check whether a test should be run */
-#define should_run_test(mask, which) (mask & (1 << which))
 
 #endif
