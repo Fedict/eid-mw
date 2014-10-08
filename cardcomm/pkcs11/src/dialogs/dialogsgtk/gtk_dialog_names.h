74e7022ecf014b239dd20c40c60dd36af40ffef2
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-10-08 16:19:44 +0000

    Add missing header file

---
+++ cardcomm/pkcs11/src/dialogs/dialogsgtk/gtk_dialog_names.h
@@ -0,0 +1,11 @@
+#ifndef DIALOGS_H
+#define DIALOGS_H
+
+#define BEID_ASKPIN_DIALOG LIBEXECDIR "/beid-askpin"
+#define BEID_CHANGEPIN_DIALOG LIBEXECDIR "/beid-changepin"
+#define BEID_BADPIN_DIALOG LIBEXECDIR "/beid-badpin"
+#define BEID_ASKACCESS_DIALOG LIBEXECDIR "/beid-askaccess"
+#define BEID_SPR_ASKPIN_DIALOG LIBEXECDIR "/beid-spr-askpin"
+#define BEID_SPR_CHANGEPIN_DIALOG LIBEXECDIR "/beid-spr-changepin"
+
+#endif
