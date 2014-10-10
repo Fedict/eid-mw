281323e1debe5c9c3dda28ff7cf43c88d42d1954
Frederik Vernelen <frederik.vernelen@fedict.be>
2014-10-10 11:24:45 +0000

    windows build script: updated paths (minidriver not yet part of build)

--- scripts/windows/build_all.bat
+++ scripts/windows/build_all.bat
@@ -8,26 +8,27 @@
 
 :: Create svn_revision.h
 :: =====================
-@call "%~dp0.\svn_revision.cmd"
+:: TODO: get g
+::@call "%~dp0.\svn_revision.cmd"
 
 :: build pkcs11, minidriver, cleanuptool and sccertprop
 :: ====================================================
-@echo [INFO] Building "%~dp0..\VS_2012\beid.sln"
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=Release /Property:Platform=Win32 "%~dp0..\VS_2012\beid.sln"
+@echo [INFO] Building "%~dp0..\..\VS_2012\beid.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=Release /Property:Platform=Win32 "%~dp0..\..\VS_2012\beid.sln"
 @if "%ERRORLEVEL%" == "1" goto msbuild_failed
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=Release /Property:Platform=Win32 "%~dp0..\VS_2012\beid.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=Release /Property:Platform=Win32 "%~dp0..\..\VS_2012\beid.sln"
 @if "%ERRORLEVEL%" == "1" goto msbuild_failed
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=PKCS11_FF_Release /Property:Platform=Win32 "%~dp0..\VS_2012\beid.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=PKCS11_FF_Release /Property:Platform=Win32 "%~dp0..\..\VS_2012\beid.sln"
 @if "%ERRORLEVEL%" == "1" goto msbuild_failed
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=PKCS11_FF_Release /Property:Platform=Win32 "%~dp0..\VS_2012\beid.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=PKCS11_FF_Release /Property:Platform=Win32 "%~dp0..\..\VS_2012\beid.sln"
 @if "%ERRORLEVEL%" == "1" goto msbuild_failed
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=Release /Property:Platform=x64 "%~dp0..\VS_2012\beid.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=Release /Property:Platform=x64 "%~dp0..\..\VS_2012\beid.sln"
 @if "%ERRORLEVEL%" == "1" goto msbuild_failed
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=Release /Property:Platform=x64 "%~dp0..\VS_2012\beid.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=Release /Property:Platform=x64 "%~dp0..\..\VS_2012\beid.sln"
 @if "%ERRORLEVEL%" == "1" goto msbuild_failed
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=PKCS11_FF_Release /Property:Platform=x64 "%~dp0..\VS_2012\beid.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=PKCS11_FF_Release /Property:Platform=x64 "%~dp0..\..\VS_2012\beid.sln"
 @if "%ERRORLEVEL%" == "1" goto msbuild_failed
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=PKCS11_FF_Release /Property:Platform=x64 "%~dp0..\VS_2012\beid.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=PKCS11_FF_Release /Property:Platform=x64 "%~dp0..\..\VS_2012\beid.sln"
 @if "%ERRORLEVEL%" == "1" goto msbuild_failed
 
 :: create minidriver driver installer
@@ -35,26 +36,25 @@
 :: ==================================
 
 :: BuildPath
-set INSTALLPATH=%~dp0..\minidriver\makemsi
+::set INSTALLPATH=%~dp0..\..\cardcomm\minidriver\makemsi
 
-
-md %INSTALLPATH%\Release
-@echo [INFO] Copying minidriver files..
+::md %INSTALLPATH%\Release
+::@echo [INFO] Copying minidriver files..
 
 :: copy inf files
-copy %INSTALLPATH%\beidmdrv.inf %INSTALLPATH%\Release
+::copy %INSTALLPATH%\beidmdrv.inf %INSTALLPATH%\Release
 
 :: copy drivers. We use the same files for 32 and 64 bit. But we create architecture dependent MSI's
-copy %~dp0..\VS_2012\Binaries\Win32_Release\beidmdrv.dll %INSTALLPATH%\Release\beidmdrv32.dll
-copy %~dp0..\VS_2012\Binaries\x64_Release\beidmdrv.dll %INSTALLPATH%\Release\beidmdrv64.dll
+::copy %~dp0..\..\VS_2012\Binaries\Win32_Release\beidmdrv.dll %INSTALLPATH%\Release\beidmdrv32.dll
+::copy %~dp0..\..\VS_2012\Binaries\x64_Release\beidmdrv.dll %INSTALLPATH%\Release\beidmdrv64.dll
 
 :: copy icon
-copy %~dp0..\minidriver\img\beid.ico %INSTALLPATH%\Release\
+::copy %~dp0..\minidriver\img\beid.ico %INSTALLPATH%\Release\
 
 :: @echo [INFO] Creating cat file
 :: Create catalog
-%INF2CAT_PATH%\inf2cat.exe /driver:%INSTALLPATH%\Release\ /os:XP_X86,XP_X64,Vista_X86,Vista_X64,7_X86,7_X64
-@if "%ERRORLEVEL%" == "1" goto inf2cat_failed
+::%INF2CAT_PATH%\inf2cat.exe /driver:%INSTALLPATH%\Release\ /os:XP_X86,XP_X64,Vista_X86,Vista_X64,7_X86,7_X64
+::@if "%ERRORLEVEL%" == "1" goto inf2cat_failed
 
 :: sign minidriver driver cat file
 :: ===============================
@@ -69,39 +69,39 @@ copy %~dp0..\minidriver\img\beid.ico %INSTALLPATH%\Release\
 set OUR_CURRENT_PATH="%cd%"
 @echo OUR_CURRENT_PATH = %OUR_CURRENT_PATH% 
 
-@cd %~dp0..\installers\eid-mw\Windows
+@cd %~dp0..\..\installers\eid-mw\Windows
 
 @call "%~dp0..\installers\eid-mw\Windows\build_msi_eidmw32.cmd"
 @if %ERRORLEVEL%==1 goto end_resetpath_with_error
 @echo [INFO] copy 32 bit msi installer
-copy %~dp0..\installers\eid-mw\Windows\bin\BeidMW_32.msi %~dp0
+copy %~dp0..\..\installers\eid-mw\Windows\bin\BeidMW_32.msi %~dp0
 
 @call "%~dp0..\installers\eid-mw\Windows\build_msi_eidmw64.cmd"
 @if %ERRORLEVEL%==1 goto end_resetpath_with_error
 @echo [INFO] copy 64 bit msi installer
-copy %~dp0..\installers\eid-mw\Windows\bin\BeidMW_64.msi %~dp0
+copy %~dp0..\..\installers\eid-mw\Windows\bin\BeidMW_64.msi %~dp0
 
 :: create the NSIS plugins
 :: =========================
-@echo [INFO] Building "%~dp0..\VS_2012\beid.sln"
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=Release /Property:Platform=Win32 "%~dp0..\installers\quickinstaller\NSIS_Plugins\VS_2012\beidplugins.sln"
+@echo [INFO] Building "%~dp0..\..\VS_2012\beid.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:clean /property:Configuration=Release /Property:Platform=Win32 "%~dp0..\..\installers\quickinstaller\NSIS_Plugins\VS_2012\beidplugins.sln"
 @if "%ERRORLEVEL%" == "1" goto msbuild_failed
-@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=Release /Property:Platform=Win32 "%~dp0..\installers\quickinstaller\NSIS_Plugins\VS_2012\beidplugins.sln"
+@"%BEID_DIR_MSBUILD%\MSBuild.exe" /target:build /property:Configuration=Release /Property:Platform=Win32 "%~dp0..\..\installers\quickinstaller\NSIS_Plugins\VS_2012\beidplugins.sln"
 
 :: copy the NSIS plugins to NSIS default plugin folder
 :: =========================
 @echo [INFO] Copying beid NSIS plugin
-copy %~dp0..\installers\quickinstaller\NSIS_Plugins\beidread\Release\beid.dll "%NSIS_PATH%\Plugins"
+copy %~dp0..\..\installers\quickinstaller\NSIS_Plugins\beidread\Release\beid.dll "%NSIS_PATH%\Plugins"
 @echo [INFO] Copying driver_installer NSIS plugin
-copy %~dp0..\installers\quickinstaller\NSIS_Plugins\driver_installer\Release\driver_installer.dll "%NSIS_PATH%\Plugins"
+copy %~dp0..\..\installers\quickinstaller\NSIS_Plugins\driver_installer\Release\driver_installer.dll "%NSIS_PATH%\Plugins"
 
 :: create the NSIS installer
 :: =========================
 @echo [INFO] Make nsis installer
-"%NSIS_PATH%\makensis.exe" "%~dp0..\installers\quickinstaller\Quickinstaller.nsi"
+"%NSIS_PATH%\makensis.exe" "%~dp0..\..\installers\quickinstaller\Quickinstaller.nsi"
 @if %ERRORLEVEL%==1 goto end_resetpath
 @echo [INFO] copy nsis installer
-copy %~dp0..\installers\quickinstaller\eID-QuickInstaller*.exe %~dp0
+copy %~dp0..\..\installers\quickinstaller\eID-QuickInstaller*.exe %~dp0
 goto end_resetpath
 
 
