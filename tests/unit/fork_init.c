32d73facf56de6b7da9a4300c2addb5f8369c3b3
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-10-17 14:35:58 +0000

    Separate out the child bits so we can do exit() so things continue working properly on a system where we run everything in a single process

--- tests/unit/fork_init.c
+++ tests/unit/fork_init.c
@@ -5,6 +5,7 @@
 #endif
 #include <pkcs11.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #include <sys/types.h>
@@ -12,21 +13,30 @@
 
 #include "testlib.h"
 
+int child_part() {
+	CK_INFO info;
+	ckrv_mod m[] = { CKR_CRYPTOKI_ALREADY_INITIALIZED, TEST_RV_OK };
+
+	check_rv_long(C_Initialize(NULL_PTR), m);
+	check_rv(C_GetInfo(&info));
+
+	return TEST_RV_OK;
+}
+
 /*
  * Check behaviour of C_Initialize across a fork() call (PKCS#11 §6.6.1)
  */
-
 TEST_FUNC(fork_init) {
 	pid_t child;
 	int status;
 
 	check_rv(C_Initialize(NULL_PTR));
 	if((child = fork()) == 0) {
-		CK_INFO info;
-		ckrv_mod m[] = { CKR_CRYPTOKI_ALREADY_INITIALIZED, TEST_RV_OK };
-
-		check_rv_long(C_Initialize(NULL_PTR), m);
-		check_rv(C_GetInfo(&info));
+		if(child_part() == TEST_RV_OK) {
+			exit(EXIT_SUCCESS);
+		} else {
+			exit(EXIT_FAILURE);
+		}
 	} else {
 		if(child == -1) {
 			printf("fork failed, skipping test\n");
