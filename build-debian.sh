503cf40761a66768bd7b533616a5643adedd19ae
Wouter Verhelst <wouter.verhelst@fedict.be>
2014-11-21 14:49:16 +0100

    Use git describe rather than svn-related stuff, where appropriate

--- build-debian.sh
+++ build-debian.sh
@@ -3,10 +3,10 @@
 
 # Figure out version numbers:
 # SVN
-# Can't use svn_revision, since that is only rebuilt as part of the
+# Can't use gitrev since that is only rebuilt as part of the
 # build step below (which means it currently contains the 'old' version
 # number)
-SVNREV=$(cat svn_revision)
+GITREV=$(cat gitrev)
 # Upstream
 MAINVER=$(./configure --version|grep ^eid-mw|cut -d' ' -f3)
 # Distribution name
@@ -15,7 +15,7 @@ DIST=$(lsb_release -c -s)
 # Generate changelog entry with correct version number, suppressing a possible
 # warning from dch that the distribution name may or may not be valid.
 # This would be true for an official upload, but this isn't that.
-yes | dch -v ${MAINVER}r${SVNREV}-0${DIST}1 -D${DIST} "Snapshot build"
+yes | dch -v ${MAINVER}${GITDATE}-0${DIST}1 -D${DIST} "Snapshot build"
 
 # Build
 make distclean
