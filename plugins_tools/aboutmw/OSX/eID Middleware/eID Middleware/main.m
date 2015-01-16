7424ddced59b3fb6fcd030a5ee88ce425247fefc
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-01-16 17:38:43 +0100

    Fix license statements

--- plugins_tools/aboutmw/OSX/eID Middleware/eID Middleware/main.m
+++ plugins_tools/aboutmw/OSX/eID Middleware/eID Middleware/main.m
@@ -1,11 +1,22 @@
-//
-//  main.m
-//  eID Middleware tool
-//
-//  Created by buildslave on 14/01/15.
-//  Copyright (c) 2015 FedICT. All rights reserved.
-//
+/* ****************************************************************************
 
+ * eID Middleware Project.
+ * Copyright (C) 2014 FedICT.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 3.0 as published by the Free Software Foundation.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, see
+ * http://www.gnu.org/licenses/.
+
+**************************************************************************** */
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
