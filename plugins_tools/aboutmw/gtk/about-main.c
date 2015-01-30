8a3c27fd8e7f10cb6663173f29415d1dbf494221
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-01-30 17:56:43 +0100

    Put git describe back into configure.ac
    We do actually need to have "git describe" work, but let's put it in a place
    where it does no harm

--- plugins_tools/aboutmw/gtk/about-main.c
+++ plugins_tools/aboutmw/gtk/about-main.c
@@ -344,13 +344,10 @@ int main(int argc, char** argv) {
 
 	store = GTK_LIST_STORE(gtk_builder_get_object(builder, "infodata"));
 
-	tmp = g_strdup(PACKAGE_VERSION);
-	loc = strchr(tmp, '-');
-	*loc = '\0';
 	gtk_list_store_append(store, &iter);
-	gtk_list_store_set(store, &iter, 0, _("Middleware version"), 1, tmp, -1);
+	gtk_list_store_set(store, &iter, 0, _("Middleware version"), 1, PACKAGE_VERSION, -1);
 	gtk_list_store_append(store, &iter);
-	gtk_list_store_set(store, &iter, 0, _("Middleware build ID"), 1, loc+1, -1);
+	gtk_list_store_set(store, &iter, 0, _("Middleware build ID"), 1, BUILD_ID, -1);
 
 	gtk_list_store_append(store, &iter);
 	gtk_list_store_set(store, &iter, 0, _("Middleware build date"), 1, EID_NOW_STRING, -1);
