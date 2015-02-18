a1b172bf10fcc79ad827c6f5dbdeb524eaa245b3
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-02-18 11:00:40 +0100

    Test for a too small buffer as well

--- tests/unit/slotlist.c
+++ tests/unit/slotlist.c
@@ -35,12 +35,20 @@ TEST_FUNC(slotlist) {
 	ckrv_mod m[] = { { CKR_BUFFER_TOO_SMALL, TEST_RV_OK } };
 	ckrv_mod m_uninit[] = { { CKR_CRYPTOKI_NOT_INITIALIZED, TEST_RV_OK }, { CKR_OK, TEST_RV_FAIL } };
 	ckrv_mod m_badarg[] = { { CKR_ARGUMENTS_BAD, TEST_RV_OK }, { CKR_OK, TEST_RV_FAIL } };
+	ckrv_mod m_small[] = { { CKR_BUFFER_TOO_SMALL, TEST_RV_OK }, { CKR_OK, TEST_RV_FAIL } };
 
 	check_rv_long(C_GetSlotList(CK_FALSE, NULL_PTR, &count), m_uninit);
 
 	check_rv(C_Initialize(NULL_PTR));
 
 	check_rv_long(C_GetSlotList(CK_FALSE, NULL_PTR, NULL_PTR), m_badarg);
+
+	if(count > 0) {
+		count=0;
+		check_rv_long(C_GetSlotList(CK_FALSE, list, &count), m_small);
+	} else {
+		printf("No slots found, can't test for CKR_BUFFER_TOO_SMALL...\n");
+	}
 
 	check_rv_long(C_GetSlotList(CK_FALSE, NULL_PTR, &count), m);
 	printf("slots found: %lu\n", count);
