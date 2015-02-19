15f5cea25f82aeff9d3186cb1fa25caee0400090
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-02-19 18:32:43 +0100

    Prepare for next iteration

--- scripts/windows/create_eidmw_version_files.cmd
+++ scripts/windows/create_eidmw_version_files.cmd
@@ -2,7 +2,7 @@
 
 @SET BASE_VERSION1=4
 @SET BASE_VERSION2=1
-@SET BASE_VERSION3=2
+@SET BASE_VERSION3=3
 
 @FOR /F "tokens=1" %%i in ('"C:\Program Files (x86)\Git\bin\git.exe" rev-list --count HEAD') do @SET EIDMW_REVISION=%%i
 ::"C:\Program Files (x86)\Git\bin\git.exe" describe
