6b20a64ee462e66c622928642c8ac4e35456e6bf
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-03-01 20:47:33 +0100

    Add missing file

---
+++ plugins_tools/eid-viewer/conversions.h
@@ -0,0 +1,24 @@
+#ifndef EID_VWR_CONVERSIONS_H
+#define EID_VWR_CONVERSIONS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum eid_vwr_langs {
+	EID_VWR_LANG_NONE,
+	EID_VWR_LANG_DE,
+	EID_VWR_LANG_EN,
+	EID_VWR_LANG_FR,
+	EID_VWR_LANG_NL,
+};
+
+const char* converted_string(const char* label, const char* normal);
+int can_convert(const char* label);
+void convert_set_lang(enum eid_vwr_langs which);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
