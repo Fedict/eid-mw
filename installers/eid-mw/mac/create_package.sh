ef23f60d7e489b3f5c2b6738bb60b5f1806cded7
Frederik <Frederik@Frederiks-MacBook-Pro.local>
2015-03-02 11:41:47 +0100

    mac installer: switched from packagemaker to pkgbuild

--- installers/eid-mw/mac/create_package.sh
+++ installers/eid-mw/mac/create_package.sh
@@ -4,23 +4,21 @@ set -e
 set -x
 
 #installer name defines
+#release dir, where all files to be released will be placed
+RELEASE_DIR="$(pwd)/release"
+#root dir, for files that are to be installed by the pkg
+ROOT_DIR="$RELEASE_DIR/root"
 #resources dir, for files that are to be kept inside the pkg
-RESOURCES_DIR="./resources"
-#install scripts dir, where the install scripts are
-INFO_DIR="./info"
-#install scripts dir, where the install scripts are
-INSTALL_SCRIPTS_DIR="./install_scripts"
+RESOURCES_DIR="$RELEASE_DIR/resources"
+#install scripts dir, where the install scripts are that will be executed by the package
+INSTALL_SCRIPTS_DIR="$RELEASE_DIR/install_scripts"
 
-#root dir, where all files to be packaged will be placed
-ROOT_DIR="$(pwd)/root"
 
 #inst dir, where our libs and binaries will be placed
 INST_DIR=/usr/local
 #licenses dir, where our licences will be placed
 LICENSES_DIR="/Library/BelgiumIdentityCard/Licenses"
 
-#dir where files will be placed temporarely, before being handled by the postinstall script
-BELGIUM_DIR="/Library/BelgiumIdentityCard"
 
 #base name of the package
 REL_NAME="beid"
@@ -40,35 +38,26 @@ DMG_NAME="${REL_NAME}_${REL_VERSION}.dmg"
 XPI_PLUGIN_DIR="/Library/Application\ Support/Mozilla/Extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}/belgiumeid@eid.belgium.be"
 
 
-#directories used to create the installer
-
-
-LIB_DIR="../../../output/lib/Release"
-BIN_DIR="../../../output/bin/Release"
+#cleanup previous build
 
 cleanup() {
-	test -e $ROOT_DIR && rm -rdf $ROOT_DIR
-	chown -R $SUDO_USER $PKG_NAME
-	chown $SUDO_USER $DMG_NAME
+test -e $RELEASE_DIR && rm -rdf $RELEASE_DIR
 }
 
 trap cleanup EXIT
 
 
 #create installer dirs
-mkdir -p $ROOT_DIR/$INST_DIR/lib/
+mkdir -p $ROOT_DIR/$INST_DIR/lib
 mkdir -p $ROOT_DIR/$LICENSES_DIR
-mkdir -p $ROOT_DIR/usr/bin/
+mkdir -p $RESOURCES_DIR
+mkdir -p $INSTALL_SCRIPTS_DIR
 
 #copy all files that should be part of the installer:
-cp $LIB_DIR/libbeidpkcs11.$REL_VERSION.dylib $ROOT_DIR/$INST_DIR/lib/
-
-
-LATEST_XPI=$(readlink ../../../plugins_tools/xpi/builds/belgiumeid-CURRENT.xpi)
-XPI_PLUGIN=../../../plugins_tools/xpi/builds/$LATEST_XPI
-cp $XPI_PLUGIN $RESOURCES_DIR
-
-cp -r ../../../cardcomm/tokend/BEID_Lion.tokend $RESOURCES_DIR
+cp ../../../output/lib/Release/libbeidpkcs11.$REL_VERSION.dylib $ROOT_DIR/$INST_DIR/lib/
+#copy pkcs11 bundle
+#cp -r ../../../misc/mac/pkcs11.bundle $RESOURCES_DIR
+#$ROOT_DIR/$INST_DIR/lib/$PKCS11_BUNDLE
 
 #copy licenses
 cp ../../../doc/licenses/Dutch/eID-toolkit_licensingtermsconditions.txt \
@@ -81,32 +70,24 @@ cp ../../../doc/licenses/German/eID-toolkit_licensingtermsconditions.txt \
 	$ROOT_DIR/$LICENSES_DIR/license_DE.txt ; \
 cp ../../../doc/licenses/THIRDPARTY-LICENSES-Mac.txt $ROOT_DIR/$LICENSES_DIR/
 
+
+cp -r ./resources $RESOURCES_DIR
+
+LATEST_XPI=$(readlink ../../../plugins_tools/xpi/builds/belgiumeid-CURRENT.xpi)
+XPI_PLUGIN=../../../plugins_tools/xpi/builds/$LATEST_XPI
+cp $XPI_PLUGIN $RESOURCES_DIR
+
+cp -r ../../../cardcomm/tokend/BEID_Lion.tokend $RESOURCES_DIR
+
 #copy certificates
 cp ../../../installers/certificates/beid-cert-belgiumrca2.der $RESOURCES_DIR
+cp ../../../installers/certificates/beid-cert-belgiumrca3.der $RESOURCES_DIR
 
-#copy pkcs11 bundle
-#cp -r ../../../misc/mac/pkcs11.bundle $RESOURCES_DIR
-#$ROOT_DIR/$INST_DIR/lib/$PKCS11_BUNDLE
+cp -r ./install_scripts $INSTALL_SCRIPTS_DIR
 	 
-## Make interpretes spaces as separator. So we specify 2 names:
-## the one that will be installed and the one to which this
-## name will be renamed into in the postflight script at the end
-## of the installation.
-
 
 #####################################################################
 ################################################################
-POSTFLIGHT=$REL_DIR/resources/postflight
-PREFLIGHT=$REL_DIR/resources/preflight
-INSTALLATIONCHECK=$REL_DIR/resources/InstallationCheck
-INSTALLATIONCHECKSTRINGS=$REL_DIR/resources/InstallationCheck.strings
-
-
-echo "********** manipulate mw_installer **********"
-#	  sed -e "s;REPL_REL_VERSION;$REL_VERSION;g" \
-#	  sed -e "s;REL_NAME;$REL_NAME;g" -e "s;PKCS11LIB_NAME_dylib;$PKCS11LIB_NAME_dylib;g" \
-#	  	   ./resources/pkcs11.bundle/Contents/Info.plist > \
-#	  	   $ROOT_DIR/$INST_DIR/lib/$PKCS11_BUNDLE/Contents/Info.plist
 
 echo "********** generate $PKG_NAME and $DMG_NAME **********"
 
@@ -116,12 +97,13 @@ echo "********** generate $PKG_NAME and $DMG_NAME **********"
 #chmod a-x $ROOT_DIR/$INST_DIR/lib/beid-pkcs11.bundle/Contents/Info.plist
 #chmod a-x $ROOT_DIR/$INST_DIR/lib/beid-pkcs11.bundle/Contents/PkgInfo
 chgrp    wheel  $ROOT_DIR/usr
-chgrp    wheel  $ROOT_DIR/usr/bin
 chgrp    wheel  $ROOT_DIR/usr/local
 chgrp    wheel  $ROOT_DIR/usr/local/lib
 chgrp -R admin  $RESOURCES_DIR/BEID_Lion.tokend
 
-$PKG_MAKER -r $ROOT_DIR -o $PKG_NAME -f $INFO_DIR/Info.plist \
-	-e $RESOURCES_DIR -s $INSTALL_SCRIPTS_DIR -n REL_VERSION
+pkgbuild --root $ROOT_DIR --scripts INSTALL_SCRIPTS_DIR --identifier eid.belgium.be --version $REL_VERSION --install-location / $PKG_NAME
+
+#$PKG_MAKER -r $ROOT_DIR -o $PKG_NAME -f $INFO_DIR/Info.plist \
+#	-e $RESOURCES_DIR -s $INSTALL_SCRIPTS_DIR -n REL_VERSION
 
 hdiutil create -srcfolder $PKG_NAME -volname "${VOL_NAME}" $DMG_NAME
