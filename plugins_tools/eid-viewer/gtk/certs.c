9afb42f27a4ab37529098ead46c3bbc518e5b603
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-03-04 12:46:21 +0100

    Set treestore values in a thread-safe way

--- plugins_tools/eid-viewer/gtk/certs.c
+++ plugins_tools/eid-viewer/gtk/certs.c
@@ -19,6 +19,43 @@ static GdkPixbuf* good_certificate;
 static GdkPixbuf* bad_certificate;
 static GdkPixbuf* unchecked_certificate;
 
+struct tree_store_data {
+	GtkTreeIter* iter;
+	gint* columns;
+	GValue* values;
+	gint n_values;
+	void(*free)(struct tree_store_data*);
+};
+
+static void tst_free_simple(struct tree_store_data* dat) {
+	int i;
+	free(dat->columns);
+	for(i=0;i<dat->n_values; i++) {
+		g_value_unset(&(dat->values[i]));
+	}
+	free(dat->values);
+	free(dat);
+}
+
+static gboolean tst_helper(gpointer user_data) {
+	struct tree_store_data* dat = (struct tree_store_data*)user_data;
+
+	gtk_tree_store_set_valuesv(certificates, dat->iter, dat->columns, dat->values, dat->n_values);
+	dat->free(dat);
+
+	return FALSE;
+}
+
+void tst_set(GtkTreeIter* i, gint* c, GValue* v, gint n) {
+	struct tree_store_data* dat = malloc(sizeof(struct tree_store_data));
+	dat->iter = i;
+	dat->columns = c;
+	dat->values = v;
+	dat->n_values = n;
+	dat->free = tst_free_simple;
+	g_main_context_invoke(NULL, tst_helper, dat);
+}
+
 static GtkTreeIter* get_iter_for(char* which) {
 	GtkTreeIter* parent;
 	enum certs w;
@@ -75,6 +112,8 @@ void add_certificate(char* label, void* data, int len) {
 	BIO *bio = BIO_new(BIO_s_mem());
 	char *buf;
 	size_t size;
+	gint *columns;
+	GValue *vals;
 
 	ensure_cert();
 	if(!strcmp(label, "CERT_RN_FILE")) {
@@ -84,27 +123,34 @@ void add_certificate(char* label, void* data, int len) {
 		g_warning("Could not parse %s certificate", label);
 		return;
 	}
+
+	columns = calloc(sizeof(gint),4);
+	vals = calloc(sizeof(GValue),4);
 	iter = get_iter_for(label);
-	gtk_tree_store_set(certificates, iter,
-	       		CERT_COL_LABEL, describe_cert(label, cert),
-			CERT_COL_IMAGE, unchecked_certificate,
-			-1);
+	columns[0] = CERT_COL_LABEL;
+	g_value_init(&(vals[0]), G_TYPE_STRING);
+	g_value_take_string(&(vals[0]), describe_cert(label, cert));
+	columns[1] = CERT_COL_IMAGE;
+	g_value_init(&(vals[1]), GDK_TYPE_PIXBUF);
+	g_value_set_instance(&(vals[1]), unchecked_certificate);
 
 	ASN1_TIME_print(bio, X509_get_notBefore(cert));
 	buf = malloc((size = BIO_ctrl_pending(bio)) + 1);
 	BIO_read(bio, buf, (int)size);
 	buf[size] = '\0';
-	gtk_tree_store_set(certificates, iter,
-			CERT_COL_VALIDFROM, buf,
-			-1);
+	columns[2] = CERT_COL_VALIDFROM;
+	g_value_init(&(vals[2]), G_TYPE_STRING);
+	g_value_set_string(&(vals[2]), buf);
 	
 	ASN1_TIME_print(bio, X509_get_notAfter(cert));
 	buf = malloc((size = BIO_ctrl_pending(bio)) + 1);
 	BIO_read(bio, buf, (int)size);
 	buf[size] = '\0';
-	gtk_tree_store_set(certificates, iter,
-			CERT_COL_VALIDTO, buf,
-			-1);
+	columns[3] = CERT_COL_VALIDTO;
+	g_value_init(&(vals[3]), G_TYPE_STRING);
+	g_value_set_string(&(vals[3]), buf);
+
+	tst_set(iter, columns, vals, 4);
 }
 
 GtkTreeModel* certificates_get_model() {
