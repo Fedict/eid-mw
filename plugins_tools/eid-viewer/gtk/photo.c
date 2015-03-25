cacdd804cf36a57812888880eae5f161dc59d6a8
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-03-25 10:49:46 +0100

    Fix memory leak

--- plugins_tools/eid-viewer/gtk/photo.c
+++ plugins_tools/eid-viewer/gtk/photo.c
@@ -22,6 +22,7 @@ void displayphoto(char* label G_GNUC_UNUSED, void* data, int length) {
 	GtkWidget* image = GTK_WIDGET(gtk_builder_get_object(builder, "photo"));
 	GInputStream *mstream = G_INPUT_STREAM(g_memory_input_stream_new_from_data(data, length, NULL));
 	GdkPixbuf *pixbuf = gdk_pixbuf_new_from_stream(mstream, NULL, NULL);
+	g_input_stream_close(mstream, NULL, NULL);
 
 	g_hash_table_insert(touched_labels, g_strdup("PHOTO_HASH"), clearphoto);
 	g_object_set_threaded(G_OBJECT(image), "pixbuf", pixbuf, g_object_unref);
