3d50a0db7e5045ddb821dbcf80692a1aafe22334
Wouter Verhelst <w@uter.be>
2015-03-25 10:16:06 +0100

    Mention where the source code can be found.

--- plugins_tools/eid-viewer/gtk/gtkui.c
+++ plugins_tools/eid-viewer/gtk/gtkui.c
@@ -34,7 +34,7 @@ void showabout(GtkMenuItem* about, gpointer user_data G_GNUC_UNUSED) {
 			"authors", authors,
 			"copyright", _("Copyright 2015 Fedict"),
 			"comments", _("View data on the Belgian eID card"),
-			"license", _("This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>."),
+			"license", _("This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\nSource code at <https://github.com/Fedict/eid-mw/>"),
 			"wrap-license", TRUE,
 			"artists", artists,
 			NULL);
