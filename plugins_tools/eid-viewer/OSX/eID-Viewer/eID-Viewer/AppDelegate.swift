07f4f555060f2b9a38c31bea55b6d89ded481483
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-04-15 17:08:06 +0200

    Start with OSX version
    UI design should be mostly complete. Still TODO is l10n, though.

---
+++ plugins_tools/eid-viewer/OSX/eID-Viewer/eID-Viewer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  eID-Viewer
+//
+//  Created by buildslave on 15/04/15.
+//  Copyright (c) 2015 Fedict. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
