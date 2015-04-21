da1cda99d75a31ee87f1da1f1109797748eed2aa
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-04-21 11:36:21 +0200

    Use NSOperationQueue and closures to put things on the main thread; implement logging.
    TODO: don't implement logging if the level isn't currently allowed.

--- plugins_tools/eid-viewer/OSX/eID-Viewer/eID-Viewer/AppDelegate.swift
+++ plugins_tools/eid-viewer/OSX/eID-Viewer/eID-Viewer/AppDelegate.swift
@@ -11,22 +11,40 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate, eIDOSLayerUI {
     @IBOutlet weak var window: NSWindow!
+    @IBOutlet var logItem: NSTextView!
+    @IBOutlet weak var logLevel: NSPopUpButton!
 
     @IBAction func do_pinop(sender: AnyObject) {
         var alert = NSAlert()
-        alert.messageText = "Not yet implemented"
+        alert.messageText = "Pin operations not yet implemented"
         alert.runModal()
     }
     @IBAction func open_file(sender: AnyObject) {
-        var alert = NSAlert()
-        alert.messageText = "Not yet implemented"
-        alert.runModal()
+        NSOperationQueue.mainQueue().addOperationWithBlock() {
+            var alert = NSAlert()
+            alert.messageText = "Opening files not yet implemented"
+            alert.runModal()
+        }
     }
     
     func log(line: String!, withLevel level: eIDLogLevel) {
-        var alert = NSAlert()
-        alert.messageText = "Not yet implemented"
-        alert.runModal()
+        NSOperationQueue.mainQueue().addOperationWithBlock() {
+            var output : String
+            switch(level) {
+            case eIDLogLevel.Detail:
+                output = "D: " + line
+            case eIDLogLevel.Normal:
+                output = "N: " + line
+            case eIDLogLevel.Coarse:
+                output = "C: " + line
+            case eIDLogLevel.Error:
+                output = "E: " + line
+                var alert = NSAlert()
+                alert.messageText = "Error:" + line;
+                alert.runModal()
+            }
+            self.logItem.insertText(output + "\n")
+        }
     }
     
     func newsrc(which: eIDSource) {
@@ -36,12 +54,14 @@ class AppDelegate: NSObject, NSApplicationDelegate, eIDOSLayerUI {
     func newstate(state: eIDState) {
     }
     func newstringdata(data: String!, withLabel label: String!) {
-        var aView : NSView
-        for aView in window.contentView.subviews {
-            if (aView.identifier.isEqualToString(label) && aView.type == "NSTextView") {
-                var tv : NSTextView = aView as! NSTextView
-                tv.setValue(data)
-                return
+        NSOperationQueue.mainQueue().addOperationWithBlock() {
+            var aView : NSView
+            for aView in self.window.contentView.subviews {
+                if (aView.identifier.isEqualToString(label) && aView.type == "NSTextView") {
+                    var tv : NSTextView = aView as! NSTextView
+                    tv.setValue(data)
+                    return
+                }
             }
         }
     }
