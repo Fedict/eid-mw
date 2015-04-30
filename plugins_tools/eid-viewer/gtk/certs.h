f6cbbf0874ccf048dd428277a8afd8f518c20b34
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-04-30 11:05:17 +0200

    Separate the column definition out into its own header file, so we can reuse it for OSX

--- plugins_tools/eid-viewer/gtk/certs.h
+++ plugins_tools/eid-viewer/gtk/certs.h
@@ -2,20 +2,7 @@
 #define EID_VWR_GTK_CERTS_H
 
 #include <gtk/gtk.h>
-
-enum cert_columns {
-	CERT_COL_LABEL,
-	CERT_COL_IMAGE,
-	CERT_COL_VALIDFROM,
-	CERT_COL_VALIDFROM_PAST,
-	CERT_COL_VALIDTO,
-	CERT_COL_VALIDTO_FUTURE,
-	CERT_COL_USE,
-	CERT_COL_VALIDITY,
-	CERT_COL_DESC,
-	CERT_COL_DATA,
-	CERT_COL_NCOLS
-};
+#include <certcols.h>
 
 void add_certificate(char* label, void* data, int len);
 void clear_certdata();
