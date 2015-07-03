1612865cb706828f16b911babc73cb2fe9d1d496
Wouter Verhelst <wouter.verhelst@fedict.be>
2015-07-03 12:59:19 +0200

    Do not validate while previewing
    On OSX, accessing our own bundle when we're a plugin is complicated. That means it's difficult to figure out what the XSD location is.
    
    We're only looking for a photo, anyway. If we don't find it, no harm done. So stop checking the XML validity when previewing. We still do check it when opening the file, however.

--- plugins_tools/eid-viewer/preview.c
+++ plugins_tools/eid-viewer/preview.c
@@ -5,6 +5,7 @@
 #include <b64/base64dec.h>
 #include <string.h>
 #include <xsdloc.h>
+#include <backend.h>
 
 #define check_xml(call) if((rc = call) < 0) { \
 be_log(EID_VWR_LOG_DETAIL, "Error while dealing with file (calling '%s'): %d", #call, rc); \
@@ -27,7 +28,6 @@ struct eid_vwr_preview* eid_vwr_get_preview(const char* filename) {
     if(!reader) {
         return p;
     }
-    check_xml(xmlTextReaderSchemaValidate(reader, get_xsdloc()));
     while((rc = xmlTextReaderRead(reader)) > 0) {
         const xmlChar *curnode = xmlTextReaderConstLocalName(reader);
         if(!strcmp(curnode, "photo")) {
