89c6a007070e49926bd7574ff12fc9b8c710c284
Frederik <Frederik@Frederiks-MacBook-Pro.local>
2015-09-25 15:09:03 +0200

    mac installer: create folder to hold the link

--- installers/eid-mw/mac/create_package.sh
+++ installers/eid-mw/mac/create_package.sh
@@ -81,7 +81,8 @@ cp ../../../Release/libbeidpkcs11.$REL_VERSION.dylib $PKCS11_INST_DIR
 #copy pkcs11 bundle
 cp -R ./Packages/beid-pkcs11.bundle $PKCS11_INST_DIR
 #make relative symblic link from bundle to the dylib
-ln -s ../../../libbeidpkcs11.$REL_VERSION.dylib $PKCS11_INST_DIR/beid-pkcs11.bundle/Contents/macOS/libbeidpkcs11.dylib
+mkdir -p "$PKCS11_INST_DIR/beid-pkcs11.bundle/Contents/MacOS/"
+ln -s ../../../libbeidpkcs11.$REL_VERSION.dylib "$PKCS11_INST_DIR/beid-pkcs11.bundle/Contents/MacOS/libbeidpkcs11.dylib"
 
 
 #copy licenses
