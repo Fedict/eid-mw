9a792f422bbba72a489fb4d008043742088c6f22
Frederikus <Frederik.v*******@f*****.be>
2015-12-15 09:38:14 +0100

    fixed type inconsistency

--- plugins_tools/eid-viewer/backend.c
+++ plugins_tools/eid-viewer/backend.c
@@ -29,7 +29,7 @@ struct eid_vwr_ui_callbacks* cb;
 #ifdef WIN32
 int eid_vwr_set_cbfuncs(		void(*newsrc)(enum eid_vwr_source source), // data source has changed.
 	void(*newstringdata)(const char* label, const char* data), // new string data to be displayed in UI.
-	void(*newbindata)(const char* label, const void* data, int datalen), // new binary data to be displayed in UI.
+	void(*newbindata)(const char* label, const unsigned char* data, int datalen), // new binary data to be displayed in UI.
 	void(*log)(enum eid_vwr_loglevel loglevel, const char* line), // log a string at the given level.
 	//void(*logv)(enum eid_vwr_loglevel loglevel, const char* line, va_list ap), // log a string using varargs. Note: a UI needs to implement only one of log() or logv(); the backend will use whichever is implemented.
 	void(*newstate)(enum eid_vwr_states states), // issued at state machine transition
