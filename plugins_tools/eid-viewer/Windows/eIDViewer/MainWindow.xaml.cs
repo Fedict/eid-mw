52868f9cbbabd19f15019bac1404a718e5a4929e
Frederikus <Frederik.v*******@f*****.be>
2015-12-17 15:00:30 +0100

    Windwos viewer: added internationalization for the identity labels

--- plugins_tools/eid-viewer/Windows/eIDViewer/MainWindow.xaml.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
+using System.Globalization;
 
 namespace eIDViewer
 {
@@ -32,5 +33,32 @@ namespace eIDViewer
             this.Close();
         }
 
+        private void ChangeLocalization (string localization)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(localization);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(localization);
+            eIDViewer.LocalizedStrings theLocalizedStrings = (LocalizedStrings)(App.Current.Resources["LocalizedStrings"]);
+            theLocalizedStrings.resource1 = new eIDViewer.Resources.ApplicationStringResources();
+        }
+
+        private void Deutch_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeLocalization("de-DE");
+        }
+
+        private void English_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeLocalization("en-US");
+        }
+
+        private void French_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeLocalization("fr-BE");
+        }
+
+        private void Nederlands_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeLocalization("nl-BE");
+        }
     }
 }
---
+++ plugins_tools/eid-viewer/Windows/eIDViewer/Sources/LocalizedStrings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace eIDViewer
+{
+    public class LocalizedStrings : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private static eIDViewer.Resources.ApplicationStringResources _resource1 = new eIDViewer.Resources.ApplicationStringResources();
+
+        public eIDViewer.Resources.ApplicationStringResources resource1
+        {
+            get { return _resource1; }
+
+            set
+            {
+                _resource1 = value;
+                NotifyPropertyChanged("resource1");
+            }
+        }
+    }
+}
