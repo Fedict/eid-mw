2278bc68319c348558d857cdc2cf8cf1d0ceb4e9
Wouter Verhelst <wouter.verhelst@fedict.be>
2016-02-26 13:52:03 +0100

    Provide a fallback if reading /proc doesn't work
    The dialogs call readlink() on /proc/<getppid()>/exe to figure out which
    process is trying to read the card.
    
    This works, but:
    - It is Linux-specific
    - It doesn't work if /proc isn't mounted for some reason.
    
    Currently, the dialog fails when either of those conditions isn't met.
    Rather than doing so, it makes more sense to just tell the user what we
    do know, and produce a (possibly somewhat awkward) message that doesn't
    have all the information we would've wanted to have.

--- cardcomm/pkcs11/src/dialogs/dialogsgtk/parent.c
+++ cardcomm/pkcs11/src/dialogs/dialogsgtk/parent.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "config.h"
 #include <stdio.h>
+#include <string.h>
 
 #include "parent.h"
 
@@ -10,10 +11,15 @@ ssize_t get_parent_path(char* exec_path, size_t exec_path_size)
 {
         char            proc_path[32];
         ssize_t         exec_path_len=-1;
+	pid_t		ppid = getppid();
 
-        snprintf(proc_path,sizeof(proc_path)-1,"/proc/%d/exe",getppid());
-        if((exec_path_len=readlink(proc_path,exec_path,exec_path_size-1))!=-1)
+        snprintf(proc_path,sizeof(proc_path)-1,"/proc/%d/exe",ppid);
+        if((exec_path_len=readlink(proc_path,exec_path,exec_path_size-1))!=-1) {
                 exec_path[exec_path_len]='\0';
+	} else {
+		snprintf(exec_path,exec_path_size - 1, "A process with PID %d", ppid);
+		exec_path_len = strlen(exec_path);
+	}
         return exec_path_len;
 }
 
