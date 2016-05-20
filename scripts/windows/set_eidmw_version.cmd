2da1f84a646a137f991eb91a87cacac341c981b8
Frederikus <Frederik.v*******@f*****.be>
2016-05-20 10:24:25 +0200

    updated windows version to 4.1.18

--- scripts/windows/set_eidmw_version.cmd
+++ scripts/windows/set_eidmw_version.cmd
@@ -2,7 +2,7 @@
 
 @SET BASE_VERSION1=4
 @SET BASE_VERSION2=1
-@SET BASE_VERSION3=17
+@SET BASE_VERSION3=18
 
 @FOR /F "tokens=1" %%i in ('"C:\Program Files (x86)\Git\bin\git.exe" rev-list --count HEAD') do @SET EIDMW_REVISION=%%i
 ::"C:\Program Files (x86)\Git\bin\git.exe" describe
