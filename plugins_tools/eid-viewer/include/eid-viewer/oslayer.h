0e23d586b48823cd7c2d9b7fbe86859cce32a022
Wouter Verhelst <wouter.verhelst@fedict.be>
2016-05-20 16:06:31 +0200

    Improve documentation

--- plugins_tools/eid-viewer/include/eid-viewer/oslayer.h
+++ plugins_tools/eid-viewer/include/eid-viewer/oslayer.h
@@ -89,61 +89,252 @@ enum eid_vwr_langs {
 };
 
 
-/** Callbacks which the backend can perform towards the UI */
+/** \brief Callbacks which the backend can perform towards the UI.
+  *
+  * To ensure forward compatibility, a UI implementation *must* call
+  * eid_vwr_cbstruct() to allocate an eid_vwr_ui_callbacks struct.
+  * Failure to do so may result in crashes in future versions of the
+  * eid-viewer library.
+  *
+  * An implementation may leave one or more of the below function
+  * pointers empty (i.e., at NULL). If this happens, the backend will
+  * simply not issue that event.
+  */
 struct eid_vwr_ui_callbacks {
-	void(*newsrc)(enum eid_vwr_source); ///< data source has changed.
-	void(*newstringdata)(const EID_CHAR* label, const EID_CHAR* data); ///< new string data to be displayed in UI.
-	void(*newbindata)(const EID_CHAR* label, const unsigned char* data, int datalen); ///< new binary data to be displayed in UI.
-	void(*log)(enum eid_vwr_loglevel, const EID_CHAR* line); ///< log a string at the given level.
-	void(*logv)(enum eid_vwr_loglevel, const EID_CHAR* line, va_list ap); ///< log a string using varargs. Note: a UI needs to implement only one of log() or logv(); the backend will use whichever is implemented.
-	void(*newstate)(enum eid_vwr_states); ///< issued at state machine transition
-	void(*pinop_result)(enum eid_vwr_pinops, enum eid_vwr_result); ///< issued when a PIN operation finished.
+	/** \brief Called when the data source has changed.
+	  * 
+	  * When the viewer library starts looking at a new file or eID
+	  * card, this event is issued. When this happens, a user
+	  * interface should clear whatever data it has received from
+	  * the backend -- it is not valid anymore.
+	  *
+	  * \param new_source the type of the new source.
+	  */
+	void(*newsrc)(enum eid_vwr_source new_source);
+	/** \brief new string data to be displayed in UI.
+	  *
+	  * Data with the given label and which should be interpreted as
+	  * a string was found. These may be re-issued (with modified
+	  * data) after a call to eid_vwr_convert_set_lang().
+	  *
+	  * \param label the label (identifier) of the data item. Should
+	  * never change.
+	  * \param data the data as represented in the currently active
+	  * language.
+	  * \see beidsdk_card_data.pdf contains the possible labels
+	  */
+	void(*newstringdata)(const EID_CHAR* label, const EID_CHAR* data);
+	/** \brief New binary data to be displayed in UI.
+	  *
+	  * Data with the given label and which should be interpreted as
+	  * a binary blob was found. This will not change when the
+	  * language is modified.
+	  *
+	  * \param label the label (identifier) of the data item. Should
+	  * never change.
+	  * \param data the data as it is on the card.
+	  * \param datalen the length of the data, in bytes.
+	  */
+	void(*newbindata)(const EID_CHAR* label, const unsigned char* data, int datalen);
+	/** \brief Log a string at the given level.
+	  *
+	  * \param level the level at which to log.
+	  * \param line the string to log at the given level.
+	  *
+	  * \note only one of log() or logv() needs to be implemented;
+	  * the backend will use whichever version is available.
+	  */
+	void(*log)(enum eid_vwr_loglevel level, const EID_CHAR* line);
+	/** \brief Log a string using varargs.
+	  *
+	  * The advantage of having this function (as opposed to log())
+	  * is that it allows for allocating a string using whatever
+	  * memory management the frontend uses. If this is not
+	  * necessary or desirable, implementing log() rather than this
+	  * function should suffice.
+	  *
+	  * \param level the level at which to log.
+	  * \param line the printf()-formatted string to log.
+	  * \param ap arguments to line's format string.
+	  *
+	  * \note only one of log() or logv() needs to be implemented;
+	  * the backend will use whichever version is available.
+	  */
+	void(*logv)(enum eid_vwr_loglevel level, const EID_CHAR* line, va_list ap);
+	/** \brief State machine transition.
+	  *
+	  * This event is issued whenever the state machine changes
+	  * state. For more information, see the state machine diagram
+	  * as documented at struct eid_vwr_states.
+	  *
+	  * \param new_state the new state we're in right now.
+	  */
+	void(*newstate)(enum eid_vwr_states new_state);
+	/** \brief Return the result of a PIN operation
+	  *
+	  * When the user interface calls eid_vwr_pinop(), then at some
+	  * later point this function may be called with the result of
+	  * the requested PIN operation.
+	  *
+	  * If the state machine is not in the TOKEN_WAIT state when
+	  * eid_vwr_pinop() was called, this event will never be fired.
+	  *
+	  * \param op the operation for which this is a result
+	  * \param res the result of the operation
+	  */
+	void(*pinop_result)(enum eid_vwr_pinops op, enum eid_vwr_result res);
+	/** \brief The number of known readers has changed
+	  *
+	  * \param nreaders the new reader count
+	  * \param slots the slot IDs and human-readable descriptions of
+	  * all known readers
+	  */
 	void(*readers_changed)(unsigned long nreaders, slotdesc* slots); ///< issued when number of readers changes
 };
 
 /** Struct used by preview handler */
 struct eid_vwr_preview {
-	void* imagedata; ///< JPEG-encoded preview
+	void* imagedata; ///< JPEG photo of the inspected XML file
 	size_t imagelen; ///< length of imagedata
 	int have_data; ///< nonzero if there is actually any data
 };
 
-/** Perform a PIN operation */
-DllExport void eid_vwr_pinop(enum eid_vwr_pinops);
-/** Initialize the callbacks */
+/** \brief Perform a PIN operation
+  *
+  * If the state machine is currently in the TOKEN_WAIT state, issue a
+  * PIN operation. In all other cases, does nothing.
+  *
+  * \param op the operation to perform; EID_VWR_PINOP_TEST to log on as
+  * a test to see if the PIN code is valid, or EID_VWR_PINOP_CHG to
+  * change the PIN code.
+  */
+DllExport void eid_vwr_pinop(enum eid_vwr_pinops op);
+/** \brief Initialize the callbacks
+  * 
+  * Must be called by every application which wants to make use of this
+  * API, otherwise nothing will happen.
+  *
+  * \param cb the callback functions which this application implements
+  * \return 0 if the callbacks were installed successfully.
+  * \note the API assumes that this function is called exactly once for
+  * the lifetime of the application. While it should be safe to call it
+  * more than once, this is not very well tested and therefore doing so
+  * is not recommended.
+  */
 DllExport int eid_vwr_createcallbacks(struct eid_vwr_ui_callbacks* cb);
-
-/** Creates the eid_vwr_ui_callbacks struct. */
+/** \brief Create the eid_vwr_ui_callbacks struct.
+  *
+  * While it is possible, in theory, to create the struct by malloc()ing
+  * it manually, doing so will fail when the eid-viewer library is
+  * updated and a new callback function is added. For this reason,
+  * applications that want to continue working with future versions of
+  * the library without compatibility issues *must* use this function to
+  * allocate a correctly-sized structure.
+  * \return an allocated eid_vwr_ui_callbacks struct, with all members
+  * set to NULL.
+  */
 DllExport struct eid_vwr_ui_callbacks* eid_vwr_cbstruct();
-/** Create a preview for the given file */
+/** \brief Create a preview for the given file 
+  *
+  * This function simply parses the XML file and extracts the embedded
+  * JPEG photo. It is useful for an "open file" dialog, or similar.
+  * \param filename the full path to the filename from which to extract
+  * the photo
+  * \return 
+  */
 DllExport struct eid_vwr_preview* eid_vwr_get_preview(const EID_CHAR* filename);
-
-/** Check if there is an event to be handled. Will handle one event and then return. */
+/** 
+  * \brief Check if there is an event to be handled.
+  *
+  * Will handle one event and then return. Useful if an event loop
+  * exists elsewhere; otherwise, using eid_vwr_be_mainloop() is
+  * recommended.
+  */
 DllExport void eid_vwr_poll();
-/** Loop over the above. Does not return. */
+/** \brief Loop over eid_vwr_poll().
+  *
+  * \return never
+  */
 DllExport void eid_vwr_be_mainloop();
-/** Save the currently-open data. Returns before the file has been saved; saving
- * data is done in a background thread. To detect when the save operation has
- * finished, listen to the state machine. */
+/**
+  * \brief Save the currently-open data.
+  *
+  * Returns before the save operation has finished. The state machine
+  * will switch to the STATE_TOKEN_SERIALIZE state while data is being
+  * saved, and will revert to STATE_TOKEN_WAIT when the operation has
+  * finished, or to STATE_TOKEN_ERROR if an error occurs.
+  *
+  * \param target_file the name of the file in which to save data.
+  * \see eid_vwr_ui_callbacks::newstate()
+  */
 DllExport void eid_vwr_be_serialize(const EID_CHAR* target_file);
-/** Open a file. Returns before the file has been opened; reading the file and
- * parsing it is done in a background thread. To detect when the open operation
- * has finished, listen to the state machine. */
+/**
+  * \brief Open a file.
+  *
+  * Returns before the file has been opened; reading the file and
+  * parsing it is done in a background thread. The immediate effect of
+  * calling this function is a state transition to STATE_FILE. If a read
+  * or parser error occurs, the state reverts to STATE_NO_TOKEN.
+  * 
+  * \param source_file the file to read
+  */
 DllExport void eid_vwr_be_deserialize(const EID_CHAR* source_file);
-/** Get the currently-open data in XML form, usable by the drag-and-drop
- * handler. Note: do *not* free this data. */
+/**
+  * \brief Get the currently-open data in XML form.
+  *
+  * This method can be called when the state machine is in one of the
+  * STATE_FILE or STATE_TOKEN_WAIT states.
+  *
+  * \return a pointer to the XML-encoded data of the most recently
+  * opened card data, or NULL if the state machine is not currently in
+  * the right state.
+  * \note this data is owned by the eid-viewer library and should *not*
+  * be freed by the caller.
+  */
 DllExport const char* eid_vwr_be_get_xmlform();
-/** Select the current reader. If "automatic" is nonzero, let the backend decide
- * which reader and ignore the value of manualslot; otherwise, force
- * "manualslot" to be the active reader. */
+/** \brief Select the current reader.
+  *
+  * \param automatic if nonzero, let the backend decide
+  * which reader and ignore the value of manualslot.
+  * \param manualslot if automatic is zero, ignore all readers except for
+  * the one with this as the slot id.
+  * \see eid_vwr_ui_callbacks::readers_changed()
+  */
 DllExport void eid_vwr_be_select_slot(int automatic, unsigned long manualslot);
-/** Set the card to "invalid". This should really not be done from the UI
- * but from the backend, but for now, keep things as they are. */
+/** \brief Mark the current data as invalid.
+  *
+  * If the UI detects that the card data is invalid for some reason,
+  * then this method will move the state machine to the
+  * STATE_CARD_INVALID state. The backend will *not* perform this
+  * operation by itself.
+  *
+  * This method is only valid if the state machine is currently in
+  * STATE_TOKEN or one of its child states.
+  *
+  * \see 
+  *  - eid_vwr_verify_cert() tests whether the certificate chains up to
+  *    a known root certificate, and is not revoked
+  *  - eid_vwr_check_data_validity() tests whether the data on the card
+  *    is signed by the given RRN certificate (but does not check the
+  *    RRN certificate itself)
+  */
 DllExport void eid_vwr_be_set_invalid();
-/** Close the current file */
+/** \brief Close the current file
+  *
+  * If the current state is STATE_FILE, switch to STATE_NO_TOKEN. In all
+  * other cases, does nothing.
+  */
 DllExport void eid_vwr_close_file();
-/** Inform the backend of what the user interface is, so that converted
- * strings are in the correct language */
+/** \brief Set the backend language.
+  *
+  * This method sets the current backend language to the given language.
+  * If any data has been loaded (i.e., the state machine is in state
+  * STATE_FILE or STATE_TOKEN_WAIT), this will also re-issue
+  * eid_vwr_ui_callbacks::newstringdata() events for which the
+  * representation in the new language may be different.
+  *
+  * \param which the language to which to switch
+  */
 DllExport void eid_vwr_convert_set_lang(enum eid_vwr_langs which);
 
 /**@}*/
