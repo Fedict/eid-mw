e20e492558f4c67718084991f119352c69bacfae
Wouter Verhelst <wouter.verhelst@fedict.be>
2016-07-15 17:06:40 +0200

    Add extra options for conversions
    If we already have a buffer, then using a return value adds an extra
    memcpy() for no good reason.
    
    Instead, add an alternate version of the UTF8TOEID and EIDTOUTF8 macros
    which write their result into a passed buffer, rather than allocating
    one.

--- plugins_tools/util/utftranslate.c
+++ plugins_tools/util/utftranslate.c
@@ -5,10 +5,14 @@
 
 //caller is responsible for freeing the returned string,
 //utf16len is the length of the returned string in bytes, excluding the terminating null
-wchar_t* Utf8ToUtf16(const char* utf8string, unsigned long* utf16len)
+wchar_t* Utf8ToUtf16(const char* utf8string, unsigned long* utf16len, wchar_t *buf)
 {
 	int wcharcount = MultiByteToWideChar(CP_UTF8, 0, utf8string, -1, NULL, 0);
-	wchar_t *utf16string = (wchar_t*)malloc(sizeof(wchar_t) * (wcharcount));
+	int reqlen = sizeof(wchar_t) * (wcharcount);
+	wchar_t *utf16string = (buf) ? buf : (wchar_t*)malloc(sizeof(wchar_t) * (wcharcount));
+	if(reqlen > *utf16len && buf != NULL) {
+		return NULL;
+	}
 	if (utf16string != NULL)
 	{
 		MultiByteToWideChar(CP_UTF8, 0, utf8string, -1, utf16string, wcharcount);
@@ -20,10 +24,13 @@ wchar_t* Utf8ToUtf16(const char* utf8string, unsigned long* utf16len)
 
 //caller is responsible for freeing the returned string
 //utf8len is the length of the returned string in bytes, excluding the terminating null
-char* Utf16ToUtf8(const wchar_t * utf16string, unsigned long* utf8len)
+char* Utf16ToUtf8(const wchar_t * utf16string, unsigned long* utf8len, char* buf)
 {
 	int utf8bytesize = WideCharToMultiByte(CP_UTF8, 0, utf16string, -1, NULL, 0, NULL, NULL);
-	char *utf8string = (char*) malloc(utf8bytesize);
+	char *utf8string = (buf) ? buf : (char*) malloc(utf8bytesize);
+	if(utf8bytesize > utf8len && buf != NULL) {
+		return NULL;
+	}
 	if (utf8string != NULL)
 	{
 		WideCharToMultiByte(CP_UTF8, 0, utf16string, -1, utf8string, utf8bytesize, NULL, NULL);
