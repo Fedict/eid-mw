6ef0fd4874122e6c8d97d0557fc23feb135c916e
Wouter Verhelst <wouter.verhelst@fedict.be>
2016-07-28 10:48:00 +0200

    Reindent this file

--- plugins_tools/eid-viewer/OSX/eID Viewer/eID Viewer/oslayer-objc.h
+++ plugins_tools/eid-viewer/OSX/eID Viewer/eID Viewer/oslayer-objc.h
@@ -12,58 +12,58 @@
 @import AppKit;
 
 typedef NS_ENUM(NSInteger, eIDSource) {
-    eIDSourceNone = EID_VWR_SRC_NONE,
-    eIDSourceFile = EID_VWR_SRC_FILE,
-    eIDSourceCard = EID_VWR_SRC_CARD,
-    eIDSourceUnknown = EID_VWR_SRC_UNKNOWN,
+	eIDSourceNone = EID_VWR_SRC_NONE,
+	eIDSourceFile = EID_VWR_SRC_FILE,
+	eIDSourceCard = EID_VWR_SRC_CARD,
+	eIDSourceUnknown = EID_VWR_SRC_UNKNOWN,
 };
 
 typedef NS_ENUM(NSInteger, eIDLogLevel) {
-    eIDLogLevelDetail = EID_VWR_LOG_DETAIL,
-    eIDLogLevelNormal = EID_VWR_LOG_NORMAL,
-    eIDLogLevelCoarse = EID_VWR_LOG_COARSE,
-    eIDLogLevelError = EID_VWR_LOG_ERROR,
+	eIDLogLevelDetail = EID_VWR_LOG_DETAIL,
+	eIDLogLevelNormal = EID_VWR_LOG_NORMAL,
+	eIDLogLevelCoarse = EID_VWR_LOG_COARSE,
+	eIDLogLevelError = EID_VWR_LOG_ERROR,
 };
 
 typedef NS_ENUM(NSInteger, eIDPinOp) {
-    eIDPinOpTest = EID_VWR_PINOP_TEST,
-    eIDPinOpChange = EID_VWR_PINOP_CHG,
+	eIDPinOpTest = EID_VWR_PINOP_TEST,
+	eIDPinOpChange = EID_VWR_PINOP_CHG,
 };
 
 typedef NS_ENUM(NSInteger, eIDState) {
-    eIDStateLibOpen = STATE_LIBOPEN,
-    eIDStateCallbacks = STATE_CALLBACKS,
-    eIDStateReady = STATE_READY,
-    eIDStateToken = STATE_TOKEN,
-    eIDStateTokenWait = STATE_TOKEN_WAIT,
-    eIDStateTokenID = STATE_TOKEN_ID,
-    eIDStateTokenCerts = STATE_TOKEN_CERTS,
-    eIDStateTokenPinop = STATE_TOKEN_PINOP,
-    eIDStateTokenSerialize = STATE_TOKEN_SERIALIZE,
-    eIDStateTokenError = STATE_TOKEN_ERROR,
-    eIDStateFile = STATE_FILE,
-    eIDStateCardInvalid = STATE_CARD_INVALID,
-    eIDStateNoToken = STATE_NO_TOKEN,
-    eIDStateNoReader = STATE_NO_READER,
+	eIDStateLibOpen = STATE_LIBOPEN,
+	eIDStateCallbacks = STATE_CALLBACKS,
+	eIDStateReady = STATE_READY,
+	eIDStateToken = STATE_TOKEN,
+	eIDStateTokenWait = STATE_TOKEN_WAIT,
+	eIDStateTokenID = STATE_TOKEN_ID,
+	eIDStateTokenCerts = STATE_TOKEN_CERTS,
+	eIDStateTokenPinop = STATE_TOKEN_PINOP,
+	eIDStateTokenSerialize = STATE_TOKEN_SERIALIZE,
+	eIDStateTokenError = STATE_TOKEN_ERROR,
+	eIDStateFile = STATE_FILE,
+	eIDStateCardInvalid = STATE_CARD_INVALID,
+	eIDStateNoToken = STATE_NO_TOKEN,
+	eIDStateNoReader = STATE_NO_READER,
 };
 
 typedef NS_ENUM(NSInteger, eIDLanguage) {
-    eIDLanguageNone = EID_VWR_LANG_NONE,
-    eIDLanguageDe = EID_VWR_LANG_DE,
-    eIDLanguageEn = EID_VWR_LANG_EN,
-    eIDLanguageFr = EID_VWR_LANG_FR,
-    eIDLanguageNl = EID_VWR_LANG_NL,
+	eIDLanguageNone = EID_VWR_LANG_NONE,
+	eIDLanguageDe = EID_VWR_LANG_DE,
+	eIDLanguageEn = EID_VWR_LANG_EN,
+	eIDLanguageFr = EID_VWR_LANG_FR,
+	eIDLanguageNl = EID_VWR_LANG_NL,
 };
 
 typedef NS_ENUM(NSInteger, eIDResult) {
-    eIDResultFailed = EID_VWR_RES_FAILED,
-    eIDResultSuccess = EID_VWR_RES_SUCCESS,
-    eIDResultUnknown = EID_VWR_RES_UNKNOWN,
+	eIDResultFailed = EID_VWR_RES_FAILED,
+	eIDResultSuccess = EID_VWR_RES_SUCCESS,
+	eIDResultUnknown = EID_VWR_RES_UNKNOWN,
 };
 
 /** \brief Protocol for implementing a UI.
-  * \see cbstruct for details on what each method does; these methods are
-  * straightforward C-to-ObjC translations */
+ * \see cbstruct for details on what each method does; these methods are
+ * straightforward C-to-ObjC translations */
 @protocol eIDOSLayerUI
 -(void)newsrc:(eIDSource)which; ///< called by eid_vwr_ui_callbacks::newsrc()
 -(void)newstringdata:(NSString*)data /**< . */ withLabel:(NSString*)label; ///< called by eid_vwr_ui_callbacks::newstringdata()
@@ -75,7 +75,7 @@ typedef NS_ENUM(NSInteger, eIDResult) {
 @end
 
 /** Class method-only class which wraps the corresponding C-only APIs for the
-    benefit of ObjC applications. */
+ benefit of ObjC applications. */
 @interface eIDOSLayerBackend : NSObject
 +(void)pinop:(eIDPinOp)which; ///< calls eid_vwr_pinop()
 +(NSInteger)setUi:(id<eIDOSLayerUI>)ui; ///< calls eid_vwr_ui_callbacks() and eid_vwr_createcallbacks()
