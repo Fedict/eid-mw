6ccf078773d42586a1024d5568d3d72db700c78f
Wouter Verhelst <wouter.verhelst@fedict.be>
2016-08-25 13:44:29 +0200

    Make the card event test actually work

--- plugins_tools/eid-viewer/test/cardevent.c
+++ plugins_tools/eid-viewer/test/cardevent.c
@@ -8,6 +8,7 @@
 
 TEST_FUNC(cardevent) {
 	struct eid_vwr_ui_callbacks* cb;
+	int i;
 
 	cursrc = EID_VWR_SRC_UNKNOWN;
 	if(!have_robot()) {
@@ -17,12 +18,19 @@ TEST_FUNC(cardevent) {
 	robot_remove_card();
 	cb = createcbs();
 	verbose_assert(cb != NULL);
-	verbose_assert(eid_vwr_createcallbacks(cb) != 0);
-	sleep(1);
+	verbose_assert(eid_vwr_createcallbacks(cb) == 0);
+	SLEEP(1);
 	verbose_assert(cursrc == EID_VWR_SRC_NONE);
 	robot_insert_card();
 	printf("Card inserted, waiting 10 seconds for card event...\n");
-	sleep(10);
+	for(i=0; i<10; i++) {
+		SLEEP(1);
+		if(cursrc != EID_VWR_SRC_NONE) {
+			i+=10;
+		}
+		eid_vwr_poll();
+		printf(".");
+	}
 	verbose_assert(cursrc == EID_VWR_SRC_CARD);
 	return TEST_RV_OK;
 }
