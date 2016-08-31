54e8fb2951762ec2a31d4e5bac906da7f1951c1e
Wouter Verhelst <wouter.verhelst@fedict.be>
2016-08-31 14:57:33 +0200

    Make sure the XSD can be found while running the test suite

--- plugins_tools/eid-viewer/xsdloc.c
+++ plugins_tools/eid-viewer/xsdloc.c
@@ -3,8 +3,13 @@
 #endif
 
 #include "config.h"
+#include <stdlib.h>
 
 /* Return the XSD location on non-OSX POSIX systems */
 const char* get_xsdloc(void) {
+	char *val = getenv("EID_XSDLOC");
+	if(val != NULL) {
+		return val;
+	}
 	return DATAROOTDIR "/" PACKAGE_NAME "/eidv4.xsd";
 }
