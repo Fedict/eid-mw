e398e3eccb4503c048065d5490f547d34568bba6
Frederikus <Frederik.v*******@f*****.be>
2016-10-10 15:05:37 +0200

    only handle cards with the BEID ATR: 0x3b, 0x98, 0x13, 0x40, 0x0a, 0xa5, 0x03, 0x01, 0x01, 0x01, 0xAD, 0x13, 0x11

--- cardcomm/ctkToken/EIDToken/TokenDriver.m
+++ cardcomm/ctkToken/EIDToken/TokenDriver.m
@@ -5,12 +5,27 @@
 #import "Token.h"
 #import <os/log.h>
 
-@implementation PIVTokenDriver
+@implementation BEIDTokenDriver
 
 - (TKSmartCardToken *)tokenDriver:(TKSmartCardTokenDriver *)driver createTokenForSmartCard:(TKSmartCard *)smartCard AID:(NSData *)AID error:(NSError * _Nullable __autoreleasing *)error {
-    //assert(0);
+
+    UInt8 eIDCardATRBytes[13] = {0x3b, 0x98, 0x13, 0x40, 0x0a, 0xa5, 0x03, 0x01, 0x01, 0x01, 0xAD, 0x13, 0x11};
+
+    NSData * eIDCardATR = [NSData dataWithBytes:eIDCardATRBytes length:13];
+    NSData * atrBytes = smartCard.slot.ATR.bytes;
+    
+    Byte* dataBytes = (Byte*)atrBytes.bytes;
+    int i;
+    for ( i = 0 ; i < atrBytes.length ; i++)
+    {
+        os_log_error(OS_LOG_DEFAULT, "%d: 0x%x ",i, dataBytes[i]);
+    }
+    if (![atrBytes isEqualToData:eIDCardATR] ){
+        return nil;
+    }
+
     os_log_error(OS_LOG_DEFAULT, "BEID attaching tokendriver");
-    return [[PIVToken alloc] initWithSmartCard:smartCard AID:AID PIVDriver:self error:error];
+    return [[BEIDToken alloc] initWithSmartCard:smartCard AID:AID BEIDDriver:self error:error];
 }
 
 @end
