c43b78768e68725195b5e34ec39df9855c56684e
Frederikus <Frederik.v*******@f*****.be>
2016-10-28 13:29:13 +0200

    bugfix: location for creation of testsigned cat was wrong

--- scripts/windows/build_all.bat
+++ scripts/windows/build_all.bat
@@ -88,7 +88,7 @@ copy %~dp0..\..\cardcomm\VS_2012\Binaries\x64_Release_No_Dialog\beidmdrv64nd.dll
 
 :: @echo [INFO] Creating cat file
 :: Create catalog
-%INF2CAT_PATH%\inf2cat.exe /driver:%MDRVINSTALLPATH%\Release\ /os:XP_X86,XP_X64,Vista_X86,Vista_X64,7_X86,7_X64
+%INF2CAT_PATH%\inf2cat.exe /driver:%MDRVINSTALLPATH%\Release_nd\ /os:XP_X86,XP_X64,Vista_X86,Vista_X64,7_X86,7_X64
 @if "%ERRORLEVEL%" == "1" goto inf2cat_failed
 
 :: sign minidriver driver cat file
