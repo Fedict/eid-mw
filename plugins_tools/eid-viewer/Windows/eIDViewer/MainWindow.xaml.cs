a3bdc02074e2c38b0031e91c88cf1845202c7576
Frederikus <Frederik.v*******@f*****.be>
2017-02-07 11:11:51 +0100

    Windows eID Viewer: put the about message in a separate file, that will be auto-generated with the correct eID version

--- plugins_tools/eid-viewer/Windows/eIDViewer/MainWindow.xaml.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/MainWindow.xaml.cs
@@ -244,28 +244,7 @@ namespace eIDViewer
             Popup myPopup = new Popup();
             myPopup.IsOpen = true;
 
-            string message = @"eID Viewer 4.2.0
-eID Middleware Project
-Copyright(C) 2016 Fedict
-
-By Frederik Vernelen and Wouter Verhelst
-Based on the design of Frank Marien
-
-This is free software; you can redistribute it and / or modify it
-under the terms of the GNU Lesser General Public License version 
-3.0 as published by the Free Software Foundation.
-
-This software is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU 
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public 
-License along with this software; if not, see 
-http://www.gnu.org/licenses
-
-Official releases and support are available on http://eid.belgium.be
-Source code and other files are available on https://github.com/Fedict/eid-viewer";
+            string message = About.AboutMessage();
             string caption = "About";
             MessageBoxResult result = new MessageBoxResult();
             MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Information, result);
