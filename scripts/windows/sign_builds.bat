d6e9f2c96d4af6d6e391ab27725b2c88052ebcb2
Frederikus <Frederik.v*******@f*****.be>
2017-02-16 09:30:31 +0100

    Updated windows sign script: fixed sha1 signature of eIDViewer nsis installer

--- scripts/windows/sign_builds.bat
+++ scripts/windows/sign_builds.bat
@@ -114,7 +114,7 @@ copy %~dp0..\..\installers\eid-viewer\Windows\bin\BeidViewer.msi %~dp0
 @if "%ERRORLEVEL%" == "1" goto signtool_failed
 
 @echo [INFO] sign nsis viewer installer
-"%SIGNTOOL_PATH%\signtool" sign /n "FedICT" /sha1 "2259EF223A51E91964D7F4695706091194E018BB" /t http://timestamp.verisign.com/scripts/timestamp.dll /v "%~dp0..\..\installers\quickinstaller\Belgium eID ViewerInstaller %BASE_VERSION1%.%BASE_VERSION2%.%BASE_VERSION3%.%EIDMW_REVISION%.exe"
+"%SIGNTOOL_PATH%\signtool" sign /n "FedICT" /sha1 "2259EF223A51E91964D7F4695706091194E018BB" /t http://timestamp.verisign.com/scripts/timestamp.dll /v "%~dp0..\..\installers\quickinstaller\Belgium eID Viewer Installer %BASE_VERSION1%.%BASE_VERSION2%.%BASE_VERSION3%.%EIDMW_REVISION%.exe"
 "%SIGNTOOL_PATH%\signtool" sign /as /fd SHA256 /s MY /n "Fedict" /sha1 "2259EF223A51E91964D7F4695706091194E018BB" /tr http://timestamp.globalsign.com/?signature=sha2 /td SHA256 /v "%~dp0..\..\installers\quickinstaller\Belgium eID Viewer Installer %BASE_VERSION1%.%BASE_VERSION2%.%BASE_VERSION3%.%EIDMW_REVISION%.exe"
 @if "%ERRORLEVEL%" == "1" goto signtool_failed
 
