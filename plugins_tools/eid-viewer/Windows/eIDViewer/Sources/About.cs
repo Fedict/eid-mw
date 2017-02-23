de97f4026c37660f7f5a28af54bfe4e409c59cc7
Frederikus <Frederik.v*******@f*****.be>
2017-02-23 10:44:08 +0100

    windows: commit auto-generated file with new version, so users can build without running the build_all script

--- plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
@@ -7,7 +7,7 @@ namespace eIDViewer
     {
         public static string AboutMessage()
         {
-            string message = @"eID Viewer 4.2.1
+            string message = @"eID Viewer 4.2.2
 eID Middleware Project
 Copyright(C) 2017 Fedict
 
