58d1be06b0dacc39473cd4886c63d8fbaa2dd05e
Frederikus <Frederik.v*******@f*****.be>
2017-02-28 11:38:05 +0100

    Added visual studio merge module to eID Viewer msi installer

--- plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
@@ -7,7 +7,7 @@ namespace eIDViewer
     {
         public static string AboutMessage()
         {
-            string message = @"eID Viewer 4.2.2
+            string message = @"eID Viewer 4.2.4
 eID Middleware Project
 Copyright(C) 2017 Fedict
 
