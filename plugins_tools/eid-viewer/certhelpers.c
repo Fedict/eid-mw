c2237aca70b6082c4f35f4884e02ab4ae7c97905
Wouter Verhelst <wouter.verhelst@fedict.be>
2017-03-09 15:36:31 +0100

    Improve data validation code
    Some CA4 cards are re-signed CA3 ones. If so, the photo hash is still SHA1, but everything else is SHA256.
    
    Allow for such cards in data validation.

--- plugins_tools/eid-viewer/certhelpers.c
+++ plugins_tools/eid-viewer/certhelpers.c
@@ -159,8 +159,12 @@ static int eid_vwr_check_data_validity(const void* photo, int plen,
 	 * Calculate the hash and verify the signature */
 	hash(datafile, datfilelen, digest);
 	if(RSA_verify(nid, digest, hashlen, datasig, datsiglen, EVP_PKEY_get1_RSA(pubkey)) != 1) {
-		be_log(EID_VWR_LOG_COARSE, "Could not verify data validity: data signature invalid!");
-		return 0;
+		/* Some CA4 cards are re-signed CA3 ones where the photo hash is still SHA1, but everything else is SHA256. Try if this is such a card */
+		SHA256(datafile, datfilelen, digest);
+		if(RSA_verify(NID_sha256, digest, 32, datasig, datsiglen, EVP_PKEY_get1_RSA(pubkey)) != 1) {
+			be_log(EID_VWR_LOG_COARSE, "Could not verify data validity: data signature invalid!");
+			return 0;
+		}
 	}
 
 	address_data = calloc(addfilelen + datsiglen, 1);
@@ -180,8 +184,12 @@ static int eid_vwr_check_data_validity(const void* photo, int plen,
 	hash(address_data, (ptr - address_data) + datsiglen, digest);
 	free(address_data);
 	if(RSA_verify(nid, digest, hashlen, addrsig, addsiglen, EVP_PKEY_get1_RSA(pubkey)) != 1) {
-		be_log(EID_VWR_LOG_COARSE, "Could not verify data validity: address signature invalid!");
-		return 0;
+		/* Some CA4 cards are re-signed CA3 ones where the photo hash is still SHA1, but everything else is SHA256. Try if this is such a card */
+		SHA256(datafile, datfilelen, digest);
+		if(RSA_verify(NID_sha256, digest, 32, addrsig, addsiglen, EVP_PKEY_get1_RSA(pubkey)) != 1) {
+			be_log(EID_VWR_LOG_COARSE, "Could not verify data validity: address signature invalid!");
+			return 0;
+		}
 	}
 
 	return 1;
