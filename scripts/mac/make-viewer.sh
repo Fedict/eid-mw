0b951298c1a9f2edb3b6809d189e9c5c8b8f8832
Wouter Verhelst <wouter.verhelst@fedict.be>
2017-05-11 11:16:44 +0200

    Add script to build the viewer.
    Needs Xcode 8 to work, though.

---
+++ scripts/mac/make-viewer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rm -rf release-viewer
+mkdir -p release-viewer
+rm tmp-eidviewer.dmg
+
+pushd $(dirname $0)
+
+. set_eidmw_version.sh
+
+pushd "../../plugins_tools/eid-viewer/OSX/eID Viewer"
+xcodebuild -project "eID Viewer.xcodeproj" clean
+xcodebuild -project "eID Viewer.xcodeproj" -target "eID Viewer" -configuration Release
+popd
+
+hdiutil create -srcdir release-viewer -volname "eID Viewer" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW -size 100m "tmp-eidviewer.dmg"
+DEVNAME=$(hdiutil attach -readwrite -noverify -noautoopen "tmp-eidviewer.dmg" | egrep '^/dev/' | sed 1q | awk '{print $1}')
+mkdir -p "/Volumes/eID Viewer/.background/"
+cp -a ../../installers/eid-viewer/mac/bg.png "/Volumes/eID Viewer/.background/"
+cp -a "../../plugins_tools/eid-viewer/OSX/eID Viewer/build/Release/eID Viewer.app" "/Volumes/eID Viewer/"
+ln -s /Applications "/Volumes/eID Viewer/ "
+/usr/bin/osascript "../../installers/eid-viewer/mac/setlayout.applescript" "eID Viewer" || true
+sleep 4
+hdiutil detach $DEVNAME
+hdiutil convert tmp-eidviewer.dmg -format UDBZ -o "eID Viewer-$REL_VERSION.dmg"
+popd
