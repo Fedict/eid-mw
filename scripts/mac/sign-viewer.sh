7e10c4284517bb31c076df957cb73d75efc78192
Frederik <Frederik@Frederiks-MacBook-Pro.local>
2017-05-16 15:06:42 +0200

    Fixed path in mac sign-viewer script

--- scripts/mac/sign-viewer.sh
+++ scripts/mac/sign-viewer.sh
@@ -4,9 +4,9 @@
 
 hdiutil convert "eID Viewer-$REL_VERSION.dmg" -format UDRW -o "tmp-eidviewer.dmg"
 DEVNAME=$(hdiutil attach -readwrite -noverify -noautoopen "tmp-eidviewer.dmg" | egrep '^/dev/' | sed 1q | awk '{print $1}')
-codesign -s "Developer ID Application" "/Volumes/eID Viewer.app/Contents/Library/QuickLook/eidfilepreview.qlgenerator/"
-codesign -s "Developer ID Application" "/Volumes/eID Viewer.app/Contents/Frameworks/BeidView.framework/Versions/A/Frameworks/libbeidpkcs11.$REL_VERSION.dylib"
-codesign -s "Developer ID Application" "/Volumes/eID Viewer.app/Contents/Frameworks/BeidView.framework"
-codesign -s "Developer ID Application" "/Volumes/eID Viewer.app"
+codesign -s "Developer ID Application" "/Volumes/eID Viewer/eID Viewer.app/Contents/Library/QuickLook/eidfilepreview.qlgenerator/"
+codesign -s "Developer ID Application" "/Volumes/eID Viewer/eID Viewer.app/Contents/Frameworks/BeidView.framework/Versions/A/Frameworks/libbeidpkcs11.$REL_VERSION.dylib"
+codesign -s "Developer ID Application" "/Volumes/eID Viewer/eID Viewer.app/Contents/Frameworks/BeidView.framework"
+codesign -s "Developer ID Application" "/Volumes/eID Viewer/eID Viewer.app"
 hdiutil detach $DEVNAME
 hdiutil convert tmp-eidviewer.dmg -format UDBZ -o "eID Viewer-$REL_VERSION-signed.dmg"
