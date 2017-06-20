6fb79381322635fd353b0e50626a93a4dcd34868
Wouter Verhelst <wouter.verhelst@fedict.be>
2017-06-20 10:46:40 +0200

    Reflow this a bit so it gets readable again

--- plugins_tools/eid-viewer/OSX/eID Viewer/eID Viewer/CertificateStore.h
+++ plugins_tools/eid-viewer/OSX/eID Viewer/eID Viewer/CertificateStore.h
@@ -10,25 +10,20 @@
 #import "binhandler.h"
 #include <eid-viewer/certhelpers.h>
 
-typedef NS_ENUM(NSInteger, eIDDumpType)
-{
-eIDDumpTypeDer = DUMP_DER, eIDDumpTypePem = DUMP_PEM,};
+typedef NS_ENUM(NSInteger, eIDDumpType) {
+	eIDDumpTypeDer = DUMP_DER,
+ 	eIDDumpTypePem = DUMP_PEM,
+};
 
-@interface CertificateStore:NSObject < NSOutlineViewDataSource, NSOutlineViewDelegate, binhandler >
-	@property NSMutableDictionary *
-	CertificateData;
-@property NSOutlineView * ov;
-@property AppDelegate * ui;
--(instancetype) initWithOutlineView:(NSOutlineView *) view;
--(NSData *) certificateForKey:(NSString *) key;
--(void) dumpFile:(int)
-     fd forKey:(NSString *)
-     key withFormat:(eIDDumpType) format;
-
--(NSString *) fileNameForKey:(NSString *) key;
--(NSString *) keyForParent:(NSString *) key;
+@interface CertificateStore:NSObject<NSOutlineViewDataSource, NSOutlineViewDelegate, binhandler>
+@property NSMutableDictionary *CertificateData;
+@property NSOutlineView *ov;
+@property AppDelegate *ui;
+-(instancetype) initWithOutlineView:(NSOutlineView *)view;
+-(NSData *) certificateForKey:(NSString *)key;
+-(void) dumpFile:(int)fd forKey:(NSString *)key withFormat:(eIDDumpType) format;
+-(NSString *) fileNameForKey:(NSString *)key;
+-(NSString *) keyForParent:(NSString *)key;
 -(void) clear;
--(void) setValid:(eIDResult)
-     valid forKey:(NSString *) key;
-
+-(void) setValid:(eIDResult)valid forKey:(NSString *)key;
 @end
