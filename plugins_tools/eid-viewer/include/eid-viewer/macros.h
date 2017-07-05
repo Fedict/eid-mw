bf978a1252d2710ae943b50913717b7ecc1f763e
Wouter Verhelst <wouter.verhelst@fedict.be>
2017-07-05 17:38:27 +0200

    Tell the optimizer that this never returns
    "__attribute__((noreturn))" tells gcc that this function will run
    "forever", and that it can never return. This allows GCC to drop certain
    initialisation and stack allocations, resulting in a slight performance
    improvement.
    
    Won't make much of a difference, but we might as well use it.

--- plugins_tools/eid-viewer/include/eid-viewer/macros.h
+++ plugins_tools/eid-viewer/include/eid-viewer/macros.h
@@ -4,10 +4,12 @@
 #ifdef WIN32
 #define DllExport __declspec( dllexport )
 #define SLEEP(x) Sleep(x*1000)
+#define NORETURN
 #else
 #include <unistd.h>
 #define DllExport __attribute__((visibility("default")))
 #define SLEEP(x) sleep(x)
+#define NORETURN __attribute__((noreturn))
 #endif
 
 #endif
