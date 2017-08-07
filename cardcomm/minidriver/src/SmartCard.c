329a29b3ad761c59d48e3d4fd0703aec8ccf9e7a
Frederik <Frederik.vernelen@fedict>
2017-08-07 14:39:29 +0200

    removed compiler warning: re-usage of name in inner scope

--- cardcomm/minidriver/src/SmartCard.c
+++ cardcomm/minidriver/src/SmartCard.c
@@ -731,7 +731,7 @@ cleanup:
 	if (DialogThreadHandle != NULL)
 	{
 		DWORD dwExitCode = 0;
-		for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
 		{
 			if (GetExitCodeThread(DialogThreadHandle,&dwExitCode))
 			{
