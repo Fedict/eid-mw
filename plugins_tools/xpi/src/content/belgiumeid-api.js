a06664e795ed82031d2a2cad77b03b1eaf8ef82a
Wouter Verhelst <wouter.verhelst@fedict.be>
2017-08-16 16:24:36 +0200

    Mark add-on as compatible with more recent versions of firefox
    For some reason, firefox stopped marking this add-on as compatible with
    recent versions. Makes no sense, so fix.

--- plugins_tools/xpi/src/content/belgiumeid-api.js
+++ plugins_tools/xpi/src/content/belgiumeid-api.js
@@ -5,5 +5,5 @@ if(!be.fedict.belgiumeid.exists) be.fedict.belgiumeid.exists = true;
 if(!be.fedict.belgiumeid.getVersion) be.fedict.belgiumeid.getVersion = function() {
 	// TODO: use content scripts and similar things to query version from
 	// actual add-on. Later.
-	return "1.0.23";
+	return "1.0.24";
 };
