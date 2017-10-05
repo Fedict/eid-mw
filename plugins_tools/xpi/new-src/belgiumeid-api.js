66a72545273326621b0c3790753778bf5094e692
Wouter Verhelst <wouter.verhelst@fedict.be>
2017-10-05 13:57:33 +0200

    Whoops, this file should not be here

--- plugins_tools/xpi/new-src/belgiumeid-api.js
+++
@@ -1,9 +0,0 @@
-if(!be) var be={};
-if(!be.fedict) be.fedict={};
-if(!be.fedict.belgiumeid) be.fedict.belgiumeid={};
-if(!be.fedict.belgiumeid.exists) be.fedict.belgiumeid.exists = true;
-if(!be.fedict.belgiumeid.getVersion) be.fedict.belgiumeid.getVersion = function() {
-	// TODO: use content scripts and similar things to query version from
-	// actual add-on. Later.
-	return "1.0.24";
-};
