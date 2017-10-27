1a3164bf9393fbf255cee0ab05047d6554bcdb7e
Wouter Verhelst <wouter.verhelst@fedict.be>
2017-10-27 13:22:10 +0200

    Add the same changes to the make-viewer.sh script, too

--- scripts/mac/make-viewer.sh
+++ scripts/mac/make-viewer.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$MAC_BUILD_CONFIG" ]
+then
+	MAC_BUILD_CONFIG=Release
+fi
+
 pushd $(dirname $0)
 
 . set_eidmw_version.sh
@@ -11,14 +16,14 @@ rm -f "eID Viewer-$REL_VERSION.dmg"
 
 pushd "../../plugins_tools/eid-viewer/OSX/eID Viewer"
 xcodebuild -project "eID Viewer.xcodeproj" clean
-xcodebuild -project "eID Viewer.xcodeproj" -target "eID Viewer" -configuration Release
+xcodebuild -project "eID Viewer.xcodeproj" -target "eID Viewer" -configuration $MAC_BUILD_CONFIG
 popd
 
 hdiutil create -srcdir release-viewer -volname "eID Viewer" -fs HFS+ -fsargs "-c c=64,a=16,e=16" -format UDRW -size 100m "tmp-eidviewer.dmg"
 DEVNAME=$(hdiutil attach -readwrite -noverify -noautoopen "tmp-eidviewer.dmg" | egrep '^/dev/' | sed 1q | awk '{print $1}')
 mkdir -p "/Volumes/eID Viewer/.background/"
 cp -a ../../installers/eid-viewer/mac/bg.png "/Volumes/eID Viewer/.background/"
-cp -a "../../plugins_tools/eid-viewer/OSX/eID Viewer/build/Release/eID Viewer.app" "/Volumes/eID Viewer/"
+cp -a "../../plugins_tools/eid-viewer/OSX/eID Viewer/build/$MAC_BUILD_CONFIG/eID Viewer.app" "/Volumes/eID Viewer/"
 ln -s /Applications "/Volumes/eID Viewer/ "
 /usr/bin/osascript "../../installers/eid-viewer/mac/setlayout.applescript" "eID Viewer" || true
 sleep 4
