77e13470d171d5b9d32327c56cade7e140601b85
Wouter Verhelst <wouter.verhelst@fedict.be>
2018-03-29 11:41:44 +0200

    Fix some bugs in multi-hash card handling
    Cards with a SHA1 photo hash and a SHA256 signature hash exist in the
    wild, but apparently our code never ran against them and it does not
    work.
    
    The current example still does not work, but at least this should be
    some improvement.

--- plugins_tools/eid-viewer/certhelpers.c
+++ plugins_tools/eid-viewer/certhelpers.c
@@ -161,7 +161,8 @@ static int eid_vwr_check_data_validity(const void* photo, int plen,
 	hash(datafile, datfilelen, digest);
 	if(RSA_verify(nid, digest, hashlen, datasig, datsiglen, EVP_PKEY_get1_RSA(pubkey)) != 1) {
 		/* Some CA4 cards are re-signed CA3 ones where the photo hash is still SHA1, but everything else is SHA256. Try if this is such a card */
-		SHA256(datafile, datfilelen, digest);
+		hash = SHA256; nid = NID_sha256; hashlen = 32;
+		hash(datafile, datfilelen, digest);
 		if(RSA_verify(NID_sha256, digest, 32, datasig, datsiglen, EVP_PKEY_get1_RSA(pubkey)) != 1) {
 			be_log(EID_VWR_LOG_COARSE, "Could not verify data validity: data signature invalid!");
 			return 0;
@@ -183,15 +184,11 @@ static int eid_vwr_check_data_validity(const void* photo, int plen,
 	ptr++;
 	memcpy(ptr, datasig, datsiglen);
 	hash(address_data, (ptr - address_data) + datsiglen, digest);
-	free(address_data);
 	if(RSA_verify(nid, digest, hashlen, addrsig, addsiglen, EVP_PKEY_get1_RSA(pubkey)) != 1) {
-		/* Some CA4 cards are re-signed CA3 ones where the photo hash is still SHA1, but everything else is SHA256. Try if this is such a card */
-		SHA256(datafile, datfilelen, digest);
-		if(RSA_verify(NID_sha256, digest, 32, addrsig, addsiglen, EVP_PKEY_get1_RSA(pubkey)) != 1) {
-			be_log(EID_VWR_LOG_COARSE, "Could not verify data validity: address signature invalid!");
-			return 0;
-		}
+		be_log(EID_VWR_LOG_COARSE, "Could not verify data validity: address signature invalid!");
+		return 0;
 	}
+	free(address_data);
 
 	return 1;
 }
