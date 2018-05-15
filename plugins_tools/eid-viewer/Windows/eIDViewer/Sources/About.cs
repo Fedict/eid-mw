bc7c5a356c0c57791c1ccadea84072acdee814d6
Frederik <Frederik.vernelen@fedict>
2018-05-15 10:35:35 +0200

    Claim version number 4.4.3 for windows viewer

--- plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
@@ -7,9 +7,9 @@ namespace eIDViewer
     {
         public static string AboutMessage()
         {
-            string message = @"eID Viewer 4.3.9
+            string message = @"eID Viewer 4.4.3
 eID Middleware Project
-Copyright(C) 2018 BOSA
+Copyright(C) 2018 Bosa
 
 By Frederik Vernelen and Wouter Verhelst
 Based on the design of Frank Marien
