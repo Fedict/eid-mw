4ea085bd40ef2a565efd1e83321f90615be97753
Frederik <Frederik.vernelen@fedict>
2018-06-13 13:31:23 +0200

    Don't mix char and wchar

--- plugins_tools/eid-viewer/conversions/xmlfileversconv.cpp
+++ plugins_tools/eid-viewer/conversions/xmlfileversconv.cpp
@@ -13,9 +13,9 @@ XmlFileversConvertor::XmlFileversConvertor() {
 }
 
 EID_STRING XmlFileversConvertor::convert(const void *original) {
-	EID_STRING orig((const char*)original);
+	EID_STRING orig((const EID_CHAR*)original);
 
-	if(orig.compare("4.1") != 0) {
+	if(orig.compare(TEXT("4.1")) != 0) {
 		be_log(EID_VWR_LOG_ERROR, msgs_[target_]);
 	}
 	return orig;
