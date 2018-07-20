8080beff7382eee659d7e9c05400f1bc9aa3d3b8
Developer <developer@developers-macbook-pro.local>
2018-07-20 15:32:49 +0200

    remove unneeded includes

--- plugins_tools/eid-viewer/conversions/toxmlmemberoffamilyconv.cpp
+++ plugins_tools/eid-viewer/conversions/toxmlmemberoffamilyconv.cpp
@@ -1,7 +1,4 @@
 #include "toxmlmemberoffamilyconv.h"
-#include <iostream>
-#include <sstream>
-#include <iomanip>
 
 //no value of this field is stored on the card, so if present, return "1" for true
 EID_STRING ToXmlMemberOfFamilyConvertor::convert(const void *normal)
