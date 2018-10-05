b960250b1b05e7e44862adeb1ee22772f7f7e8dd
Frederik <Frederik.vernelen@fedict>
2018-10-05 14:53:51 +0200

    eID Viewer backend: special status convertor: convert "0" (NO STATUS) to "-" in stead of "" as requested by RR
    (cherry picked from commit 4d0bc1b9f0fa373612ffb6b3ebcd9d13b593f5e7)

--- plugins_tools/eid-viewer/conversions/specconv.cpp
+++ plugins_tools/eid-viewer/conversions/specconv.cpp
@@ -2,28 +2,28 @@
 
 SpecConvertor::SpecConvertor()
 {
-	conversions[EID_VWR_LANG_DE]['0'] = TEXT("");
+	conversions[EID_VWR_LANG_DE]['0'] = TEXT("-");
 	conversions[EID_VWR_LANG_DE]['1'] = TEXT("Weißer Stock");
 	conversions[EID_VWR_LANG_DE]['2'] = TEXT("Verlängerte Minderjährigkeit");
 	conversions[EID_VWR_LANG_DE]['3'] = TEXT("Weißer Stock\nVerlängerte Minderjährigkeit");
 	conversions[EID_VWR_LANG_DE]['4'] = TEXT("Gelber Stock");
 	conversions[EID_VWR_LANG_DE]['5'] = TEXT("Gelber Stock\nVerlängerte Minderjährigkeit");
 
-	conversions[EID_VWR_LANG_EN]['0'] = TEXT("");
+	conversions[EID_VWR_LANG_EN]['0'] = TEXT("-");
 	conversions[EID_VWR_LANG_EN]['1'] = TEXT("White Cane");
 	conversions[EID_VWR_LANG_EN]['2'] = TEXT("Extended Nonage");
 	conversions[EID_VWR_LANG_EN]['3'] = TEXT("White Cane\nExtended Nonage");
 	conversions[EID_VWR_LANG_EN]['4'] = TEXT("Yellow Cane");
 	conversions[EID_VWR_LANG_EN]['5'] = TEXT("Yellow Cane\nExtended Nonage");
 
-	conversions[EID_VWR_LANG_FR]['0'] = TEXT("");
+	conversions[EID_VWR_LANG_FR]['0'] = TEXT("-");
 	conversions[EID_VWR_LANG_FR]['1'] = TEXT("Canne blanche");
 	conversions[EID_VWR_LANG_FR]['2'] = TEXT("Minorité étendue");
 	conversions[EID_VWR_LANG_FR]['3'] = TEXT("Canne blanche\nMinorité étendue");
 	conversions[EID_VWR_LANG_FR]['4'] = TEXT("Canne jaune");
 	conversions[EID_VWR_LANG_FR]['5'] = TEXT("Canne jaune\nMinorité étendue");
 
-	conversions[EID_VWR_LANG_NL]['0'] = TEXT("");
+	conversions[EID_VWR_LANG_NL]['0'] = TEXT("-");
 	conversions[EID_VWR_LANG_NL]['1'] = TEXT("Witte stok");
 	conversions[EID_VWR_LANG_NL]['2'] = TEXT("Verlengde minderjarigheid");
 	conversions[EID_VWR_LANG_NL]['3'] = TEXT("Witte stok\nVerlengde minderjarigheid");
