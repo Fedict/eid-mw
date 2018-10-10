487134639493bde0874e8550f88ee5efd40fb0e7
Frederik <Frederik.vernelen@fedict>
2018-10-10 16:17:58 +0200

    eID Viewer windows: don't use a separate "print_enabled" state, but allow printing whenever eid data is ready.
    (cherry picked from commit e758936984261d632b8e6abc9fce8064b7f8711d)

--- plugins_tools/eid-viewer/Windows/eIDViewer/Sources/BackendDataViewModel.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/Sources/BackendDataViewModel.cs
@@ -1014,7 +1014,6 @@ namespace eIDViewer
             HideProgressBar();
             foreigner_fields_height = 0;
             pinop_ready = false;
-            print_enabled = false;
             open_enabled = true;
 
             photo = null;
@@ -1683,17 +1682,6 @@ namespace eIDViewer
             {
                 _pinop_ready = value;
                 this.NotifyPropertyChanged("pinop_ready");
-            }
-        }
-
-        private Boolean _print_enabled = false;
-        public Boolean print_enabled
-        {
-            get { return _print_enabled; }
-            set
-            {
-                _print_enabled = value;
-                this.NotifyPropertyChanged("print_enabled");
             }
         }
 
