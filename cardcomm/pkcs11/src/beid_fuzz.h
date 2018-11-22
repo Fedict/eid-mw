Fedict/eid-mw

commit 29124c1c15ee3714a549f4f592c27df1b9b39344

Do fuzzing with libFuzzer
In order to be able to fuzz, we need to instrument our libraries, which
requires a few extra compiler flags. Add them, but only if
--enable-test-fuzzer is provided to configure. Also, add a function to
inject the fuzzed data in that case.

libFuzzer also requires a function that injects the random data and then
runs some code. Write it, in a generic way so we can reuse it for other
stuff later.

TODO: try different fuzzers (AFL, perhaps) and different files (not just
the ID file). Later.

cardcomm/pkcs11/src/beid_fuzz.h
@@ -1,0 +1,15 @@
+#ifndef BEID_FUZZ_H
+#define BEID_FUZZ_H
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+#include "common/export.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+EIDMW_CAL_API void beid_set_fuzz_data(const uint8_t *data, size_t size, const char* filename);
+#ifdef __cplusplus
+}
+#endif
+#else
+#error this header is only meant for fuzzing
+#endif
+#endif

Makefile.am
@@ -1,7 +1,7 @@
 ACLOCAL_AMFLAGS = -I scripts/m4
 EXTRA_DIST = scripts/build-aux/config.rpath scripts/build-aux/genver.sh .version debian rpm doc
 
-SUBDIRS=cardcomm/pkcs11/src doc/sdk/include/rsaref220 plugins_tools/util tests/unit plugins_tools/xpi plugins_tools/chrome_pkcs11
+SUBDIRS=cardcomm/pkcs11/src doc/sdk/include/rsaref220 plugins_tools/util tests/unit plugins_tools/xpi plugins_tools/chrome_pkcs11 tests/fuzz
 
 if GTK
 SUBDIRS += plugins_tools/aboutmw/gtk plugins_tools/eid-viewer

cardcomm/pkcs11/src/Makefile.am
@@ -1,6 +1,6 @@
 lib_LTLIBRARIES = libbeidpkcs11.la
-AM_CFLAGS = -Wall -Wextra -Wno-unused-parameter
-AM_CXXFLAGS = -Wall -Wextra -Wno-unused-parameter -std=c++98
+AM_CFLAGS = -Wall -Wextra -Wno-unused-parameter @FUZZING@
+AM_CXXFLAGS = -Wall -Wextra -Wno-unused-parameter -std=c++98 @FUZZING@
 libbeidpkcs11_la_CFLAGS = $(AM_CFLAGS) -DLTC_NO_ASM
 libbeidpkcs11_la_CXXFLAGS = $(AM_CXXFLAGS) -DUSING_DL_OPEN -DEIDMW_CAL_EXPORT -DCAL_BEID -DCARDPLUGIN_IN_CAL -DBEID_35 -DNDEBUG -DBEID_OLD_PINPAD -DLTC_NO_ASM -fvisibility=hidden -I$(srcdir)/common -I$(srcdir)/cardlayer -I$(top_srcdir)/doc/sdk/include/rsaref220 @PCSC_CFLAGS@
 libbeidpkcs11_la_CPPFLAGS = -I$(srcdir)/common -I$(srcdir)/cardlayer -I$(top_srcdir)/doc/sdk/include/rsaref220 @PCSC_CFLAGS@ -DLIBEXECDIR='"$(libexecdir)"'

cardcomm/pkcs11/src/cal.cpp
@@ -31,6 +31,7 @@
 #include "mw_util.h"
 #include "tlvbuffer.h"
 #include "thread.h"
+#include "beid_fuzz.h"
 
 #ifndef WIN32
 #define strcpy_s(a,b,c)         strcpy((a),(c))

cardcomm/pkcs11/src/cardlayer/card.cpp
@@ -22,6 +22,17 @@
 #include "thread.h"
 #include "common/log.h"
 
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+static std::string fuzz_path = "";
+static eIDMW::CByteArray fuzz_data = eIDMW::CByteArray();
+extern "C" {
+	void beid_set_fuzz_data(const uint8_t *data, size_t size, const char *path) {
+		fuzz_path = path;
+		fuzz_data = eIDMW::CByteArray(data, size);
+	}
+}
+#endif
+
 namespace eIDMW
 {
 
@@ -151,6 +162,11 @@ namespace eIDMW
 				   unsigned long ulOffset,
 				   unsigned long ulMaxLen, bool bDoNotCache)
 	{
+#ifdef FUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION
+		if(csPath == fuzz_path) {
+			return fuzz_data;
+		}
+#endif
 		return ReadUncachedFile(csPath, ulOffset, ulMaxLen);
 	}
 

cardcomm/pkcs11/src/cardlayer/card.h
@@ -39,6 +39,11 @@
 #include "p15correction.h"
 #include "dialogs/dialogs.h"
 
+#include <stddef.h>
+#include <stdint.h>
+
+#include "beid_fuzz.h"
+
 namespace eIDMW
 {
 	/** Compatibility version for the entire plugin-relevant API: V1.00 */

configure.ac
@@ -37,6 +37,31 @@ CPPFLAGS="$PCSC_CFLAGS $CPPFLAGS"
 LDFLAGS="$PCSC_LIBS $CPPFLAGS"
 ])
 
+AC_ARG_ENABLE([test-fuzzer],AS_HELP_STRING([--enable-test-fuzzer],[Build a version of eid-mw instrumented for fuzzing (not meant for production use!)]),[fuzzer=$withval],[fuzzer=no])
+AC_MSG_CHECKING([whether to build for fuzzing])
+if test "$fuzzer" = "yes"
+then
+	case $CC in
+		*clang*)
+			AC_MSG_RESULT([yes])
+			AC_SUBST(FUZZING,"-fsanitize=fuzzer-no-link -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION")
+			;;
+		*)
+			AC_MSG_ERROR([Fuzzing only works with clang. Please set CC to clang version 6.0 or higher])
+			;;
+	esac
+	case $CXX in
+		*clang++*)
+			;;
+		*)
+			AC_MSG_ERROR([Fuzzing only works with clang. Please set CXX to clang version 6.0 or higher])
+			;;
+	esac
+else
+	AC_MSG_RESULT([no])
+fi
+AM_CONDITIONAL([FUZZING], [test x$fuzzer = xyes])
+
 AC_ARG_WITH([gtkvers],
 	[AS_HELP_STRING([--with-gtkvers],[select GTK version to use [default: 3 if available, falls back to 2 if not]; --without-gtkvers disables GTK altogether [note: this implies --disable-dialogs].])],
 	[gtkvers=$withval],[gtkvers=detect])
@@ -180,6 +205,7 @@ AC_CONFIG_FILES([Makefile
 		 cardcomm/pkcs11/src/dialogs/dialogsgtk/po/Makefile.in
 		 doc/sdk/include/rsaref220/Makefile
 		 tests/unit/Makefile
+		 tests/fuzz/Makefile
 		 plugins_tools/util/Makefile
 		 plugins_tools/aboutmw/gtk/Makefile
 		 plugins_tools/aboutmw/gtk/po/Makefile.in

plugins_tools/aboutmw/gtk/Makefile.am
@@ -2,7 +2,7 @@ bin_PROGRAMS = about-eid-mw
 
 about_eid_mw_SOURCES = about-main.c gettext.h
 about_eid_mw_LDFLAGS = @GTK_LIBS@
-about_eid_mw_CFLAGS = @GTK_CFLAGS@ -DLIBDIR='"$(libdir)"' -DDATAROOTDIR='"$(datarootdir)"'
+about_eid_mw_CFLAGS = @GTK_CFLAGS@ -DLIBDIR='"$(libdir)"' -DDATAROOTDIR='"$(datarootdir)"' @FUZZING@
 
 BUILT_SOURCES = about_glade.h now.h
 EXTRA_DIST = about.glade gen.sh

commit 6e02bfd7753b01040a062c19109c26e1eb7e7ed3

Unbreak the build
Originally, we wanted to only include this when we're actually fuzzing,
but it turned out that wasn't a good idea for various reasons.

So, we started including it everywhere, but then forgot to remove the
explicit compile-time #error

Fixes: 29124c1c15ee3714a549f4f592c27df1b9b39344

cardcomm/pkcs11/src/beid_fuzz.h
@@ -9,7 +9,4 @@ EIDMW_CAL_API void beid_set_fuzz_data(const uint8_t *data, size_t size, const ch
 #ifdef __cplusplus
 }
 #endif
-#else
-#error this header is only meant for fuzzing
-#endif
 #endif