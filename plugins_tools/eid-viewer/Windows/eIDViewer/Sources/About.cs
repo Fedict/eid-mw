e2941c942d9fb0418cf8e254a86eaee8db5bb329
Frederik <Frederik.vernelen@fedict>
2018-11-28 16:36:56 +0100

    auto-generated (version) info file

--- plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
@@ -7,7 +7,7 @@ namespace eIDViewer
     {
         public static string AboutMessage()
         {
-            string message = @"eID Viewer 4.4.10
+            string message = @"eID Viewer 4.4.12
 eID Middleware Project
 Copyright(C) 2018 Bosa
 
