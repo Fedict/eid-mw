f1580a944cb4bcd9d259fa2a79b98041dff2b3c0
Frederik <Frederik.vernelen@fedict>
2018-12-19 10:00:01 +0100

    Removed unused configuration settings

--- cardcomm/pkcs11/src/common/configuration.h
+++ cardcomm/pkcs11/src/common/configuration.h
@@ -130,13 +130,10 @@ replace $install by string in install parameter
 
 #define EIDMW_CNF_SECTION_GENERAL       L"general"	//section with the general pupose parameters
 #define EIDMW_CNF_GENERAL_INSTALLDIR    L"install_dirname"	//string, directory of the basic eid software; “c:/Program Files/beid/”
-#define EIDMW_CNF_GENERAL_INSTALL_PRO_DIR L"install_pro_dirname"	//string, directory of the pro eid software; “c:/Program Files/beid/”
-#define EIDMW_CNF_GENERAL_INSTALL_SDK_DIR L"install_sdk_dirname"	//string, directory of the sdk eid software; “c:/Program Files/beid/”
-#define EIDMW_CNF_GENERAL_CACHEDIR      L"cache_dirname"	//string, cache directory for card-file; $common/beid/crlcache/
 #define EIDMW_CNF_GENERAL_LANGUAGE      L"language"	//string, user lanugage: fr, nl, en, de, es, …
+
 #define EIDMW_CNF_GENERAL_CARDTXDELAY   L"card_transmit_delay"	//number, delay while communicating with the smartcard, in mili-seconds, default 1 mSec
 #define EIDMW_CNF_GENERAL_CARDCONNDELAY L"card_connect_delay"	//number, delay before connecting to a smartcard, in mili-seconds, default 0 mSec
-#define EIDMW_CNF_GENERAL_BUILDNBR		L"build_number"	//Number of the installed build
 
 #define EIDMW_CNF_SECTION_LOGGING       L"logging"	//section with the logging parameters
 #define EIDMW_CNF_LOGGING_DIRNAME       L"log_dirname"	//string, location of the log-file; $home/beid/ Full path with volume name.
@@ -146,54 +143,13 @@ replace $install by string in install parameter
 #define EIDMW_CNF_LOGGING_LEVEL         L"log_level"	//string, Specify what should be logged; none, critical, error, warning, info or debug
 #define EIDMW_CNF_LOGGING_GROUP			L"log_group_in_new_file"	//number; 0=no (default), 1=yes (create on log file by module)
 
-#define EIDMW_CNF_SECTION_CRL           L"crl"	//section with the crl parameters
-#define EIDMW_CNF_CRL_SERVDOWNLOADNR    L"crl_service_download_nr"	//number
-#define EIDMW_CNF_CRL_TIMEOUT           L"crl_download_timeout"	//number, in seconds ??
-#define EIDMW_CNF_CRL_CACHEFILE         L"crl_cachefile"	//string
-#define EIDMW_CNF_CRL_CACHEDIR          L"crl_cachedir"	//string    “$common/crl/
-#define EIDMW_CNF_CRL_LOCKFILE          L"crl_lockfile"	//string
-
-#define EIDMW_CNF_SECTION_CERTVALID     L"certificatevalidation"	//section with the certificate validation parameters
-#define EIDMW_CNF_CERTVALID_ALLOWTESTC  L"cert_allow_testcard"	//number; 0=no, 1=yes
-#define EIDMW_CNF_CERTVALID_CRL			L"cert_validation_crl"	//number; 0=no, 1=optional, 2=always
-#define EIDMW_CNF_CERTVALID_OCSP		L"cert_validation_ocsp"	//number; 0=no, 1=optional, 2=always
-
-#define EIDMW_CNF_SECTION_CERTCACHE     L"certificatecache"	//section with the certificate cache parameters
-#define EIDMW_CNF_CERTCACHE_CACHEFILE   L"cert_cachefile"	//number; 0=no, 1=yes, If true, will read certain files (or most parts of them) on the card only the first time a card is used and cache their contents on disk for later use.
-#define EIDMW_CNF_CERTCACHE_LINENUMB    L"cert_cache_linenumb"	//number
-#define EIDMW_CNF_CERTCACHE_VALIDITY    L"cert_cache_validity"	//number
-#define EIDMW_CNF_CERTCACHE_WAITDELAY   L"cert_cache_waitdelay"	//number
-
-#define EIDMW_CNF_SECTION_PROXY         L"proxy"	//section with the proxy parameters
-#define EIDMW_CNF_PROXY_HOST            L"proxy_host"	//string;
-#define EIDMW_CNF_PROXY_PORT            L"proxy_port"	//number
-#define EIDMW_CNF_PROXY_PACFILE         L"proxy_pacfile"	//string;
-#define EIDMW_CNF_PROXY_CONNECT_TIMEOUT  L"connect_timeout"	//number
-
 #define EIDMW_CNF_SECTION_SECURITY      L"security"	//section with the security parameters
+
 #define EIDMW_CNF_SECURITY_SINGLESIGNON L"single_signon"	//number; 0=no, 1=yes; If yes, the PIN is requested by the driver and asked only once with multiple applications.
 
 #define EIDMW_CNF_SECTION_GUITOOL       L"configuretool"	//section with the configure tool parameters
-#define EIDMW_CNF_GUITOOL_STARTWIN      L"start_with_windows"	//number; 0=no(default), 1=yes
-#define EIDMW_CNF_GUITOOL_STARTMINI     L"start_minimized"	//number; 0=no(default), 1=yes
-#define EIDMW_CNF_GUITOOL_SHOWPIC       L"show_picture"	//number; 0=no(default), 1=yes
-#define EIDMW_CNF_GUITOOL_SHOWNOTIFICATION  L"show_notification"	//number; 0=no, 1=yes(default)
-#define EIDMW_CNF_GUITOOL_SHOWTBAR      L"show_toolbar"	//number; 0=no, 1=yes
+
 #define EIDMW_CNF_GUITOOL_VIRTUALKBD    L"use_virtual_keypad"	//number; 0=no, 1=yes
-#define EIDMW_CNF_GUITOOL_AUTOCARDREAD  L"automatic_cardreading"	//number; 0=no, 1=yes(default)
-#define EIDMW_CNF_GUITOOL_CARDREADNUMB  L"cardreader"	//number; -1(not specified), 0-10
-#define EIDMW_CNF_GUITOOL_REGCERTIF     L"registrate_certificate"	//number; 0=no, 1=yes(default)
-#define EIDMW_CNF_GUITOOL_REMOVECERTIF  L"remove_certificate"	//number; 0=no, 1=yes(default)
-#define EIDMW_CNF_GUITOOL_FILESAVE		L"default_save_path"	//string; path, ""(default)
-
-#define EIDMW_CNF_SECTION_XSIGN			L"xsign"	//section for xsign
-#define EIDMW_CNF_XSIGN_TSAURL			L"tsa_url"	//string url of the tsa
-#define EIDMW_CNF_XSIGN_ONLINE			L"online"	//number; 0=no(default), 1=yes
-#define EIDMW_CNF_XSIGN_WORKINGDIR		L"working_dir"	//string working directory
-#define EIDMW_CNF_XSIGN_TIMEOUT			L"timeout"	//number;
-
-#define EIDMW_CNF_SECTION_MESSAGES	L"messages"	//section for messages
-#define EIDMW_CNF_MESSAGES_SHOWSDK35WARN		L"show_sdk35_warning"	//number; 0=no, 1=yes
 
 namespace eIDMW
 {
@@ -230,8 +186,8 @@ public:
 			long lDefault;
 		};
 
-		          CConfig();
-		         ~CConfig();
+		CConfig();
+		~CConfig();
 
 #ifndef WIN32
 		static void Init();
@@ -307,111 +263,24 @@ public:
 				    const std::wstring & czSection);
 
 		//GENERAL
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_GENERAL_INSTALLDIR;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_GENERAL_INSTALL_PRO_DIR;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_GENERAL_INSTALL_SDK_DIR;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_GENERAL_CACHEDIR;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_GENERAL_LANGUAGE;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GENERAL_CARDTXDELAY;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GENERAL_CARDCONNDELAY;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GENERAL_BUILDNBR;
+		static const struct Param_Str EIDMW_CONFIG_PARAM_GENERAL_INSTALLDIR;
+		static const struct Param_Str EIDMW_CONFIG_PARAM_GENERAL_LANGUAGE;
+		static const struct Param_Num EIDMW_CONFIG_PARAM_GENERAL_CARDTXDELAY;
+		static const struct Param_Num EIDMW_CONFIG_PARAM_GENERAL_CARDCONNDELAY;
 
 		//LOGGING
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_LOGGING_DIRNAME;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_LOGGING_PREFIX;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_LOGGING_FILENUMBER;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_LOGGING_FILESIZE;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_LOGGING_LEVEL;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_LOGGING_GROUP;
-
-		//CRL
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_CRL_SERVDOWNLOADNR;
-		static const struct Param_Num EIDMW_CONFIG_PARAM_CRL_TIMEOUT;
-		static const struct Param_Str EIDMW_CONFIG_PARAM_CRL_CACHEDIR;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_CRL_CACHEFILE;
-		static const struct Param_Str EIDMW_CONFIG_PARAM_CRL_LOCKFILE;
-
-		//CERTIFICATE VALIDATION
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_CERTVALID_ALLOWTESTC;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_CERTVALID_CRL;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_CERTVALID_OCSP;
-
-		//CERTIFICATE CACHE
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_CERTCACHE_CACHEFILE;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_CERTCACHE_LINENUMB;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_CERTCACHE_VALIDITY;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_CERTCACHE_WAITDELAY;
-
-		//PROXY
-		static const struct Param_Str EIDMW_CONFIG_PARAM_PROXY_HOST;
-		static const struct Param_Num EIDMW_CONFIG_PARAM_PROXY_PORT;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_PROXY_PACFILE;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_PROXY_CONNECT_TIMEOUT;
+		static const struct Param_Str EIDMW_CONFIG_PARAM_LOGGING_DIRNAME;
+		static const struct Param_Str EIDMW_CONFIG_PARAM_LOGGING_PREFIX;
+		static const struct Param_Num EIDMW_CONFIG_PARAM_LOGGING_FILENUMBER;
+		static const struct Param_Num EIDMW_CONFIG_PARAM_LOGGING_FILESIZE;
+		static const struct Param_Str EIDMW_CONFIG_PARAM_LOGGING_LEVEL;
+		static const struct Param_Num EIDMW_CONFIG_PARAM_LOGGING_GROUP;
 
 		//SECURITY
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_SECURITY_SINGLESIGNON;
+		static const struct Param_Num EIDMW_CONFIG_PARAM_SECURITY_SINGLESIGNON;
 
 		//GUI
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_STARTWIN;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_STARTMINI;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_SHOWPIC;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_SHOWNOTIFICATION;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_SHOWTBAR;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_VIRTUALKBD;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_AUTOCARDREAD;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_CARDREADNUMB;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_REGCERTIF;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_GUITOOL_REMOVECERTIF;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_GUITOOL_FILESAVE;
-
-		//XSIGN
-		static const struct Param_Str EIDMW_CONFIG_PARAM_XSIGN_TSAURL;
-		static const struct Param_Num EIDMW_CONFIG_PARAM_XSIGN_ONLINE;
-		static const struct Param_Str
-			EIDMW_CONFIG_PARAM_XSIGN_WORKINGDIR;
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_XSIGN_TIMEOUT;
-
-		//MESSAGES
-		static const struct Param_Num
-			EIDMW_CONFIG_PARAM_MESSAGES_SHOWSDK35WARN;
+		static const struct Param_Num EIDMW_CONFIG_PARAM_GUITOOL_VIRTUALKBD;
 
 private:
 		//below info if not needed any more when the ini-file is hard-coded.
@@ -420,9 +289,7 @@ private:
 		static CDataFile o_userDataFile;
 		static CDataFile o_systemDataFile;
 #endif
-		static CMutex m_Mutex;
-
-				/**< Mutex for exclusive access */
+		static CMutex m_Mutex;	/**< Mutex for exclusive access */
 	};
 
 }
