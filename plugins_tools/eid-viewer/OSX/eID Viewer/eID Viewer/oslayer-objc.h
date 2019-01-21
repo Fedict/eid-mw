acf9dd96b945fc478099036e6e0efc778f2b8870
Wouter Verhelst <Wouter.Verhelst@zetes.com>
2019-01-21 10:57:30 +0100

    make drag-and-drop work again
    while at it, fix whitespace in a few files
    
    (cherry picked from commit 2f44200af310497fbb8613c237afdac613261a01)

--- plugins_tools/eid-viewer/OSX/eID Viewer/eID Viewer/oslayer-objc.h
+++ plugins_tools/eid-viewer/OSX/eID Viewer/eID Viewer/oslayer-objc.h
@@ -65,60 +65,37 @@ eIDResultFailed = EID_VWR_RES_FAILED,
 /** \brief Protocol for implementing a UI.
  * \see cbstruct for details on what each method does; these methods are
  * straightforward C-to-ObjC translations */
-@protocol eIDOSLayerUI < NSObject > @optional - (void) newsrc:(eIDSource)
-	which;
-				///< called by eid_vwr_ui_callbacks::newsrc()
-
--(void) newstringdata: (NSString *) data /**< . */ withLabel:(NSString *)
-	label;
-				///< called by eid_vwr_ui_callbacks::newstringdata()
--(void) newbindata:(NSData *)
-     data withLabel:(NSString *) label;	///< called by eid_vwr_ui_callbacks::newbindata()
-
--(void) log:(NSString *)
-     line withLevel:(eIDLogLevel) level;	///< called by eid_vwr_ui_callbacks::log()
-
--(void) newstate:(eIDState) state;
-				///< called by eid_vwr_ui_callbacks::newstate()
--(void) pinop_result:(eIDResult)
-     result forOperation:(eIDPinOp) operation;	///< called by eid_vwr_ui_callbacks::pinop_result()
-
--(void) readersFound:(NSArray *)
-     readers withSlotNumbers:(NSArray *) slots;	///< called by eid_vwr_ui_callbacks::readers_changed()
-
+@protocol eIDOSLayerUI <NSObject>
+@optional
+-(void) newsrc:(eIDSource) which;						///< called by eid_vwr_ui_callbacks::newsrc()
+-(void) newstringdata: (NSString *) data /**< . */ withLabel:(NSString *)label; ///< called by eid_vwr_ui_callbacks::newstringdata()
+-(void) newbindata:(NSData *) data withLabel:(NSString *) label;		///< called by eid_vwr_ui_callbacks::newbindata()
+-(void) log:(NSString *) line withLevel:(eIDLogLevel) level;			///< called by eid_vwr_ui_callbacks::log()
+-(void) newstate:(eIDState) state;						///< called by eid_vwr_ui_callbacks::newstate()
+-(void) pinop_result:(eIDResult) result forOperation:(eIDPinOp) operation;	///< called by eid_vwr_ui_callbacks::pinop_result()
+-(void) readersFound:(NSArray *) readers withSlotNumbers:(NSArray *) slots;	///< called by eid_vwr_ui_callbacks::readers_changed()
 @end
 
 /** Class method-only class which wraps the corresponding C-only APIs for the
  benefit of ObjC applications. */
-@ interface eIDOSLayerBackend: NSObject + (void) pinop:(eIDPinOp)
-	which;
-				///< calls eid_vwr_pinop()
-+(NSInteger) setUi:(id < eIDOSLayerUI >) ui;
-				///< calls eid_vwr_ui_callbacks() and eid_vwr_createcallbacks()
-+(NSImage *) getPreview:(NSURL *) from;
-				///< calls eid_vwr_get_preview(), and converts the result to an NSImage*
-+(void) setLang:(eIDLanguage) language;
-				///< calls eid_vwr_convert_set_lang()
-+(eIDLanguage) lang;		///< calls eid_vwr_convert_get_lang()
-+(void) poll;			///< calls eid_vwr_poll()
-+(void) mainloop;		///< calls eid_vwr_be_mainloop(), which does not return
-+(void) deserialize:(NSURL *) from;
-				///< calls eid_vwr_be_deserialize()
-+(void) serialize:(NSURL *) to;
-				///< calls eid_vwr_be_serialize()
-+(NSData *) xmlform;		///< calls eid_vwr_be_get_xmlform(), and converts the result to an NSData*
-+(void) closeFile;		///< calls eid_vwr_close_file()
-+(void) setInvalid;		///< calls eid_vwr_be_set_invalid()
-+(eIDResult) validateCert:(NSData *)
-     certificate withCa:(NSData *) ca;	///< calls eid_vwr_verify_cert() with valid perform_ocsp_request and free_ocsp_request function pointers
-
-+(eIDResult) validateRrnCert:(NSData *) certificate;
-				///< calls eid_vwr_verify_rrncert()
-+(eIDResult) validateIntCert:(NSData *) certificate withCa:(NSData *)ca; ///< calls eid_vwr_verify_int_cert() with valid perform_http_request and free_http_request function pointers
-+(void) selectReader:(NSInteger) readerNumber;
-				///< calls eid_vwr_be_select_slot() with 0 as the first parameter, and the given reader number as the second parameter.
-+(void) setReaderAuto:(BOOL) automatic;
-				///< calls eid_vwr_be_select_slot with nonzero (if YES) or zero (if NO) as the first parameter, and 0 as the second parameter.
+@interface eIDOSLayerBackend: NSObject
++(void) pinop:(eIDPinOp) which;							///< calls eid_vwr_pinop()
++(NSInteger) setUi:(id < eIDOSLayerUI >) ui;					///< calls eid_vwr_ui_callbacks() and eid_vwr_createcallbacks()
++(NSImage *) getPreview:(NSURL *) from;						///< calls eid_vwr_get_preview(), and converts the result to an NSImage*
++(void) setLang:(eIDLanguage) language;						///< calls eid_vwr_convert_set_lang()
++(eIDLanguage) lang;								///< calls eid_vwr_convert_get_lang()
++(void) poll;									///< calls eid_vwr_poll()
++(void) mainloop;								///< calls eid_vwr_be_mainloop(), which does not return
++(void) deserialize:(NSURL *) from;						///< calls eid_vwr_be_deserialize()
++(void) serialize:(NSURL *) to;							///< calls eid_vwr_be_serialize()
++(NSData *) xmlform;								///< calls eid_vwr_be_get_xmlform(), and converts the result to an NSData*
++(void) closeFile;								///< calls eid_vwr_close_file()
++(void) setInvalid;								///< calls eid_vwr_be_set_invalid()
++(eIDResult) validateCert:(NSData *) certificate withCa:(NSData *) ca;		///< calls eid_vwr_verify_cert() with valid perform_ocsp_request and free_ocsp_request function pointers
++(eIDResult) validateRrnCert:(NSData *) certificate;				///< calls eid_vwr_verify_rrncert()
++(eIDResult) validateIntCert:(NSData *) certificate withCa:(NSData *)ca;	///< calls eid_vwr_verify_int_cert() with valid perform_http_request and free_http_request function pointers
++(void) selectReader:(NSInteger) readerNumber;					///< calls eid_vwr_be_select_slot() with 0 as the first parameter, and the given reader number as the second parameter.
++(void) setReaderAuto:(BOOL) automatic;						///< calls eid_vwr_be_select_slot with nonzero (if YES) or zero (if NO) as the first parameter, and 0 as the second parameter.
 +(NSString *) getCertDetail:(NSData *) certificate;
 @end
 
