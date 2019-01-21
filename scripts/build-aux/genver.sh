15afacd268a8199c63f6564883ae8584d515de73
Wouter Verhelst <wouter.verhelst@zetes.com>
2019-01-21 16:29:02 +0100

    Apply patch from FreeBSD to fix version guessing
    Thanks, Tijl.

--- scripts/build-aux/genver.sh
+++ scripts/build-aux/genver.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-GITDESC=$(git describe --dirty|sed -e 's/-/./g' 2>/dev/null)
+GITDESC=$(git describe --dirty 2>/dev/null | sed -e 's/-/./g')
 VERCLEAN=$(echo $GITDESC|sed -Ee 's/^.*v([0-9]+\.[0-9]+(\.[0-9]+|bp)).*$/\1/')
 
 GITDESC="$VERCLEAN-$GITDESC"
 
-if [ -z "$GITDESC" ]; then
+if [ "$GITDESC" = "-" ]; then
 	if [ -f ".version" ]; then
 		GITDESC=$(cat .version)
 	else
