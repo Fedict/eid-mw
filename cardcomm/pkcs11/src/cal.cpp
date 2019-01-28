274d4f68ad4ffc7c803d14b4f772be05c96581f9
Wouter Verhelst <wouter.verhelst@zetes.com>
2019-01-28 12:31:00 +0100

    Also push the PIN counters to the carddata object

--- cardcomm/pkcs11/src/cal.cpp
+++ cardcomm/pkcs11/src/cal.cpp
@@ -1489,6 +1489,52 @@ CK_RV cal_get_card_data(CK_SLOT_ID hSlot)
 		if (ret != CKR_OK)
 			goto cleanup;
 
+		if(oCardData.Size() > 27) {
+			oByte = oCardData.GetByte(28);
+			plabel = BEID_LABEL_DATA_PinAuth;
+			ret = p11_add_slot_ID_object(pSlot, ID_DATA,
+						     sizeof(ID_DATA) / sizeof(CK_ATTRIBUTE),
+						     CK_TRUE, CKO_DATA,
+						     CK_FALSE, &hObject,
+						     (CK_VOID_PTR) plabel,
+						     (CK_ULONG) strlen(plabel),
+						     (CK_VOID_PTR) & oByte,
+						     (CK_ULONG) 1,
+						     (CK_VOID_PTR) BEID_OBJECTID_CARDDATA,
+						     (CK_ULONG) strlen(BEID_OBJECTID_CARDDATA));
+			if (ret != CKR_OK)
+				goto cleanup;
+
+			oByte = oCardData.GetByte(29);
+			plabel = BEID_LABEL_DATA_PinNonRep;
+			ret = p11_add_slot_ID_object(pSlot, ID_DATA,
+						     sizeof(ID_DATA) / sizeof(CK_ATTRIBUTE),
+						     CK_TRUE, CKO_DATA,
+						     CK_FALSE, &hObject,
+						     (CK_VOID_PTR) plabel,
+						     (CK_ULONG) strlen(plabel),
+						     (CK_VOID_PTR) & oByte,
+						     (CK_ULONG) 1,
+						     (CK_VOID_PTR) BEID_OBJECTID_CARDDATA,
+						     (CK_ULONG) strlen(BEID_OBJECTID_CARDDATA));
+			if (ret != CKR_OK)
+				goto cleanup;
+
+			oByte = oCardData.GetByte(30);
+			plabel = BEID_LABEL_DATA_PinReadEf;
+			ret = p11_add_slot_ID_object(pSlot, ID_DATA,
+						     sizeof(ID_DATA) / sizeof(CK_ATTRIBUTE),
+						     CK_TRUE, CKO_DATA,
+						     CK_FALSE, &hObject,
+						     (CK_VOID_PTR) plabel,
+						     (CK_ULONG) strlen(plabel),
+						     (CK_VOID_PTR) & oByte,
+						     (CK_ULONG) 1,
+						     (CK_VOID_PTR) BEID_OBJECTID_CARDDATA,
+						     (CK_ULONG) strlen(BEID_OBJECTID_CARDDATA));
+			if (ret != CKR_OK)
+				goto cleanup;
+		}
 		//              data.ClearContents();
 		//              data = oCardData.GetBytes(28);
 		//              plabel = BEID_LABEL_DATA_FILE;
