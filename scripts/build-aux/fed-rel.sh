88303d6874223158c8cef24209b66b08c9cb3031
Wouter Verhelst <wouter.verhelst@zetes.com>
2019-04-09 11:44:23 +0200

    Split this out into a separate script

---
+++ scripts/build-aux/fed-rel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ARCHMAP=([i386]=32 [x86_64]=64)
+DISTS=([fedora]="28 29" [epel]="6 7")
+VERARCHS=([6]="i386 x86_64" [7]="x86_64" [28]="i386 x86_64" [29]="i386 x86_64")
+for dist in fedora epel
+do
+	for vers in ${DISTS[$dist]}
+	do
+		for arch in ${VERARCHS[$vers]}
+		do
+			mkdir -p /srv/repo/repo/rpm/$TARGET/$dist/$vers/RPMS/$arch
+			for i in products/$dist-$vers-${ARCHMAP[$arch]}
+			do
+				targetfile=/srv/repo/repo/rpm/$TARGET/$dist/$vers/RPMS/$arch/$(basename $i)
+				echo "$i => $targetfile"
+				cp $i $targetfile
+				rpmsign --resign --key-id=$GPG_TEST_KEY_ID $targetfile
+			done
+		done
+		createrepo /srv/repo/repo/rpm/$TARGET/$dist/$vers
+	done
+done
