b93d4621ef042477f13f432274d65994da38ef96
Wouter Verhelst <wouter.verhelst@zetes.com>
2019-04-09 20:21:29 +0200

    Translate epel to el

--- scripts/build-aux/fed-rel.sh
+++ scripts/build-aux/fed-rel.sh
@@ -4,22 +4,23 @@ set -e
 
 find ./products -print
 
-declare -A ARCHMAP
-declare -A DISTS
+declare -A DISTVERS
+declare -a DISTNAMES
 declare -A VERARCHS
 
-DISTS=([fedora]="28 29" [epel]="6 7")
+DISTVERS=([fedora]="28 29" [epel]="6 7")
+DISTNAMES=([fedora]="fedora" [epel]="el")
 VERARCHS=([6]="i386 x86_64" [7]="x86_64" [28]="i386 x86_64" [29]="i386 x86_64")
 for dist in fedora epel
 do
-	for vers in ${DISTS[$dist]}
+	for vers in ${DISTVERS[$dist]}
 	do
 		for arch in ${VERARCHS[$vers]}
 		do
-			mkdir -p /srv/repo/repo/rpm/$TARGET/$dist/$vers/RPMS/$arch
+			mkdir -p /srv/repo/repo/rpm/$TARGET/${DISTNAMES[$dist]}/$vers/RPMS/$arch
 			for i in products/$dist-$vers-$arch/*
 			do
-				targetfile=/srv/repo/repo/rpm/$TARGET/$dist/$vers/RPMS/$arch/$(basename $i)
+				targetfile=/srv/repo/repo/rpm/$TARGET/${DISTNAMES[$dist]}/$vers/RPMS/$arch/$(basename $i)
 				echo "$i => $targetfile"
 				cp $i $targetfile
 				rpmsign --resign --key-id=$GPG_TEST_KEY_ID $targetfile
