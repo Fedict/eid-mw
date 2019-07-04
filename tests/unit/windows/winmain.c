9652b071d5dfabb7017703090b1727b2ae908f5c
Wouter Verhelst <wouter.verhelst@zetes.com>
2019-07-04 16:51:17 +0200

    Clean up Windows test file, and allow for selecting which tests to run from the command line

--- tests/unit/windows/winmain.c
+++ tests/unit/windows/winmain.c
@@ -42,6 +42,7 @@ _TCHAR* eid_builtin_reader = NULL;
 //int _tmain(int argc, _TCHAR* argv[]){
 typedef	struct {
 	int result;
+	bool doTest;
 	char* testDescription;
 	int (*test_function_ptr)(void);
 } eIDTest;
@@ -57,79 +58,35 @@ int	_tmain(int argc, _TCHAR* argv[]){
 	int nrofTests = 0;
 	errno_t error;
 
-	eIDTest eIDTests[] = { \
-//	{{0,TEST_SKIPPED},"Tests C_getinfo results in a single thread",&test_getinfo},
-//#ifdef WIN32
-//	{{0,TEST_SKIPPED},"Tests multiple finalize/initialize sessions in multiple threads with different args",&test_finalize_initialize},
-//	{{0,TEST_SKIPPED},"Tests multiple finalize/initialize sessions in a single thread",&test_finalize_initialize_st},
-//	{{0,TEST_SKIPPED},"Tests C_initialize when bad parameters are supplied 1",&test_initialize_ownmutex},
-//	{{0,TEST_SKIPPED},"Tests C_initialize when bad parameters are supplied",&test_initialize_preserved},
-//	{{0,TEST_SKIPPED},"Tests C_finalize when bad parameters are supplied",&test_finalize_preserved},
-//#endif //WIN32
-//	{{0,TEST_SKIPPED},"Tests C_getslotlist in single and two call usage",&test_getslotlist},
-//	{{0,TEST_SKIPPED},"Tests C_getslotlist when insufficient memory is reserved",&test_getslotlist_multiple_slots},
-//	{{0,TEST_SKIPPED},"Tests opening and closing of a session in a single thread",&test_open_close_session},
-//	{{0,TEST_SKIPPED},"Tests getting slotID and slotdescription",&test_open_close_session_info},
-//	{{0,TEST_SKIPPED},"Tests C_opensession with wrong parameters supplied",&test_open_close_session_bad_params},
-//	{{0,TEST_SKIPPED},"Tests how many sessions can be opened",&test_open_close_session_limits},
-//#ifdef WIN32
-//{{0,TEST_SKIPPED},"tests getting all the objects from the card",&test_logslotevents_blocking},
-
-//  #ifdef USER_INTERACTION
-//	{{0,TEST_SKIPPED},"Tests waitforslot event (needs user interaction)",&test_waitforslotevent_userinteraction},//
-//	{{0,TEST_SKIPPED},"tests signing with the card",&test_sign},
-//		{{0,TEST_SKIPPED},"tests signing sequence with all mechanisms on the card",&test_sign_allmechs},
-//	{{0,TEST_SKIPPED},"Tests waiting for slot event blocking",&test_waitforslotevent_blocking},
-//  #endif
-//	{{0,TEST_SKIPPED},"Tests waiting for slot event non-blocking",&test_waitforslotevent_nonblocking},
-//	{{0,TEST_SKIPPED},"Tests waiting for slot event blocking, while another thread calls c_finalize",&test_waitforslotevent_whilefinalize},
-//#endif //WIN32
-//	{{0,TEST_SKIPPED},"Tests the retrieval of slot and token info",&test_get_token_info},
-//	{{0,TEST_SKIPPED},"Shows info on the mechanisms supported by the card",&test_getmechanisms},
-//	{{0,TEST_SKIPPED},"tests getting the keys from the card",&test_getkeys},
-//	{{0,TEST_SKIPPED},"tests the return values of the getattributes function",&test_getkeys_retvals},
-	//{{0,TEST_SKIPPED},"tests getting all the objects from the card",&test_getallobjects},
-		{0,"tests initialize/finalize",&init_finalize},
-			{0,"tests getinfo",&getinfo},
-			//{0,"tests initialize/finalize",&funclist},
-			{0,"tests slotlist",&slotlist},
-			{0,"tests slotinfo",&slotinfo},
-			{0,"tests tkinfo",&tkinfo},
-			{0,"tests double_init",&double_init},
-			{0,"tests slotevent",&slotevent},
-			{0,"tests mechlist",&mechlist},
-			{0,"tests mechinfo",&mechinfo},
-			{0,"tests sessions",&sessions},
-			{0,"tests sessions_nocard",&sessions_nocard},
-			{0,"tests sessioninfo",&sessioninfo},
-			{0,"tests login",&slogin},
-			{0,"tests nonsensible",&nonsensible},
-			{0,"tests objects",&objects},
-			{0,"tests readdata",&readdata},
-			{0,"tests readdata_limited", &readdata_limited},
-			{0,"tests readdata_sequence", &readdata_sequence},
-			{0,"tests digest",&digest},
-			{0,"tests sign", &sign},
-			{0,"tests sign_state",&sign_state},
-			{0,"tests reading the photo",&decode_photo},
-			{0,"tests ordering",&ordering},
-			{0,"tests wrong_init", &wrong_init},
-			{0,"tests login_state", &login_state},
-
-
-//		{{0,TEST_SKIPPED},"tests getting all the objects from the card",&test_getallidobjects},
-	
-//	{{0,TEST_SKIPPED},"tests getting object's attributes from the card",&test_getattributevalue_all},
-//	{{0,TEST_SKIPPED},"tests getting object's attributes from the card",&test_getattributevalue_rrn},
-	
-//	{{0,TEST_SKIPPED},"tests getting the lastname object's attributes from the card",&test_getattributevalue_lastname},	
-//#ifdef WIN32
-//	{{0,TEST_SKIPPED},"tests adding and removing readers",&test_add_remove_readers},
-//	{{0,TEST_SKIPPED},"tests waiting for card and reader events",&test_add_remove_readerevents},
-//#endif //WIN32
-//	{{0,TEST_SKIPPED},"tests getting the private object's from the card without logging in",&test_findPrivateKeyWithoutLoginShouldFail},	
-//	{{0,TEST_SKIPPED},"tests the return value of C_FindObjectsFinal when called without C_FindObjectsInit",&test_findObjectsFinalNotInitialized},
+	eIDTest eIDTests[] = {
+		{0,true, "initialize_finalize",&init_finalize},
+		{0,true, "getinfo",&getinfo},
+		{0,true, "funclist",&funclist},
+		{0,true, "slotlist",&slotlist},
+		{0,true, "slotinfo",&slotinfo},
+		{0,true, "tkinfo",&tkinfo},
+		{0,true, "double_init",&double_init},
+		{0,true, "slotevent",&slotevent},
+		{0,true, "mechlist",&mechlist},
+		{0,true, "mechinfo",&mechinfo},
+		{0,true, "sessions",&sessions},
+		{0,true, "sessions_nocard",&sessions_nocard},
+		{0,true, "sessioninfo",&sessioninfo},
+		{0,true, "slogin",&slogin},
+		{0,true, "nonsensible",&nonsensible},
+		{0,true, "objects",&objects},
+		{0,true, "readdata",&readdata},
+		{0,true, "readdata_limited", &readdata_limited},
+		{0,true, "readdata_sequence", &readdata_sequence},
+		{0,true, "digest",&digest},
+		{0,true, "sign", &sign},
+		{0,true, "sign_state",&sign_state},
+		{0,true, "decode_photo",&decode_photo},
+		{0,true, "ordering",&ordering},
+		{0,true, "wrong_init", &wrong_init},
+		{0,true, "login_state", &login_state},
 	};
+	nrofTests = (sizeof(eIDTests) / sizeof(eIDTest));
 
 	if(argc >=2)
 		eid_robot_style = argv[1];
@@ -140,19 +97,30 @@ int	_tmain(int argc, _TCHAR* argv[]){
 	if(argc >=4)
 		eid_builtin_reader = argv[3];
 
+	if(argc >= 5) {
+		for (i = 0; i < nrofTests; i++) {
+			eIDTests[i].doTest = false;
+			for (int j = 4; j < argc; j++) {
+				if (strcmp(argv[j], eIDTests[i].testDescription) == 0) {
+					eIDTests[i].doTest = true;
+					j = argc;
+				}
+			}
+		}
+	}
+
 	error = initLog();
 	if(error != 0)
 	{
 		printf("Can't open logfile");
 		return -1;
 	}
-	nrofTests = (sizeof(eIDTests)/sizeof(eIDTest));
-
-	while (i < nrofTests )
+	for(i=0; i < nrofTests; i++)
 	{
+		if (!eIDTests[i].doTest)
+			continue;
 		printf("test %d: %s\n", i, eIDTests[i].testDescription);
 		eIDTests[i].result = eIDTests[i].test_function_ptr();
-		i++;
 	}
 
 	int success = 0;
@@ -162,6 +130,8 @@ int	_tmain(int argc, _TCHAR* argv[]){
 	//testlog(LVL_NOLEVEL,"\n\n_______________________________________________\n");
 	for (i = 0; i < nrofTests; i++)
 	{
+		if(!eIDTests[i].doTest)
+			continue;
 		if (eIDTests[i].testDescription != NULL)
 		{
 			testlog(LVL_NOLEVEL,"_______________________________________________\n");
