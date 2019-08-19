3bd66331475fb6bbe821fcfaa204e00861c411f1
Wouter Verhelst <wouter.verhelst@zetes.com>
2019-08-19 15:14:36 +0200

    Improve the deserialize test
    - Also allow running it when no card or robot is present
    - Drop the sleep calls, instead use a mutex and a condition to signal
      across threads when it's safe to move on

--- plugins_tools/eid-viewer/test/deserialize.c
+++ plugins_tools/eid-viewer/test/deserialize.c
@@ -3,9 +3,15 @@
 #include <testlib.h>
 #include "common.h"
 #include <eid-viewer/oslayer.h>
+#include <pthread.h>
 
 static void(*nsd)(const EID_CHAR*, const EID_CHAR*) = NULL;
+static void(*nst)(enum eid_vwr_states new_state) = NULL;
 static int have_found_cardnumber = 0;
+static int result = TEST_RV_FAIL;
+
+static pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cnd = PTHREAD_COND_INITIALIZER;
 
 static void newstringdata(const EID_CHAR* label, const EID_CHAR* data) {
 	nsd(label, data);
@@ -15,24 +21,59 @@ static void newstringdata(const EID_CHAR* label, const EID_CHAR* data) {
 	}
 }
 
+static void newstate(enum eid_vwr_states new_state) {
+	static enum eid_vwr_states prev_state = STATE_LIBOPEN;
+	if(nst != NULL) nst(new_state);
+	pthread_mutex_lock(&mut);
+	switch(new_state) {
+		case STATE_NO_TOKEN:
+			if(prev_state == STATE_FILE) {
+				pthread_cond_signal(&cnd);
+				break;
+			}
+			if(!have_robot()) {
+				CK_SLOT_ID sl;
+				CK_RV rv = find_slot(CK_TRUE, &sl);
+				if(rv != TEST_RV_SKIP) {
+					result = TEST_RV_SKIP;
+					pthread_cond_signal(&cnd);
+					break;
+				}
+			} else {
+				robot_remove_card();
+			}
+			printf("opening " SRCDIR "/67.06.30-296.59.eid\n");
+			eid_vwr_be_deserialize(SRCDIR "/67.06.30-296.59.eid");
+			break;
+		case STATE_FILE_WAIT:
+			verbose_assert(cursrc == EID_VWR_SRC_FILE);
+			verbose_assert(have_found_cardnumber == 1);
+			result = TEST_RV_OK;
+			pthread_cond_signal(&cnd);
+			break;
+		case STATE_TOKEN:
+			result = TEST_RV_FAIL;
+			pthread_cond_signal(&cnd);
+			break;
+		default:
+			break;
+	}
+	prev_state = new_state;
+	pthread_mutex_unlock(&mut);
+}
+
 TEST_FUNC(deserialize) {
 	struct eid_vwr_ui_callbacks* cb;
 
 	cb = createcbs();
-	if(!have_robot()) {
-		printf("E: Need a robot to remove the card...\n");
-		return TEST_RV_SKIP;
-	}
-	robot_remove_card();
-	SLEEP(1);
 	verbose_assert(cb != NULL);
 	nsd = cb->newstringdata;
 	cb->newstringdata = newstringdata;
+	nst = cb->newstate;
+	cb->newstate = newstate;
 	verbose_assert(eid_vwr_createcallbacks(cb) == 0);
-	printf("opening " SRCDIR "/67.06.30-296.59.eid\n");
-	eid_vwr_be_deserialize(SRCDIR "/67.06.30-296.59.eid");
-	SLEEP(1);
-	verbose_assert(cursrc == EID_VWR_SRC_FILE);
-	verbose_assert(have_found_cardnumber == 1);
-	return TEST_RV_OK;
+	pthread_mutex_lock(&mut);
+	pthread_cond_wait(&cnd, &mut);
+	pthread_mutex_unlock(&mut);
+	return result;
 }
