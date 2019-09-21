506d11409fda5423b3e944ed7c49137778af6830
Wouter Verhelst <w@uter.be>
2019-09-21 13:35:34 +0200

    Allow setting up chroot more easily

---
+++ .ci/dockerfiles/deb-sbuild/setup-chroot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+set -x
+
+CODE=shift
+ARCH=shift
+DIST=shift
+case $DIST in
+	debian)
+		/usr/local/bin/download-debuerreotype-tarball.sh $CODE $ARCH
+	;;
+	ubuntu)
+		mkdir -p /srv/chroot
+		sbuild-createchroot --arch=$ARCH $CODE /srv/chroot/$CODE https://archive.ubuntu.com/ubuntu
+	;;
+esac
