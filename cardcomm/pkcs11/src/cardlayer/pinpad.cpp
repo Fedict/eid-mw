84d112e0ef8f935a65990942b8db89f99f2f9dc4
Frederik <Frederik>
2019-10-18 09:45:29 +0200

    Added "ETSWW eKrypto PINPad" to the ppdu over transmit whitelists in minidriver and pkcs11
    (cherry picked from commit ece1b74ce5577c8a90f7236765d36a7cbfe4c04b)

--- cardcomm/pkcs11/src/cardlayer/pinpad.cpp
+++ cardcomm/pkcs11/src/cardlayer/pinpad.cpp
@@ -509,6 +509,7 @@ namespace eIDMW
 			(m_csReader.find("VASCO DIGIPASS 876") == 0) ||    
 		    (m_csReader.find("Gemalto ING Shield Pro") == 0) ||
 			(m_csReader.find("ETSWW eKrypto PINPhab") == 0) ||
+			(m_csReader.find("ETSWW eKrypto PINPad") == 0) ||
 			(m_csReader.find("DIOSS pinpad") == 0))
 		{
 			oResp = m_poPCSC->Transmit(m_hCard, oCmd, &lRetVal);
