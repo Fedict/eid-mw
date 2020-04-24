46c079b8edeeda14e4838c20dc4cdda38d2dc579
Vampire Cat <catvampire35@gmail.com>
2020-04-24 07:10:31 +0200

    Minor copy xsd fix
    Minor fix

--- cardcomm/pkcs11/src/common/beidversions.h
+++ cardcomm/pkcs11/src/common/beidversions.h
@@ -6,10 +6,10 @@
 #define BEID_PRODUCT_NAME "Belgium eID Middleware"
 #define BEID_PRODUCT_VERSION "5.0.1"
 #define BEID_VERSION_STRING "5, 0, 1"
-#define BEID_BUILD_DATE "2020-04-21"
+#define BEID_BUILD_DATE "2020-04-24"
 #define BASE_VERSION1 5
 #define BASE_VERSION2 0
 #define BASE_VERSION3 1
-#define EIDMW_REVISION_STR "v4.4.23-791-g1bd204ed-dirty"
+#define EIDMW_REVISION_STR "v5.0.2-22-g63ebe2a5-dirty"
 
 #endif
