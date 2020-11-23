d6ddf8c482617bde0e6b4f811bee1c23cf9bf9d0
Frederik <Frederik>
2020-11-23 10:45:40 +0100

    viewer about: update version number

--- plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
@@ -7,7 +7,7 @@ namespace eIDViewer
     {
         public static string AboutMessage()
         {
-            string message = @"eID Viewer 5.0.8
+            string message = @"eID Viewer 5.0.7
 eID Middleware Project
 Copyright(C) 2020 Bosa
 
