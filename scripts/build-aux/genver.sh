4022ebc5c04a8151f43bb4776bc48b892631b0af
Wouter Verhelst <wouter.verhelst@zetes.com>
2021-01-27 10:29:56 +0200

    Revert "test:distcheck: fail and retry if the version can't be found"
    This reverts commit 3b219bfb53e3f299ad1480aab1b0da034f169f60.
    
    Doesn't seem to work (and I don't know why...)

--- scripts/build-aux/genver.sh
+++ scripts/build-aux/genver.sh
@@ -2,7 +2,6 @@
 
 GITDESC=$(git describe --match="v5*" --dirty 2>/dev/null | sed -e 's/-/./g')
 VERCLEAN=$(echo $GITDESC|sed -Ee 's/^.*v([0-9]+\.[0-9]+(\.[0-9]+|bp|\.pre[0-9]*)).*$/\1/')
-FAIL=$1
 
 GITDESC="$VERCLEAN-$GITDESC"
 
@@ -10,11 +9,7 @@ if [ "$GITDESC" = "-" ]; then
 	if [ -f ".version" ]; then
 		GITDESC=$(cat .version)
 	else
-		if [ -z "$FAIL" ]; then
-			GITDESC="0.unknown"
-		else
-			exit 1
-		fi
+		GITDESC="0.unknown"
 	fi
 fi
 
