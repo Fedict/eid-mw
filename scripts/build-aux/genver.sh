a79d5956b73554ae63acc0a7b89a6c95616f6be6
Wouter Verhelst <wouter.verhelst@zetes.com>
2021-01-27 12:07:27 +0200

    Do a git fetch --all if necessary

--- scripts/build-aux/genver.sh
+++ scripts/build-aux/genver.sh
@@ -2,6 +2,7 @@
 
 GITDESC=$(git describe --match="v5*" --dirty 2>/dev/null | sed -e 's/-/./g')
 VERCLEAN=$(echo $GITDESC|sed -Ee 's/^.*v([0-9]+\.[0-9]+(\.[0-9]+|bp|\.pre[0-9]*)).*$/\1/')
+FORCE_VERSION=${FORCE_VERSION:0}
 
 GITDESC="$VERCLEAN-$GITDESC"
 
@@ -9,6 +10,11 @@ if [ "$GITDESC" = "-" ]; then
 	if [ -f ".version" ]; then
 		GITDESC=$(cat .version)
 	else
+		if [ $FORCE_VERSION -gt 0 ]; then
+			git fetch --all
+			export FORCE_VERSION=0
+			exec $0 "$@"
+		fi
 		GITDESC="0.unknown"
 	fi
 fi
