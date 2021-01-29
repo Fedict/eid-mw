236af0813e4855393f02098cba65010d8a3a86e0
osboxes.org <yannick.schoels@zetes.com>
2021-01-29 04:15:55 -0500

    clean start toegevoed aan states test

--- plugins_tools/eid-viewer/test/states.c
+++ plugins_tools/eid-viewer/test/states.c
@@ -110,6 +110,8 @@ TEST_FUNC(states) {
 		printf("no robot");
 		return TEST_RV_SKIP;
 	}
+	robot_remove_card();
+	robot_remove_reader();
 	cb = createcbs();
 	orig_bindata = cb->newbindata;
 	cb->newbindata = mybindata;
@@ -119,7 +121,7 @@ TEST_FUNC(states) {
 	verbose_assert(eid_vwr_createcallbacks(cb) == 0);
 	
 #ifndef WIN32
-	robot_remove_reader();
+
 	SLEEP(20);
 	verbose_assert(flags[STATE_CALLBACKS]);
 	verbose_assert(flags[STATE_NO_TOKEN]);
@@ -127,7 +129,6 @@ TEST_FUNC(states) {
 	verbose_assert(curstate == STATE_NO_READER);
 	clearflags();
 #endif
-
 
 	robot_insert_reader();
 	SLEEP(20);
@@ -234,7 +235,7 @@ TEST_FUNC(states) {
 	clearflags();
 	SLEEP(5);
 	/*
-	printf("test for challenge (only run with a card v1.8)\n");
+	printf("test for challenge(only run with a card v1.8)\n");
 	if(have_keyhash){
 		const unsigned char* challenge = "123456789012345678901234567890123456789012345678";
 		int lenght = 48;
