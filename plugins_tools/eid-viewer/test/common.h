c4bd17fe359460c374accaf15c9f91ead5d256ae
Vampire Cat <catvampire35@gmail.com>
2021-02-01 12:22:21 +0100

    More deprecation fixes.
    More deprecation fixes.

--- plugins_tools/eid-viewer/test/common.h
+++
@@ -1,8 +0,0 @@
-#ifndef EID_VWR_TEST_COMMON_H
-#define EID_VWR_TEST_COMMON_H
-
-extern enum eid_vwr_source cursrc;
-
-struct eid_vwr_ui_callbacks *createcbs();
-
-#endif
--- plugins_tools/util/runindent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-indent -v -bad -bap -bbb -bbo -bl -blf -bls -bli0 -cbi0 -cd30 -cdw -ce -ci8 -cli8 -cs -d0 -hnl -i8 -il0 -lp -npsl -di0 -npro -npcs -saf -sai -saw -ss -ut -ts8 "$@"
