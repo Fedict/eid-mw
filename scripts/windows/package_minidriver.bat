2c4188ca05c2e64efa4e81ab6d777230643e3537
Frederik <Frederik>
2021-03-10 10:46:27 +0100

    Windows build scripts: also create the minidriver zip with version number

--- scripts/windows/package_minidriver.bat
+++ scripts/windows/package_minidriver.bat
@@ -1,3 +1,6 @@
+:: get eidmw version
+:: =================
+call "%~dp0.\set_eidmw_version.cmd"
 
 del /q beidmdrv_all.zip
 del /q %~dp0\beidmdr_all
@@ -12,6 +15,8 @@ robocopy %~dp0..\..\installers\quickinstaller\Drivers\XP-WIN8\beidmdrv %~dp0\bei
 :: zip the minidrivers folders
 powershell.exe -nologo -noprofile -command "Compress-Archive -Path .\beidmdr_all\* -CompressionLevel Optimal -DestinationPath ./beidmdrv_all.zip"
 
+:: create a copy with the full revision name
+copy "%~dp0\beidmdrv_all.zip" "%~dp0\beidmdrv_all_%BASE_VERSION1%.%BASE_VERSION2%.%BASE_VERSION3%.%EIDMW_REVISION%.zip"
 
 :end
 @exit /B 0
