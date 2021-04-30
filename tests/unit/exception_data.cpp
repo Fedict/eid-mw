81a560278ef99e3799d9ae114f2708364dce8eb7
osboxes.org <yannick.schoels@zetes.com>
2021-04-30 09:27:32 +0200

    hotfix methode call

--- tests/unit/exception_data.cpp
+++ tests/unit/exception_data.cpp
@@ -33,7 +33,7 @@ TEST_FUNC(exceptiondata){
 		trow CNotAuthenticatedException(0xe1d00404L)
 	}	
 	catch(CNotAuthenticatedException n){
-		printf("Not authenticated error code: %lu\n", n.get_m_lError);
+		printf("Not authenticated error code: %lu\n", n.GetError);
 	}
 	catch(CMWException mw){
 		if (mw.GetError() != 0xe1d00404L)return TEST_RV_FAIL;
