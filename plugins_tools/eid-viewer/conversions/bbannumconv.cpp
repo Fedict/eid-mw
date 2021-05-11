7f5103e67d4d0677dc091afcabbe22385530042f
Frederik <Frederik>
2021-05-11 14:40:25 +0200

    Added foreigner card numbers of 10 chars to the card number conversion function

--- plugins_tools/eid-viewer/conversions/bbannumconv.cpp
+++ plugins_tools/eid-viewer/conversions/bbannumconv.cpp
@@ -4,17 +4,36 @@
 #include <stdexcept>
 #include "cppeidstring.h"
 
-/* BBAN numbers.
+/* belgian eID card numbers. (12)
    On card: 123123456712
    Display: 123-1234567-12
+
+   foreigner eID card numbers (10)
+   On card: B123456789
+   Display: B 1234567 89
+
+   older foreigner eID card number (9)
+   On card: 123456789
+   Display: 123456789
  */
+
+
 EID_STRING BBANNumberConvertor::convert(const void *original)
 {
 	const EID_STRING normal((const EID_CHAR *) original);
 
 	try
 	{
-		return normal.substr(0, 3) + TEXT("-") + normal.substr(3, 7) + TEXT("-") + normal.substr(10, 2);
+		switch (normal.length())
+		{
+		case 9:
+			return normal;
+		case 10:
+			return normal.substr(0, 1) + TEXT(" ") + normal.substr(1, 7) + TEXT(" ") + normal.substr(8, 2);
+		case 12:
+		default:
+			return normal.substr(0, 3) + TEXT("-") + normal.substr(3, 7) + TEXT("-") + normal.substr(10, 2);
+		}
 	}
 	catch(const std::out_of_range & oor)
 	{
