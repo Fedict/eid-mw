Fedict/eid-mw

commit 4bb1eba0675c1372682ebf59244196d48ab9eb66

macOS viewer: only recheck hash value with SHA256 hash if photo hash is SHA1

plugins_tools/eid-viewer/certhelpers.c
@@ -209,11 +209,14 @@ int eid_vwr_check_data_validity(const void* photo, int plen,
 		/* Some CA4 cards are re-signed CA3 ones where the photo hash
 		 * is still SHA1, but everything else is SHA256. Try if this is
 		 * such a card. */
-		md = EVP_get_digestbyname("sha256");
-		if(!verify_once(pubkey, md, datafile, datfilelen, datasig, datsiglen)) {
-			be_log(EID_VWR_LOG_COARSE, "Data signature fails validation!");
-			return 0;
-		}
+        if(hashlen == SHA_DIGEST_LENGTH)
+        {
+            md = EVP_get_digestbyname("sha256");
+            if(!verify_once(pubkey, md, datafile, datfilelen, datasig, datsiglen)) {
+                be_log(EID_VWR_LOG_COARSE, "Data signature fails validation!");
+                return 0;
+            }
+        }
 	}
 	address_data = calloc(addfilelen + datsiglen, 1);
 	memcpy(address_data, addrfile, addfilelen);

commit 87c8ee81cff70948589603862790c1715b016066

Add missing return
If the hash fails and the hash was not a SHA1 hash, we need to return 0 to
signal validation failure.

Fixes: 4bb1eba0675c1372682ebf59244196d48ab9eb66

plugins_tools/eid-viewer/certhelpers.c
@@ -220,6 +220,8 @@ int eid_vwr_check_data_validity(const void* photo, int plen,
 				be_log(EID_VWR_LOG_COARSE, "Data signature fails validation!");
 				return 0;
 			}
+		} else {
+			return 0;
 		}
 	}
 	address_data = calloc(addfilelen + datsiglen, 1);