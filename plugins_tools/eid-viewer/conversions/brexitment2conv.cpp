0f65ece47748b15c8c0625304b0d7355bb410363
Frederik <Frederik>
2021-08-02 11:18:36 +0200

    eID Viewer Backend: adjust some conversion functions that will need to take the graphical byte into account

--- plugins_tools/eid-viewer/conversions/brexitment2conv.cpp
+++ plugins_tools/eid-viewer/conversions/brexitment2conv.cpp
@@ -1,20 +1,25 @@
 ﻿#include "brexitment2conv.h"
 
-std::map < eid_vwr_langs, std::map <char, EID_STRING> > BrexitMention2Convertor::conversions;
+std::map < eid_vwr_graphvers, std::map < eid_vwr_langs, std::map <char, EID_STRING> > > BrexitMention2Convertor::conversions;
 
 BrexitMention2Convertor::BrexitMention2Convertor() {
 	if(conversions.size() == 0)
 	{
-		conversions[EID_VWR_LANG_DE]['C'] = TEXT("Daueraufenthalt");
-		conversions[EID_VWR_LANG_EN]['C'] = TEXT("Permanent stay");
-		conversions[EID_VWR_LANG_FR]['C'] = TEXT("Séjour permanent");
-		conversions[EID_VWR_LANG_NL]['C'] = TEXT("Duurzaam verblijf");
+		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_DE]['C'] = TEXT("Daueraufenthalt");
+		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_EN]['C'] = TEXT("Permanent stay");
+		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_FR]['C'] = TEXT("Séjour permanent");
+		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_NL]['C'] = TEXT("Duurzaam verblijf");
+
+		conversions[EID_VWR_GRAPH_VERSION_NINE][EID_VWR_LANG_DE]['C'] = TEXT("DAUERAUFENTHALT");
+		conversions[EID_VWR_GRAPH_VERSION_NINE][EID_VWR_LANG_EN]['C'] = TEXT("PERMANENT STAY");
+		conversions[EID_VWR_GRAPH_VERSION_NINE][EID_VWR_LANG_FR]['C'] = TEXT("SEJOUR PERMANENT");
+		conversions[EID_VWR_GRAPH_VERSION_NINE][EID_VWR_LANG_NL]['C'] = TEXT("DUURZAAM VERBLIJF");
 	}
 }
 
 EID_STRING BrexitMention2Convertor::convert(const void *normal) {
 	const char *norm = (const char*)normal;
-	return conversions[target_][*norm];
+	return conversions[graphvers_][target_][*norm];
 }
 
 std::map < EID_STRING, EID_STRING > XmlBrexitMention2Convertor::conversions;
