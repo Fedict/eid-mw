544add1a03a3c4686c69f9e8a6201ba1bf8bfae9
Frederik <Frederik>
2021-08-05 11:00:00 +0200

    Up Windows versions to 5.1.0

--- plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/Sources/About.cs
@@ -7,7 +7,7 @@ namespace eIDViewer
     {
         public static string AboutMessage()
         {
-            string message = @"eID Viewer 5.0.27
+            string message = @"eID Viewer 5.1.0
 eID Middleware Project
 Copyright(C) 2021 Bosa
 
