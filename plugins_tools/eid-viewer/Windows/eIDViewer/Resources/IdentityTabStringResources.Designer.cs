2c77eaeaceb23a6d8ae3b3e2cc50c996a682b634
Frederik <Frederik>
2021-08-09 15:23:39 +0200

    Provide a label name for the cardA mentions

--- plugins_tools/eid-viewer/Windows/eIDViewer/Resources/IdentityTabStringResources.Designer.cs
+++ plugins_tools/eid-viewer/Windows/eIDViewer/Resources/IdentityTabStringResources.Designer.cs
@@ -19,7 +19,7 @@ namespace eIDViewer.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class IdentityTabStringResources {
@@ -66,6 +66,15 @@ namespace eIDViewer.Resources {
         public static string Brexit_mentions {
             get {
                 return ResourceManager.GetString("Brexit_mentions", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Mentions:.
+        /// </summary>
+        public static string CardA_mentions {
+            get {
+                return ResourceManager.GetString("CardA_mentions", resourceCulture);
             }
         }
         
