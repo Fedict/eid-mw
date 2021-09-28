9039c350e347ac1846e6cde9a270a6e55e09f5dd
Frederik <Frederik>
2021-09-28 16:21:13 +0200

    For the eID Viewer backend, card mentions are all EID_STRINGS, not binaries

--- plugins_tools/eid-viewer/conversions/brexitment2conv.cpp
+++ plugins_tools/eid-viewer/conversions/brexitment2conv.cpp
@@ -1,25 +1,35 @@
 ﻿#include "brexitment2conv.h"
 
-std::map < eid_vwr_graphvers, std::map < eid_vwr_langs, std::map <char, EID_STRING> > > BrexitMention2Convertor::conversions;
+std::map < eid_vwr_graphvers, std::map < eid_vwr_langs, std::map <EID_STRING, EID_STRING> > > BrexitMention2Convertor::conversions;
 
 BrexitMention2Convertor::BrexitMention2Convertor() {
 	if(conversions.size() == 0)
 	{
-		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_DE]['C'] = TEXT("Daueraufenthalt");
-		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_EN]['C'] = TEXT("Permanent stay");
-		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_FR]['C'] = TEXT("Séjour permanent");
-		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_NL]['C'] = TEXT("Duurzaam verblijf");
+		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_DE][TEXT("C")] = TEXT("Daueraufenthalt");
+		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_EN][TEXT("C")] = TEXT("Permanent stay");
+		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_FR][TEXT("C")] = TEXT("Séjour permanent");
+		conversions[EID_VWR_GRAPH_VERSION_NONE][EID_VWR_LANG_NL][TEXT("C")] = TEXT("Duurzaam verblijf");
 
-		conversions[EID_VWR_GRAPH_VERSION_EIGHT][EID_VWR_LANG_DE]['C'] = TEXT("DAUERAUFENTHALT");
-		conversions[EID_VWR_GRAPH_VERSION_EIGHT][EID_VWR_LANG_EN]['C'] = TEXT("PERMANENT STAY");
-		conversions[EID_VWR_GRAPH_VERSION_EIGHT][EID_VWR_LANG_FR]['C'] = TEXT("SEJOUR PERMANENT");
-		conversions[EID_VWR_GRAPH_VERSION_EIGHT][EID_VWR_LANG_NL]['C'] = TEXT("DUURZAAM VERBLIJF");
+		conversions[EID_VWR_GRAPH_VERSION_EIGHT][EID_VWR_LANG_DE][TEXT("C")] = TEXT("DAUERAUFENTHALT");
+		conversions[EID_VWR_GRAPH_VERSION_EIGHT][EID_VWR_LANG_EN][TEXT("C")] = TEXT("PERMANENT STAY");
+		conversions[EID_VWR_GRAPH_VERSION_EIGHT][EID_VWR_LANG_FR][TEXT("C")] = TEXT("SEJOUR PERMANENT");
+		conversions[EID_VWR_GRAPH_VERSION_EIGHT][EID_VWR_LANG_NL][TEXT("C")] = TEXT("DUURZAAM VERBLIJF");
 	}
 }
 
 EID_STRING BrexitMention2Convertor::convert(const void *normal) {
-	const char *norm = (const char*)normal;
-	return conversions[graphvers_][target_][*norm];
+	if (conversions.count(graphvers_) > 0)
+	{
+		if (conversions[graphvers_].count(target_) > 0)
+		{
+			EID_STRING norm = (EID_CHAR*)normal;
+			if (conversions[graphvers_][target_].count(norm) > 0)
+			{
+				return conversions[graphvers_][target_][norm];
+			}
+		}
+	}
+	return (EID_CHAR*)normal;
 }
 
 std::map < EID_STRING, EID_STRING > XmlBrexitMention2Convertor::conversions;
--- plugins_tools/eid-viewer/conversions/cardAment2conv.h
+++ plugins_tools/eid-viewer/conversions/cardAment2conv.h
@@ -4,7 +4,7 @@
 class CardAMention2Convertor :public ConversionWorker
 {
 private:
-	static std::map < eid_vwr_graphvers, std::map < eid_vwr_langs, std::map <char, EID_STRING> > > conversions;
+	static std::map < eid_vwr_graphvers, std::map < eid_vwr_langs, std::map <EID_STRING, EID_STRING> > > conversions;
 public:
 	CardAMention2Convertor();
 	virtual EID_STRING convert(const void *normal);
