a4032e5d5df9de8d7d481e848f102f446aedd1e9
Frederik <Frederik>
2021-10-26 11:11:32 +0200

    Windows viewer signing script: do not sign 64bit pkcs11, as it is not used in the viewer yet

--- scripts/windows/sign_viewer.bat
+++ scripts/windows/sign_viewer.bat
@@ -19,8 +19,8 @@ set MDRVCERTPATH=%~dp0..\..\cardcomm\minidriver\makemsi
 
 @echo [INFO] Sign the pkcs11_ff dll, 32bit
 "%SIGNTOOL_PATH%\signtool" sign /fd SHA256 /s MY /n "Zetes SA" /sha1 "e20634d42e8bc522c6341dce24badd103f5f4312" /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 /v "%~dp0..\..\cardcomm\pkcs11\VS_2019\Binaries\Win32_PKCS11_FF_Release\beid_ff_pkcs11.dll"
-@echo [INFO] Sign the pkcs11_ff dll, 64bit
-"%SIGNTOOL_PATH%\signtool" sign /fd SHA256 /s MY /n "Zetes SA" /sha1 "e20634d42e8bc522c6341dce24badd103f5f4312" /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 /v "%~dp0..\..\cardcomm\pkcs11\VS_2019\Binaries\x64_PKCS11_FF_Release\beid_ff_pkcs11.dll"
+:: @echo [INFO] Sign the pkcs11_ff dll, 64bit
+:: "%SIGNTOOL_PATH%\signtool" sign /fd SHA256 /s MY /n "Zetes SA" /sha1 "e20634d42e8bc522c6341dce24badd103f5f4312" /tr http://rfc3161timestamp.globalsign.com/advanced /td SHA256 /v "%~dp0..\..\cardcomm\pkcs11\VS_2019\Binaries\x64_PKCS11_FF_Release\beid_ff_pkcs11.dll"
 
 
 :: create the MSI installers
