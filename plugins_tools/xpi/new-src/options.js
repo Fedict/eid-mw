2182eb4175a0d590032922b29fd789e5944803e2
Wouter Verhelst <wouter.verhelst@zetes.com>
2021-12-02 16:24:47 +0200

    Also show whether the middleware was found, and at which version

--- plugins_tools/xpi/new-src/options.js
+++ plugins_tools/xpi/new-src/options.js
@@ -1,6 +1,8 @@
 async function findReaders() {
   let modnames = ["beidpkcs11", "beidpkcs11_32", "beidpkcs11_64", "beidpkcs11_alt"];
   let slots = undefined;
+  var modfound = document.querySelector("#middlewarefound");
+  var modvers = document.querySelector("#middlewareversion");
   for(modname of modnames) {
     try {
       slots = await browser.pkcs11.getModuleSlots(modname);
@@ -21,8 +23,14 @@ async function findReaders() {
       }
       innerHTML += "</li>";
     }
+    modfound.innerHTML = "yes";
+    let versionInfo = await browser.storage.managed.get("versionInfo");
+    versionInfo = versionInfo.versionInfo;
+    modvers.innerHTML = "" + versionInfo.major + "." + versionInfo.minor + "." + versionInfo.patch;
   } else {
     innerHTML = "(none)";
+    modfound.innerHTML = "no";
+    modvers.innerHTML = "(not found)";
   }
   ul.innerHTML = innerHTML;
 }
