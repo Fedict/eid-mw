fbba48102e694d0fe65215abe9f8ec328f1895b3
Wouter Verhelst <wouter.verhelst@zetes.com>
2022-05-16 13:04:14 +0200

    Update fedora injection script
    Packages built on rocky 8 won't be installed in an "epel" directory,
    which means our current script won't find them.
    
    Add a mapping to remedy.

--- scripts/build-aux/fed-rel.sh
+++ scripts/build-aux/fed-rel.sh
@@ -10,9 +10,10 @@ declare -A DISTARCHS
 
 DISTS="fedora epel"
 
-DISTVERS=([fedora]="$FEDORA_OLDSTABLE_VERSION $FEDORA_STABLE_VERSION" [epel]="$CENTOS_OLDSTABLE_VERSION $CENTOS_STABLE_VERSION")
-DISTNAMES=([fedora]="fedora" [epel]="el")
+DISTVERS=([fedora]="$FEDORA_OLDSTABLE_VERSION $FEDORA_STABLE_VERSION" [epel]="$EL_OLDSTABLE_VERSION $EL_STABLE_VERSION")
+DIST_TARGET_NAMES=([fedora]="fedora" [epel]="el")
 DISTARCHS=([fedora]="i386 x86_64" [epel]="x86_64")
+DIST_SOURCE_NAMES=(["epel:$EL_OLDSTABLE_VERSION"]="$EL_OLDSTABLE_DIST" ["epel:$EL_STABLE_VERSION"]="$EL_STABLE_DIST" ["fedora:$FEDORA_STABLE_VERSION"]="fedora" ["fedora:$FEDORA_OLDSTABLE_VERSION"]="fedora")
 
 for dist in $DISTS
 do
@@ -20,18 +21,17 @@ do
 	do
 		for arch in ${DISTARCHS[$dist]}
 		do
-			mkdir -p $REPO_LOC/rpm/$TARGET/${DISTNAMES[$dist]}/$vers/RPMS/$arch
-			if [ -d products/$dist-$vers-$arch ]
+			mkdir -p $REPO_LOC/rpm/$TARGET/${DIST_TARGET_NAMES[$dist]}/$vers/RPMS/$arch
+			if [ -d products/${DIST_SOURCE_NAMES["$dist:$vers"]}-$vers-$arch ]
 			then
-				for i in products/$dist-$vers-$arch/*rpm
+				for i in products/${DIST_SOURCE_NAMES["$dist:$vers"]}-$vers-$arch/*rpm
 				do
-					targetfile=$REPO_LOC/rpm/$TARGET/${DISTNAMES[$dist]}/$vers/RPMS/$arch/$(basename $i)
-					echo "$i => $targetfile"
+					targetfile=$REPO_LOC/rpm/$TARGET/${DIST_TARGET_NAMES[$dist]}/$vers/RPMS/$arch/$(basename $i)
 					cp $i $targetfile
 					rpmsign --resign --key-id=$GPG_TEST_KEY_ID $targetfile
 				done
 			fi
 		done
-		createrepo_c $REPO_LOC/rpm/$TARGET/${DISTNAMES[$dist]}/$vers
+		createrepo_c $REPO_LOC/rpm/$TARGET/${DIST_TARGET_NAMES[$dist]}/$vers
 	done
 done
