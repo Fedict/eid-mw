ca69106b5d6b826c0b707e03fc1395d70c878d44
Wouter Verhelst <wouter.verhelst@zetes.com>
2026-03-30 14:52:49 +0200

    Update old MSBuild path to something that might exist today

--- scripts/windows/SetPaths.bat
+++ scripts/windows/SetPaths.bat
@@ -1,7 +1,7 @@
 ::Define search paths here
 @set SEARCH_BEID_DIR_MSBUILD=C:\Program Files\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin
 @set SEARCH_BEID_DIR_MSBUILD_2=C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin
-@set SEARCH_BEID_DIR_MSBUILD_3=C:\Program Files (x86)\MSBuild\14.0\Bin
+@set SEARCH_BEID_DIR_MSBUILD_3=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\Bin
 
 ::@set SEARCH_SIGNTOOL_PATH=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Bin
 @set SEARCH_SIGNTOOL_PATH=C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64
